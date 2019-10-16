6935dbde0ba65bb29901903f0e17e985c6cf7b29
Richard W.M. Jones <rjones@redhat.com>
2019-10-16 22:46:55 +0100

    po-docs: Remove references to non-v2v man pages.
    Fixes commit 5f355c2952729581f1b988297fe8cabe756c01e2.

--- po-docs/language.mk
+++ po-docs/language.mk
@@ -25,59 +25,6 @@ LINGUA = $(shell basename -- `pwd`)
 CLEANFILES += *.pl
 
 MANPAGES = \
-	guestfish.1 \
-	guestfs.3 \
-	guestfs-building.1 \
-	guestfs-erlang.3 \
-	guestfs-examples.3 \
-	guestfs-faq.1 \
-	guestfs-hacking.1 \
-	guestfs-internals.1 \
-	guestfs-golang.3 \
-	guestfs-java.3 \
-	guestfs-lua.3 \
-	guestfs-ocaml.3 \
-	guestfs-performance.1 \
-	guestfs-perl.3 \
-	guestfs-python.3 \
-	guestfs-recipes.1 \
-	guestfs-release-notes.1 \
-	guestfs-ruby.3 \
-	guestfs-security.1 \
-	guestfs-testing.1 \
-	guestfsd.8 \
-	guestmount.1 \
-	guestunmount.1 \
-	libguestfs-make-fixed-appliance.1 \
-	libguestfs-test-tool.1 \
-	libguestfs-tools.conf.5 \
-	virt-alignment-scan.1 \
-	virt-builder.1 \
-	virt-cat.1 \
-	virt-copy-in.1 \
-	virt-copy-out.1 \
-	virt-customize.1 \
-	virt-df.1 \
-	virt-dib.1 \
-	virt-diff.1 \
-	virt-edit.1 \
-	virt-filesystems.1 \
-	virt-format.1 \
-	virt-get-kernel.1 \
-	virt-index-validate.1 \
-	virt-inspector.1 \
-	virt-list-filesystems.1 \
-	virt-list-partitions.1 \
-	virt-log.1 \
-	virt-ls.1 \
-	virt-make-fs.1 \
-	virt-rescue.1 \
-	virt-resize.1 \
-	virt-sparsify.1 \
-	virt-sysprep.1 \
-	virt-tar.1 \
-	virt-tar-in.1 \
-	virt-tar-out.1 \
 	virt-v2v.1 \
 	virt-v2v-copy-to-local.1 \
 	virt-v2v-input-vmware.1 \
@@ -86,8 +33,7 @@ MANPAGES = \
 	virt-v2v-output-openstack.1 \
 	virt-v2v-output-rhv.1 \
 	virt-v2v-support.1 \
-	virt-v2v-test-harness.1 \
-	virt-win-reg.1
+	virt-v2v-test-harness.1
 
 podfiles := $(shell for f in `cat $(top_srcdir)/po-docs/podfiles`; do echo `basename $$f .pod`.pod; done)
 
@@ -100,91 +46,10 @@ EXTRA_DIST = \
 
 all-local: $(MANPAGES)
 
-guestfs.3: guestfs.pod guestfs-actions.pod guestfs-availability.pod guestfs-structs.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --section 3 \
-	  --license LGPLv2+ \
-	  --insert $(srcdir)/guestfs-actions.pod:__ACTIONS__ \
-	  --insert $(srcdir)/guestfs-availability.pod:__AVAILABILITY__ \
-	  --insert $(srcdir)/guestfs-structs.pod:__STRUCTS__ \
-	  $<
-
-# XXX --warning parameter is not passed, so no WARNING section is
-# generated in any translated manual.  To fix this we need to expand
-# out all the %.1 pattern rules below.
-
-guestfish.1: guestfish.pod guestfish-actions.pod guestfish-commands.pod guestfish-prepopts.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --license GPLv2+ \
-	  --insert $(srcdir)/guestfish-actions.pod:__ACTIONS__ \
-	  --insert $(srcdir)/guestfish-commands.pod:__FISH_COMMANDS__ \
-	  --insert $(srcdir)/guestfish-prepopts.pod:__PREPOPTS__ \
-	  $<
-
-virt-builder.1: virt-builder.pod customize-synopsis.pod customize-options.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --license GPLv2+ \
-	  --insert $(srcdir)/customize-synopsis.pod:__CUSTOMIZE_SYNOPSIS__ \
-	  --insert $(srcdir)/customize-options.pod:__CUSTOMIZE_OPTIONS__ \
-	  $<
-
-virt-customize.1: virt-customize.pod customize-synopsis.pod customize-options.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --license GPLv2+ \
-	  --insert $(srcdir)/customize-synopsis.pod:__CUSTOMIZE_SYNOPSIS__ \
-	  --insert $(srcdir)/customize-options.pod:__CUSTOMIZE_OPTIONS__ \
-	  $<
-
-virt-sysprep.1: virt-sysprep.pod sysprep-extra-options.pod sysprep-operations.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --license GPLv2+ \
-          --insert $(srcdir)/sysprep-extra-options.pod:__EXTRA_OPTIONS__ \
-          --insert $(srcdir)/sysprep-operations.pod:__OPERATIONS__ \
-	  $<
-
-virt-p2v.1: virt-p2v.pod virt-p2v-kernel-config.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --license GPLv2+ \
-	  --insert $(srcdir)/virt-p2v-kernel-config.pod:__KERNEL_CONFIG__ \
-	  $<
-
 %.1: %.pod
 	$(PODWRAPPER) \
 	  --no-strict-checks \
 	  --man $@ \
-	  $<
-
-%.3: %.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --section 3 \
-	  $<
-
-%.5: %.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --section 5 \
-	  $<
-
-%.8: %.pod
-	$(PODWRAPPER) \
-	  --no-strict-checks \
-	  --man $@ \
-	  --section 8 \
 	  $<
 
 # If a POD file is missing, the user needs to run make update-po.
@@ -205,7 +70,3 @@ $(podfiles):
 install-data-hook:
 	$(MKDIR_P) $(DESTDIR)$(mandir)/$(LINGUA)/man1
 	$(INSTALL) -m 0644 $(srcdir)/*.1 $(DESTDIR)$(mandir)/$(LINGUA)/man1
-	$(MKDIR_P) $(DESTDIR)$(mandir)/$(LINGUA)/man3
-	$(INSTALL) -m 0644 $(srcdir)/*.3 $(DESTDIR)$(mandir)/$(LINGUA)/man3
-	$(MKDIR_P) $(DESTDIR)$(mandir)/$(LINGUA)/man5
-	$(INSTALL) -m 0644 $(srcdir)/*.5 $(DESTDIR)$(mandir)/$(LINGUA)/man5
