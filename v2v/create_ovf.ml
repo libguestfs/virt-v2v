libguestfs/virt-v2v

commit 7a925005869bb0883691824e7b4251a31a501092

ovf: Try to set BiosType correctly according to the oVirt sources.
Previously we mapped TargetBIOS to 0 and TargetUEFI to 2, but I
believe that both of these are wrong, and they certainly don't match
the oVirt source code.

This changes the BiosType calculation to use all necessary inputs
(architecture, I440FX|Q35, and TargetBIOS|TargetUEFI) and try to map
these as closely as possible to the BiosType as described in the oVirt
sources.

An exact mapping is not possible, so in cases where we can't find one
we punt on this by returning 0 (use cluster default).

v2v/create_ovf.ml
@@ -462,13 +462,18 @@ let origin_of_source_hypervisor = function
    *)
   | _ -> None
 
-(* Set the <BiosType> element. Other possible values:
- *   1  q35 + SeaBIOS
- *   3  q35 + UEFI + Secure Boot
+(* Set the <BiosType> element.
+ * https://github.com/oVirt/ovirt-engine/blob/master/backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BiosType.java#L10
  *)
-let get_ovirt_biostype = function
-  | TargetBIOS -> 0  (* i440fx + SeaBIOS *)
-  | TargetUEFI -> 2  (* q35 + UEFI *)
+let get_ovirt_biostype arch machine firmware =
+  if arch <> "x86_64" then
+    1
+  else
+    match machine, firmware with
+    | I440FX, TargetBIOS -> 1 (* i440fx + SeaBIOS *)
+    | Q35, TargetBIOS -> 2    (* q35 + SeaBIOS *)
+    | Q35, TargetUEFI -> 3    (* q35 + UEFI *)
+    | _ -> 0                  (* who knows! try cluster default *)
 
 (* Generate the .meta file associated with each volume. *)
 let create_meta_files output_alloc sd_uuid image_uuids overlays =
@@ -523,7 +528,9 @@ let rec create_ovf source targets guestcaps inspect target_firmware
   let vmtype = get_vmtype inspect in
   let vmtype = match vmtype with `Desktop -> "0" | `Server -> "1" in
   let ostype = get_ostype inspect in
-  let biostype = get_ovirt_biostype target_firmware in
+  let biostype =
+    get_ovirt_biostype guestcaps.gcaps_arch guestcaps.gcaps_machine
+                       target_firmware in
 
   let ovf : doc =
     doc "ovf:Envelope" [
@@ -611,8 +618,6 @@ let rec create_ovf source targets guestcaps inspect target_firmware
         e "Info" [] [PCData (sprintf "%d CPU, %Ld Memory"
                                      source.s_vcpu memsize_mb)]
       ] in
-
-      (* XXX How to set machine type for Q35? *)
 
       List.push_back virtual_hardware_section_items (
         e "Item" [] ([

tests/test-v2v-o-rhv.ovf.expected
@@ -25,7 +25,7 @@
     <IsStateless>False</IsStateless>
     <VmType>0</VmType>
     <DefaultDisplayType>1</DefaultDisplayType>
-    <BiosType>0</BiosType>
+    <BiosType>2</BiosType>
     <Section ovf:id='#VM_ID#' ovf:required='false' xsi:type='ovf:OperatingSystemSection_Type'>
       <Info>Microsoft Windows 7 Phony Edition</Info>
       <Description>Windows7</Description>

tests/test-v2v-o-vdsm-options.ovf.expected
@@ -25,7 +25,7 @@
     <IsStateless>False</IsStateless>
     <VmType>0</VmType>
     <DefaultDisplayType>1</DefaultDisplayType>
-    <BiosType>0</BiosType>
+    <BiosType>2</BiosType>
     <OperatingSystemSection ovf:id='VM' ovf:required='false' ovirt:id='11'>
       <Info>Microsoft Windows 7 Phony Edition</Info>
       <Description>Windows7</Description>

commit c8662724b7d8fbb7ea5e75b3a08f1e9a0311d525

ovf: Fix BiosType mapping to RHV again.
Previous commit 7a925005869b adjusted these mapping to match the oVirt
sources.  Unfortunately the sources refer to an "internal" BiosType
which doesn't match the external <BiosType> in the OVF.  We have to
adjust by subtracting one from the internal numbers.

I also adjusted the test cases to take into account that we are now
defaulting to Q35 machine type for the phony guests.  Thus the
<BiosType> across commit 7a925005869b and this commit changes
from 0 -> 1 (ie. i440fx -> Q35).

Fixes: commit 7a925005869bb0883691824e7b4251a31a501092
Thanks: Tomáš Golembiovský

v2v/create_ovf.ml
@@ -463,17 +463,26 @@ let origin_of_source_hypervisor = function
   | _ -> None
 
 (* Set the <BiosType> element.
+ * The internal numbers are:
  * https://github.com/oVirt/ovirt-engine/blob/master/backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BiosType.java#L10
+ * BUT the values in <BiosType> are adjusted by adding 1:
+ * https://www.redhat.com/archives/libguestfs/2020-December/msg00005.html
+ * So the actual mapping in the OVF is:
+ * 0 => i440fx + SeaBIOS
+ * 1 => Q35 + SeaBIOS
+ * 2 => Q35 + UEFI
+ * 3 => Q35 + UEFI + SecureBoot
+ * All non-x86 arches should use 0
  *)
 let get_ovirt_biostype arch machine firmware =
   if arch <> "x86_64" then
-    1
+    Some 0
   else
     match machine, firmware with
-    | I440FX, TargetBIOS -> 1 (* i440fx + SeaBIOS *)
-    | Q35, TargetBIOS -> 2    (* q35 + SeaBIOS *)
-    | Q35, TargetUEFI -> 3    (* q35 + UEFI *)
-    | _ -> 0                  (* who knows! try cluster default *)
+    | I440FX, TargetBIOS -> Some 0 (* i440fx + SeaBIOS *)
+    | Q35, TargetBIOS -> Some 1    (* q35 + SeaBIOS *)
+    | Q35, TargetUEFI -> Some 2    (* q35 + UEFI *)
+    | _ -> None                    (* who knows!  try cluster default *)
 
 (* Generate the .meta file associated with each volume. *)
 let create_meta_files output_alloc sd_uuid image_uuids overlays =
@@ -578,8 +587,16 @@ let rec create_ovf source targets guestcaps inspect target_firmware
         e "VmType" [] [PCData vmtype];
         (* See https://bugzilla.redhat.com/show_bug.cgi?id=1260590#c17 *)
         e "DefaultDisplayType" [] [PCData "1"];
-        e "BiosType" [] [PCData (string_of_int biostype)];
       ] in
+
+      (match biostype with
+       | None ->
+          (* omitting <BiosType> means use the cluster default *)
+          ()
+       | Some biostype ->
+          List.push_back content_subnodes
+                         (e "BiosType" [] [PCData (string_of_int biostype)])
+      );
 
       (match source.s_cpu_model with
         | None -> ()

tests/test-v2v-o-rhv.ovf.expected
@@ -25,7 +25,7 @@
     <IsStateless>False</IsStateless>
     <VmType>0</VmType>
     <DefaultDisplayType>1</DefaultDisplayType>
-    <BiosType>2</BiosType>
+    <BiosType>1</BiosType>
     <Section ovf:id='#VM_ID#' ovf:required='false' xsi:type='ovf:OperatingSystemSection_Type'>
       <Info>Microsoft Windows 7 Phony Edition</Info>
       <Description>Windows7</Description>

tests/test-v2v-o-vdsm-options.ovf.expected
@@ -25,7 +25,7 @@
     <IsStateless>False</IsStateless>
     <VmType>0</VmType>
     <DefaultDisplayType>1</DefaultDisplayType>
-    <BiosType>2</BiosType>
+    <BiosType>1</BiosType>
     <OperatingSystemSection ovf:id='VM' ovf:required='false' ovirt:id='11'>
       <Info>Microsoft Windows 7 Phony Edition</Info>
       <Description>Windows7</Description>