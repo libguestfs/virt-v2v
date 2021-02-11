libguestfs/virt-v2v

commit d4f8e5a4a01b0fbbb0158d0acc11f1920a0ac4fd

v2v: xen: Replace qemu block ssh driver with nbdkit-ssh-plugin.
Initially this is a like-for-like replacement, but in future commits
this will allow us to implement:

 - password authentication (instead of SSH agent)

 - bandwidth throttling

 - readahead

Note this requires nbdkit >= 1.12.

v2v/input_libvirt_xen_ssh.ml
@@ -45,18 +45,15 @@ object (self)
 
     let source, disks, _ = parse_libvirt_domain self#conn guest in
 
+    let port =
+      match parsed_uri.uri_port with
+      | 0 | 22 -> None
+      | i -> Some (string_of_int i) in
+
+    let user = parsed_uri.uri_user in
+
     (* Map the <source/> filename (which is relative to the remote
-     * Xen server) to an ssh URI.  This is a JSON URI looking something
-     * like this:
-     *
-     * json: {
-     *   "file.driver": "ssh",
-     *   "file.user": "username",
-     *   "file.host": "xen-host",
-     *   "file.port": 1022,
-     *   "file.path": <remote-path>,
-     *   "file.host_key_check": "no"
-     * }
+     * Xen server) to an ssh URI pointing to nbdkit.
      *)
     let disks = List.map (
       function
@@ -64,32 +61,9 @@ object (self)
         disk
       | { p_source_disk = disk; p_source = P_source_dev path }
       | { p_source_disk = disk; p_source = P_source_file path } ->
-        (* Construct the JSON parameters. *)
-        let json_params = [
-          "file.driver", JSON.String "ssh";
-          "file.path", JSON.String path;
-          "file.host", JSON.String server;
-          "file.host_key_check", JSON.String "no";
-        ] in
-
-        let json_params =
-          match parsed_uri.uri_port with
-          | 0 | 22 -> json_params
-          (* qemu will actually assert-fail if you send the port
-           * number as a string ...
-           *)
-          | i -> ("file.port", JSON.Int (Int64.of_int i)) :: json_params in
-
-        let json_params =
-          match parsed_uri.uri_user with
-          | None -> json_params
-          | Some user -> ("file.user", JSON.String user) :: json_params in
-
-        debug "ssh: json parameters: %s" (JSON.string_of_doc json_params);
-
-        (* Turn the JSON parameters into a 'json:' protocol string. *)
-        let qemu_uri = "json: " ^ JSON.string_of_doc json_params in
-
+         let nbdkit = Nbdkit.create_ssh ~password:NoPassword
+                                        ?port ~server ?user path in
+         let qemu_uri = Nbdkit.run nbdkit in
         { disk with s_qemu_uri = qemu_uri }
     ) disks in
 

v2v/nbdkit.ml
@@ -26,8 +26,8 @@ open Unix_utils
 
 open Utils
 
-let nbdkit_min_version = (1, 2)
-let nbdkit_min_version_string = "1.2"
+let nbdkit_min_version = (1, 12)
+let nbdkit_min_version_string = "1.12"
 
 type password =
 | NoPassword                    (* no password option at all *)

commit 2c91610ab35ad1e8b49596bcaa94731c67031180

v2v/input_libvirt_xen_ssh.ml: Remove obsolete error check.
Since https://bugzilla.redhat.com/1140166 was fixed (or worked around)
by using nbdkit-ssh-plugin to access Xen servers, we no longer need
libvirt to support access to the qemu ssh block driver, so the check
is no longer correct.

Fixes: commit d4f8e5a4a01b0fbbb0158d0acc11f1920a0ac4fd
Thanks: Tomáš Golembiovský

v2v/input_libvirt_xen_ssh.ml
@@ -35,8 +35,6 @@ object (self)
   inherit input_libvirt libvirt_conn ~input_conn guest
 
   method precheck () =
-    if backend_is_libvirt () then
-      error (f_"because of libvirt bug https://bugzilla.redhat.com/1140166 you must set this environment variable:\n\nexport LIBGUESTFS_BACKEND=direct\n\nand then rerun the virt-v2v command.");
     error_if_libvirt_does_not_support_json_backingfile ();
     error_if_no_ssh_agent ()
 