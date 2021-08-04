libguestfs/virt-v2v

commit 143a22860216b94d3a81706193088d50c03fc35c

v2v: -o rhv-upload: Split plugin functions from create/finalize
The existing plugin created a new disk in open() and tried to finalize
it in close().  A correct NBD client may make several connections
(eg. connecting just to query for multi-conn or other metadata).  This
could result in multiple disks being created.

Let's fix this by using Nir's suggestion to split out the create and
finalize functions.  The nbdkit plugin becomes a much more
straightforward plugin - it no longer depends on the oVirt SDK at all
so could in theory be rewritten in another programming language.  The
other functions have been moved into new scripts ("transfer",
"finalize", "cancel") which are run separately.

Some subtler points about this commit:

 - We no longer use or need the @failing decorator.  I have extended
   the cleanup code so as well as deleting disks it also cancels the
   transfers.  Any failure detected by qemu-img convert will result in
   virt-v2v exiting and cancelling the transfer, which is much cleaner
   and more robust.

 - We still require the HTTP pool.  The reasons is a bit subtle: Since
   we are using the parallel thread model, pread and pwrite (etc) can
   be called at any time, even overlapping, so storing an HTTP handle
   per NBD connection does not work as the handle gets reused (which
   results in a fatal error).

 - In the plugin we now use the standard nbdkit_debug (Python
   nbdkit.debug) function instead of writing debug to stderr.

 - This is not noticably any slower or faster than before.

Thanks: Nir Soffer

v2v/rhv-upload-cancel.py
@@ -1,6 +1,6 @@
 # -*- python -*-
-# oVirt or RHV upload delete disks used by ‘virt-v2v -o rhv-upload’
-# Copyright (C) 2019 Red Hat Inc.
+# oVirt or RHV upload cancel used by ‘virt-v2v -o rhv-upload’
+# Copyright (C) 2019-2021 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,11 +19,18 @@
 import json
 import logging
 import sys
-
+from contextlib import closing
 from urllib.parse import urlparse
 
 import ovirtsdk4 as sdk
 import ovirtsdk4.types as types
+
+
+def debug(s):
+    if params['verbose']:
+        print(s, file=sys.stderr)
+        sys.stderr.flush()
+
 
 # Parameters are passed in via a JSON doc from the OCaml code.
 # Because this Python code ships embedded inside virt-v2v there
@@ -56,14 +63,31 @@ connection = sdk.Connection(
     insecure=params['insecure'],
 )
 
-system_service = connection.system_service()
-disks_service = system_service.disks_service()
+with closing(connection):
+    system_service = connection.system_service()
+    image_transfers_service = system_service.image_transfers_service()
 
-for uuid in params['disk_uuids']:
-    # Try to get and remove the disk, however do not fail
-    # if it does not exist (maybe removed in the meanwhile).
-    try:
-        disk_service = disks_service.disk_service(uuid)
-        disk_service.remove()
-    except sdk.NotFoundError:
-        pass
+    # Try to cancel the transfers.  This should delete the associated disk.
+    for id in params['transfer_ids']:
+        try:
+            transfer_service = \
+                image_transfers_service.image_transfer_service(id)
+            transfer_service.cancel()
+        except sdk.NotFoundError:
+            debug("unexpected error: transfer id %s not found" % id)
+        except Exception:
+            if params['verbose']: traceback.print_exc()
+
+    disks_service = system_service.disks_service()
+
+    # In case we didn't associate a disk with a transfer and as a last
+    # resort, delete the disk too.
+    for uuid in params['disk_uuids']:
+        try:
+            disk_service = disks_service.disk_service(uuid)
+            disk_service.remove()
+        except (sdk.NotFoundError, sdk.Error):
+            # We expect these exceptions so ignore them.
+            pass
+        except Exception:
+            if params['verbose']: traceback.print_exc()

po/POTFILES-ml
@@ -62,10 +62,12 @@ v2v/output_openstack.ml
 v2v/output_qemu.ml
 v2v/output_rhv.ml
 v2v/output_rhv_upload.ml
+v2v/output_rhv_upload_cancel_source.ml
 v2v/output_rhv_upload_createvm_source.ml
-v2v/output_rhv_upload_deletedisks_source.ml
+v2v/output_rhv_upload_finalize_source.ml
 v2v/output_rhv_upload_plugin_source.ml
 v2v/output_rhv_upload_precheck_source.ml
+v2v/output_rhv_upload_transfer_source.ml
 v2v/output_rhv_upload_vmcheck_source.ml
 v2v/output_vdsm.ml
 v2v/parse_libvirt_xml.ml

tests/Makefile.am
@@ -243,6 +243,7 @@ EXTRA_DIST += \
 	test-v2v-o-rhv.ovf.expected \
 	test-v2v-o-rhv.sh \
 	test-v2v-o-rhv-upload.sh \
+	test-v2v-o-rhv-upload-module/imageio.py \
 	test-v2v-o-rhv-upload-module/ovirtsdk4/__init__.py \
 	test-v2v-o-rhv-upload-module/ovirtsdk4/types.py \
 	test-v2v-o-rhv-upload-oo-query.sh \

tests/test-v2v-o-rhv-upload-module/imageio.py
@@ -1,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- python -*-
+# Copyright (C) 2018-2021 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Fake imageio web server used as a test harness.
+# See v2v/test-v2v-o-rhv-upload.sh
+
+import sys
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
+
+    def do_OPTIONS(self):
+        self.discard_request()
+
+        # Advertize only flush and zero support.
+        content = b'''{ "features": [ "flush", "zero" ] }'''
+        length = len(content)
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json; charset=UTF-8")
+        self.send_header("Content-Length", length)
+        self.end_headers()
+        self.wfile.write(content)
+
+    # eg. zero request.  Just ignore it.
+    def do_PATCH(self):
+        self.discard_request()
+        self.send_response(200)
+        self.send_header("Content-Length", "0")
+        self.end_headers()
+
+    # Flush request.  Ignore it.
+    def do_PUT(self):
+        self.discard_request()
+        self.send_response(200)
+        self.send_header("Content-Length", "0")
+        self.end_headers()
+
+    def discard_request(self):
+        length = self.headers.get('Content-Length')
+        if length:
+            length = int(length)
+            content = self.rfile.read(length)
+
+server_address = ("", 0)
+# XXX This should test HTTPS, not HTTP, because we are testing a
+# different path through the main code.
+httpd = HTTPServer(server_address, RequestHandler)
+imageio_port = httpd.server_address[1]
+
+print("port: %d" % imageio_port)
+sys.stdout.flush()
+
+httpd.serve_forever()

tests/test-v2v-o-rhv-upload-module/ovirtsdk4/__init__.py
@@ -18,10 +18,6 @@
 # Fake ovirtsdk4 module used as a test harness.
 # See v2v/test-v2v-o-rhv-upload.sh
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import threading
-
-
 class Error(Exception):
     pass
 
@@ -144,59 +140,3 @@ class VmsService(object):
 
     def list(self, search=None):
         return []
-
-
-# Create a background thread running a web server which is
-# simulating the imageio server.
-
-
-class RequestHandler(BaseHTTPRequestHandler):
-    protocol_version = 'HTTP/1.1'
-
-    def do_OPTIONS(self):
-        self.discard_request()
-
-        # Advertize only flush and zero support.
-        content = b'''{ "features": [ "flush", "zero" ] }'''
-        length = len(content)
-
-        self.send_response(200)
-        self.send_header("Content-type", "application/json; charset=UTF-8")
-        self.send_header("Content-Length", length)
-        self.end_headers()
-        self.wfile.write(content)
-
-    # eg. zero request.  Just ignore it.
-    def do_PATCH(self):
-        self.discard_request()
-        self.send_response(200)
-        self.send_header("Content-Length", "0")
-        self.end_headers()
-
-    # Flush request.  Ignore it.
-    def do_PUT(self):
-        self.discard_request()
-        self.send_response(200)
-        self.send_header("Content-Length", "0")
-        self.end_headers()
-
-    def discard_request(self):
-        length = self.headers.get('Content-Length')
-        if length:
-            length = int(length)
-            content = self.rfile.read(length)
-
-
-server_address = ("", 0)
-# XXX This should test HTTPS, not HTTP, because we are testing a
-# different path through the main code.
-httpd = HTTPServer(server_address, RequestHandler)
-imageio_port = httpd.server_address[1]
-
-
-def server():
-    httpd.serve_forever()
-
-
-thread = threading.Thread(target=server, args=[], daemon=True)
-thread.start()

tests/test-v2v-o-rhv-upload-module/ovirtsdk4/types.py
@@ -18,8 +18,11 @@
 # Fake ovirtsdk4 module used as a test harness.
 # See v2v/test-v2v-o-rhv-upload.sh
 
+import os
 from enum import Enum
-from ovirtsdk4 import imageio_port
+
+imageio_port = os.getenv("IMAGEIO_PORT")
+assert imageio_port is not None
 
 
 class Architecture(Enum):
@@ -130,7 +133,7 @@ class ImageTransfer(object):
 
     id = "e26ac8ab-7090-4d5e-95ad-e707b511a359"
     phase = ImageTransferPhase.TRANSFERRING
-    transfer_url = "http://localhost:" + str(imageio_port) + "/"
+    transfer_url = "http://localhost:" + imageio_port + "/"
 
 
 class Initialization(object):

tests/test-v2v-o-rhv-upload.sh
@@ -41,6 +41,26 @@ export VIRT_TOOLS_DATA_DIR="$srcdir/../test-data/fake-virt-tools"
 export VIRTIO_WIN="$srcdir/../test-data/fake-virtio-win"
 export PYTHONPATH=$srcdir/test-v2v-o-rhv-upload-module:$PYTHONPATH
 
+# Run the imageio process and get the port number.
+log=test-v2v-o-rhv-upload.webserver.log
+rm -f $log
+cleanup_fn rm -f $log
+$srcdir/test-v2v-o-rhv-upload-module/imageio.py >$log 2>&1 &
+pid=$!
+cleanup_fn kill $pid
+export IMAGEIO_PORT=
+for i in {1..5}; do
+    IMAGEIO_PORT=$( grep "^port:" $log | awk '{print $2}' )
+    if [ -n "$IMAGEIO_PORT" ]; then break; fi
+    sleep 3
+done
+if [ ! -n "$IMAGEIO_PORT" ]; then
+    echo "$0: imageio process did not start up"
+    cat $log
+    exit 1
+fi
+echo IMAGEIO_PORT=$IMAGEIO_PORT
+
 # Run virt-v2v -o rhv-upload.
 #
 # The fake ovirtsdk4 module doesn't care about most of the options

commit 35ce82170b267650dbe77dd50e2826b13892b9ce

v2v: Fix minor Python style issues
Fixes: commit 143a22860216b94d3a81706193088d50c03fc35c

v2v/rhv-upload-cancel.py
@@ -76,7 +76,8 @@ with closing(connection):
         except sdk.NotFoundError:
             debug("unexpected error: transfer id %s not found" % id)
         except Exception:
-            if params['verbose']: traceback.print_exc()
+            if params['verbose']:
+                traceback.print_exc()
 
     disks_service = system_service.disks_service()
 
@@ -90,4 +91,5 @@ with closing(connection):
             # We expect these exceptions so ignore them.
             pass
         except Exception:
-            if params['verbose']: traceback.print_exc()
+            if params['verbose']:
+                traceback.print_exc()

v2v/rhv-upload-plugin.py
@@ -311,6 +311,7 @@ class UnixHTTPConnection(HTTPConnection):
             self.sock.settimeout(timeout)
         self.sock.connect(self.path)
 
+
 # Connection pool.
 def create_http_pool(url, options):
     pool = queue.Queue()