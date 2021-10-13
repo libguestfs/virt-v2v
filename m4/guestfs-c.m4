libguestfs/virt-v2v

commit af9251086030886580cd5243afabf726d5e50fdc

build: Remove gnulib submodule.

m4/guestfs-c.m4
@@ -24,111 +24,27 @@ AC_PROG_CC_STDC
 AC_PROG_INSTALL
 AC_PROG_CPP
 
-AC_ARG_ENABLE([werror],
-    [AS_HELP_STRING([--enable-werror],
-                    [turn GCC warnings into errors (for developers)])],
-    [case $enableval in
-     yes|no) ;;
-     *)      AC_MSG_ERROR([bad value $enableval for werror option]) ;;
-     esac
-     gl_gcc_werror=$enableval],
-    [gl_gcc_werror=no]
-)
-
-if test "$gl_gcc_werror" = yes; then
-    gl_WARN_ADD([-Werror], [WERROR_CFLAGS])
-    AC_SUBST([WERROR_CFLAGS])
-fi
-
-dnl This, $nw, is the list of warnings we disable.
-nw=
-nw="$nw -Waggregate-return"          # anachronistic
-nw="$nw -Wundef"                     # Warns on '#if GNULIB_FOO' etc in gnulib
-nw="$nw -Wtraditional"               # Warns on #elif which we use often
-nw="$nw -Wsystem-headers"            # Don't let system headers trigger warnings
-nw="$nw -Wpadded"                    # Our structs are not padded
-nw="$nw -Wvla"                       # Allow variable length arrays.
-nw="$nw -Wvla-larger-than=4031"
-nw="$nw -Winline"                    # inline functions in Python binding
-nw="$nw -Wshadow"                    # Not useful, as it applies to global vars
-nw="$nw -Wunsafe-loop-optimizations" # just a warning that an optimization
-                                     # was not possible, safe to ignore
-nw="$nw -Wstack-protector"           # Useless warning when stack protector
-                                     # cannot being used in a function.
-nw="$nw -Wcast-align"                # Useless warning on arm >= 7, intel
-nw="$nw -Wabi"                       # Broken in GCC 8.1.
-dnl things I might fix soon:
-nw="$nw -Wpacked"                    # Allow attribute((packed)) on structs
-nw="$nw -Wlong-long"                 # Allow long long since it's required
-                                     # by Python, Ruby and xstrtoll.
-nw="$nw -Wsuggest-attribute=pure"    # Don't suggest pure functions.
-nw="$nw -Wsuggest-attribute=const"   # Don't suggest const functions.
-nw="$nw -Wsuggest-attribute=malloc"  # Don't suggest malloc functions.
-nw="$nw -Wunsuffixed-float-constants" # Don't care about these.
-nw="$nw -Wswitch-default"            # This warning is actively dangerous.
-nw="$nw -Woverlength-strings"        # Who cares about stupid ISO C99 limit.
-
-gl_MANYWARN_ALL_GCC([ws])
-gl_MANYWARN_COMPLEMENT([ws], [$ws], [$nw])
-for w in $ws; do
-    gl_WARN_ADD([$w])
-done
-
-dnl Normally we disable warnings in $nw above.  However $nw only
-dnl filters out exact matching warning strings from a list inside
-dnl gnulib (see m4/manywarnings.m4).  So we need to explicitly list a
-dnl few disabled warnings below.
-
-dnl Unused parameters are not a bug.
-gl_WARN_ADD([-Wno-unused-parameter])
-
-dnl Missing field initializers is not a bug in C.
-gl_WARN_ADD([-Wno-missing-field-initializers])
-
-dnl Display the name of the warning option with the warning.
-gl_WARN_ADD([-fdiagnostics-show-option])
-
-dnl Now some warnings we want to enable and/or customize ...
-
-dnl Warn about large stack frames.  This does not include alloca and
-dnl variable length arrays.  Coverity warns about 10000 byte frames.
-gl_WARN_ADD([-Wframe-larger-than=5000])
-
-dnl Warn about large stack frames, including estimates for alloca
-dnl and variable length arrays.
-gl_WARN_ADD([-Wstack-usage=10000])
-
-dnl Warn about implicit fallthrough in case statements, but suppress
-dnl the warning if /*FALLTHROUGH*/ comment is used.
-gl_WARN_ADD([-Wimplicit-fallthrough=4])
-
-dnl GCC level 2 gives incorrect warnings, so use level 1.
-gl_WARN_ADD([-Wformat-truncation=1])
-
-dnl GCC 9 at level 2 gives apparently bogus errors when %.*s is used.
-gl_WARN_ADD([-Wformat-overflow=1])
-
-AC_SUBST([WARN_CFLAGS])
-
-NO_SNV_CFLAGS=
-gl_COMPILER_OPTION_IF([-Wno-shift-negative-value],[
-    NO_SNV_CFLAGS="-Wno-shift-negative-value"
-])
-AC_SUBST([NO_SNV_CFLAGS])
-
-NO_UM_CFLAGS=
-gl_COMPILER_OPTION_IF([-Wno-unused-macros],[
-    NO_UM_CFLAGS="-Wno-unused-macros"
-])
-AC_SUBST([NO_UM_CFLAGS])
-
-AC_DEFINE([lint], [1], [Define to 1 if the compiler is checking for lint.])
-AC_DEFINE([GNULIB_PORTCHECK], [1], [Enable some gnulib portability checks.])
-
 AC_C_PROTOTYPES
 test "x$U" != "x" && AC_MSG_ERROR([Compiler not ANSI compliant])
 
 AM_PROG_CC_C_O
+
+AC_ARG_ENABLE([werror],
+    [AS_HELP_STRING([--enable-error],
+                    [turn on lots of GCC warnings (for developers)])],
+     [case $enableval in
+      yes|no) ;;
+      *)      AC_MSG_ERROR([bad value $enableval for werror option]) ;;
+      esac
+      gcc_warnings=$enableval],
+      [gcc_warnings=no]
+)
+WARN_CFLAGS="-Wall"
+AC_SUBST([WARN_CFLAGS])
+if test "x$gcc_warnings" = "xyes"; then
+    WERROR_CFLAGS="-Werror"
+fi
+AC_SUBST([WERROR_CFLAGS])
 
 # Provide a global place to set CFLAGS.  (Note that setting AM_CFLAGS
 # is no use because it doesn't override target_CFLAGS).
@@ -189,13 +105,3 @@ The code will still compile, but is likely to leak memory and other
 resources when it runs.])])
 dnl restore CFLAGS
 CFLAGS="${acx_nbdkit_save_CFLAGS}"
-
-dnl Should we run the gnulib tests?
-AC_MSG_CHECKING([if we should run the GNUlib tests])
-AC_ARG_ENABLE([gnulib-tests],
-    [AS_HELP_STRING([--disable-gnulib-tests],
-        [disable running GNU Portability library tests @<:@default=yes@:>@])],
-        [ENABLE_GNULIB_TESTS="$enableval"],
-        [ENABLE_GNULIB_TESTS=yes])
-AM_CONDITIONAL([ENABLE_GNULIB_TESTS],[test "x$ENABLE_GNULIB_TESTS" = "xyes"])
-AC_MSG_RESULT([$ENABLE_GNULIB_TESTS])

.gitignore
@@ -60,16 +60,11 @@ Makefile.in
 /docs/virt-v2v-support.1
 /.gitattributes
 /.git-module-status
-/gnulib
-/GNUmakefile
 /installcheck.sh
 /install-sh
 /libtool
 /local*
 /ltmain.sh
-/m4/ChangeLog
-/m4/gnulib-cache.m4
-/m4/intmax.m4
 /m4/libtool.m4
 /m4/lt~obsolete.m4
 /m4/ltoptions.m4

.gitmodules
@@ -1,6 +1,3 @@
-[submodule "gnulib"]
-	path = .gnulib
-	url = https://git.savannah.gnu.org/git/gnulib.git
 [submodule "common"]
 	path = common
 	url = https://github.com/libguestfs/libguestfs-common

Makefile.am
@@ -25,11 +25,7 @@ SUBDIRS = common/mlstdutils
 # tests run, except tests/qemu.
 SUBDIRS += test-data
 
-# Gnulib - must be built and tested before the program.
 SUBDIRS += gnulib/lib
-if ENABLE_GNULIB_TESTS
-SUBDIRS += gnulib/tests
-endif
 
 SUBDIRS += common/qemuopts
 SUBDIRS += common/utils
@@ -64,8 +60,6 @@ endif
 # lib/guestfs-internal-all.h is a hack, remove it XXX
 EXTRA_DIST = \
 	.gitignore \
-	autogen.sh \
-	bootstrap \
 	bugs-in-changelog.sh \
 	build-aux/config.rpath \
 	cfg.mk \
@@ -74,7 +68,6 @@ EXTRA_DIST = \
 	common/README \
 	COPYING \
 	lib/guestfs-internal-all.h \
-	m4/.gitignore \
 	ocaml-link.sh \
 	podcheck.pl \
 	tmp/.gitignore \
@@ -114,7 +107,7 @@ po/POTFILES: configure.ac
 	rm -f $@ $@-t
 	cd $(srcdir); \
 	find $(DIST_SUBDIRS) -name '*.c' | \
-	grep -v -E '^(gnulib|po-docs|tests|test-data|bundled)/' | \
+	grep -v -E '^(po-docs|tests|test-data|bundled)/' | \
 	grep -v -E '/(dummy\.c)$$' | \
 	grep -v -E '.*-(tests)\.c$$' | \
 	LC_ALL=C sort -u > $@-t
@@ -215,7 +208,6 @@ maintainer-check-extra-dist:
 	  grep -v '^common/windows/' | \
 	  grep -v '^intltool-.*\.in' | \
 	  grep -v '^\.gitmodules' | \
-	  grep -v '^\.gnulib' | \
 	  sort > tmp/gitfiles
 	comm -13 tmp/tarfiles tmp/gitfiles > tmp/comm-out
 	@echo Checking for differences between EXTRA_DIST and git ...

README
@@ -20,7 +20,8 @@ the source tree.
 To build from git:
 
   git submodule update --init
-  ./autogen.sh
+  autoreconf -i
+  ./configure
   make
 
 To build from tarball:

autogen.sh
@@ -1,51 +1,0 @@
-#!/bin/bash -
-# libguestfs
-# Copyright (C) 2009 Red Hat Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Rebuild the autotools environment.
-
-set -e
-set -v
-
-# Ensure that whenever we pull in a gnulib update or otherwise change to a
-# different version (i.e., when switching branches), we also rerun ./bootstrap.
-curr_status=.git-module-status
-t=$(git submodule status | grep gnulib | sed 's/^[ +-]//;s/ .*//')
-if test "$t" = "$(cat $curr_status 2>/dev/null)"; then
-    : # good, it's up to date
-else
-    echo running bootstrap...
-    ./bootstrap && echo "$t" > $curr_status
-fi
-
-CONFIGUREDIR=.
-
-# Run configure in BUILDDIR if it's set
-if [ ! -z "$BUILDDIR" ]; then
-    mkdir -p $BUILDDIR
-    cd $BUILDDIR
-
-    CONFIGUREDIR=..
-fi
-
-# If no arguments were specified and configure has run before, use the previous
-# arguments
-if test $# -eq 0 && test -x ./config.status; then
-    ./config.status --recheck
-else
-    $CONFIGUREDIR/configure "$@"
-fi

bootstrap
@@ -1,132 +1,0 @@
-#!/bin/sh
-
-usage() {
-  echo >&2 "\
-Usage: $0 [OPTION]...
-Bootstrap this package from the checked-out sources.
-
-Options:
- --gnulib-srcdir=DIRNAME  specify the local directory where gnulib
-                          sources reside.  Use this if you already
-                          have gnulib sources on your machine, and
-                          do not want to waste your bandwidth downloading
-                          them again.  Defaults to \$GNULIB_SRCDIR
-"
-}
-
-for option
-do
-  case $option in
-  --help)
-    usage
-    exit;;
-  --gnulib-srcdir=*)
-    GNULIB_SRCDIR=${option#--gnulib-srcdir=};;
-  *)
-    echo >&2 "$0: $option: unknown option"
-    exit 1;;
-  esac
-done
-
-cleanup_gnulib() {
-  status=$?
-  rm -fr "$gnulib_path"
-  exit $status
-}
-
-git_modules_config () {
-  test -f .gitmodules && git config --file .gitmodules "$@"
-}
-
-gnulib_path=$(git_modules_config submodule.gnulib.path)
-test -z "$gnulib_path" && gnulib_path=gnulib
-
-# Get gnulib files.  Populate $GNULIB_SRCDIR, possibly updating a
-# submodule, for use in the rest of the script.
-
-case ${GNULIB_SRCDIR--} in
--)
-  if git_modules_config submodule.gnulib.url >/dev/null; then
-    echo "$0: getting gnulib files..."
-    git submodule init -- "$gnulib_path" || exit $?
-    git submodule update -- "$gnulib_path" || exit $?
-
-  elif [ ! -d "$gnulib_path" ]; then
-    echo "$0: getting gnulib files..."
-
-    trap cleanup_gnulib 1 2 13 15
-
-    shallow=
-    git clone -h 2>&1 | grep -- --depth > /dev/null && shallow='--depth 2'
-    git clone $shallow git://git.sv.gnu.org/gnulib "$gnulib_path" ||
-      cleanup_gnulib
-
-    trap - 1 2 13 15
-  fi
-  GNULIB_SRCDIR=$gnulib_path
-  ;;
-*)
-  # Use GNULIB_SRCDIR directly or as a reference.
-  if test -d "$GNULIB_SRCDIR"/.git && \
-        git_modules_config submodule.gnulib.url >/dev/null; then
-    echo "$0: getting gnulib files..."
-    if git submodule -h|grep -- --reference > /dev/null; then
-      # Prefer the one-liner available in git 1.6.4 or newer.
-      git submodule update --init --reference "$GNULIB_SRCDIR" \
-        "$gnulib_path" || exit $?
-    else
-      # This fallback allows at least git 1.5.5.
-      if test -f "$gnulib_path"/gnulib-tool; then
-        # Since file already exists, assume submodule init already complete.
-        git submodule update -- "$gnulib_path" || exit $?
-      else
-        # Older git can't clone into an empty directory.
-        rmdir "$gnulib_path" 2>/dev/null
-        git clone --reference "$GNULIB_SRCDIR" \
-          "$(git_modules_config submodule.gnulib.url)" "$gnulib_path" \
-          && git submodule init -- "$gnulib_path" \
-          && git submodule update -- "$gnulib_path" \
-          || exit $?
-      fi
-    fi
-    GNULIB_SRCDIR=$gnulib_path
-  fi
-  ;;
-esac
-
-# Autoreconf runs aclocal before libtoolize, which causes spurious
-# warnings if the initial aclocal is confused by the libtoolized
-# (or worse out-of-date) macro directory.
-libtoolize --copy --install
-
-gnulib_tool=$GNULIB_SRCDIR/gnulib-tool
-<$gnulib_tool || exit
-
-modules='
-c-ctype
-getprogname
-hash
-ignore-value
-lock
-maintainer-makefile
-manywarnings
-sys_types
-tls
-xstrtol
-'
-
-# If any tests fail, avoid including them by adding them to
-# this list.
-avoid=""
-
-$gnulib_tool			\
-  $avoid			\
-  --with-tests			\
-  --m4-base=m4			\
-  --source-base=gnulib/lib	\
-  --tests-base=gnulib/tests	\
-  --libtool			\
-  --import $modules
-
-# Disable autopoint and libtoolize, since they were already done above.
-AUTOPOINT=true LIBTOOLIZE=true autoreconf --verbose --install

configure.ac
@@ -76,11 +76,6 @@ PACKAGE_VERSION_FULL="v2v_major.v2v_minor.v2v_release${v2v_extra}"
 AC_DEFINE_UNQUOTED([PACKAGE_VERSION_FULL],["$PACKAGE_VERSION_FULL"],[Full version string.])
 AC_SUBST([PACKAGE_VERSION_FULL])
 
-dnl Early gnulib initialization.
-HEADING([Configuring Gnulib])
-gl_EARLY
-gl_INIT
-
 dnl Check for external programs required to either build or run
 dnl virt-v2v.
 HEADING([Checking for external programs])
@@ -149,7 +144,6 @@ AC_CONFIG_FILES([Makefile
                  config.sh
                  docs/Makefile
                  gnulib/lib/Makefile
-                 gnulib/tests/Makefile
                  po-docs/Makefile
                  po-docs/ja/Makefile
                  po-docs/uk/Makefile

commit 7ce5df273be304ec3ba87e46319cf78275eb4990

build: fix typo in "--enable-werror" help string
Fixes: af9251086030886580cd5243afabf726d5e50fdc
Signed-off-by: Laszlo Ersek <lersek@redhat.com>
Message-Id: <20211012160824.8488-1-lersek@redhat.com>
Acked-by: Richard W.M. Jones <rjones@redhat.com>

m4/guestfs-c.m4
@@ -30,7 +30,7 @@ test "x$U" != "x" && AC_MSG_ERROR([Compiler not ANSI compliant])
 AM_PROG_CC_C_O
 
 AC_ARG_ENABLE([werror],
-    [AS_HELP_STRING([--enable-error],
+    [AS_HELP_STRING([--enable-werror],
                     [turn on lots of GCC warnings (for developers)])],
      [case $enableval in
       yes|no) ;;