libguestfs/virt-v2v

commit 40cfe6da0861ca6360f670e254c71ed923a0402f

v2v: Force format of input to be specified
qemu 6.1 unnecessarily insists on the backing format of files being
set.  Change the type of the input disk so the format is no longer an
option, but must be set by the input mode.

This change is only required on the 1.44 branch, since modular
virt-v2v uses the qemu-nbd -s (snapshot) option to do the equivalent
which seems to handle the backing format automatically.

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2025769
Reported-by: Vera Wu

v2v/parse_libvirt_xml.ml
@@ -264,9 +264,10 @@ let parse_libvirt_xml ?bandwidth ?conn xml =
 
       let format =
         match xpath_string "driver/@type" with
-        | Some "aio" -> Some "raw" (* Xen wierdness *)
-        | None -> None
-        | Some format -> Some format in
+        | Some "aio" -> "raw" (* Xen wierdness *)
+        | Some format -> format
+        | None ->
+           error (f_"<disk><driver type=\"format\"> attribute is missing from the libvirt XML") in
 
       (* The <disk type='...'> attribute may be 'block', 'file',
        * 'network' or 'volume'.  We ignore any other types.
@@ -333,7 +334,11 @@ let parse_libvirt_xml ?bandwidth ?conn xml =
           let xpath_string = Xpath_helpers.xpath_string xpathctx in
 
           (* Use the format specified in the volume itself. *)
-          let format = xpath_string "/volume/target/format/@type" in
+          let format =
+            match xpath_string "/volume/target/format/@type" with
+            | Some format -> format
+            | None ->
+               error (f_"<volume><target>.<format type=\"format\"> attribute is missing from the libvirt XML of volume %s") vol in
 
           (match xpath_string "/volume/@type" with
           | None | Some "file" ->

v2v/input_disk.ml
@@ -64,7 +64,7 @@ class input_disk input_format disk = object
     let disk = {
       s_disk_id = 0;
       s_qemu_uri = disk_absolute;
-      s_format = Some format;
+      s_format = format;
       s_controller = None;
     } in
 

v2v/input_libvirt_vcenter_https.ml
@@ -95,7 +95,7 @@ object (self)
         (* The libvirt ESX driver doesn't normally specify a format, but
          * the format of the -flat file is *always* raw, so force it here.
          *)
-        { disk with s_qemu_uri = qemu_uri; s_format = Some "raw" }
+        { disk with s_qemu_uri = qemu_uri; s_format = "raw" }
     ) disks in
 
     source, disks

v2v/input_libvirt_vddk.ml
@@ -187,7 +187,7 @@ object (self)
          (* nbdkit always presents us with the raw disk blocks from
           * the guest, so force the format to raw here.
           *)
-         { disk with s_qemu_uri = qemu_uri; s_format = Some "raw" }
+         { disk with s_qemu_uri = qemu_uri; s_format = "raw" }
     ) disks in
 
     source, disks

v2v/input_vmx.ml
@@ -190,7 +190,7 @@ and find_hdds ?bandwidth input_password vmx vmx_source
            let uri, format = qemu_uri_of_filename ?bandwidth input_password
                                                   vmx_source filename in
            let s = { s_disk_id = (-1);
-                     s_qemu_uri = uri; s_format = Some format;
+                     s_qemu_uri = uri; s_format = format;
                      s_controller = Some controller } in
            Some (c, t, s)
         | _ -> None

v2v/parse_ovf_from_ova.ml
@@ -157,7 +157,7 @@ and parse_disks xpathctx =
         source_disk = {
           s_disk_id = i;
           s_qemu_uri = "";
-          s_format = Some "vmdk";
+          s_format = "vmdk";
           s_controller = controller;
         };
         href = href;

v2v/types.ml
@@ -55,7 +55,7 @@ and source_firmware =
 and source_disk = {
   s_disk_id : int;
   s_qemu_uri : string;
-  s_format : string option;
+  s_format : string;
   s_controller : s_controller option;
 }
 and s_controller = Source_IDE | Source_SATA | Source_SCSI |
@@ -196,11 +196,8 @@ and string_of_source_firmware = function
 
 and string_of_source_disk { s_qemu_uri = qemu_uri; s_format = format;
                             s_controller = controller } =
-  sprintf "\t%s%s%s"
-    qemu_uri
-    (match format with
-    | None -> ""
-    | Some format -> " (" ^ format ^ ")")
+  sprintf "\t%s (%s)%s"
+    qemu_uri format
     (match controller with
     | None -> ""
     | Some controller -> " [" ^ string_of_controller controller ^ "]")

v2v/types.mli
@@ -100,7 +100,7 @@ and source_firmware =
 and source_disk = {
   s_disk_id : int;                      (** A unique ID for each source disk. *)
   s_qemu_uri : string;                  (** QEMU URI of source disk. *)
-  s_format : string option;             (** Format. *)
+  s_format : string;                    (** Format of source disk. *)
   s_controller : s_controller option;   (** Controller, eg. IDE, SCSI. *)
 }
 (** A source disk. *)

commit 59dc3293a9b4fdf11da8571c50e3e5badbb511c2

v2v: Cope with libvirt vpx/esx driver which does not set format
See discussion in this bug:
https://bugzilla.redhat.com/show_bug.cgi?id=2026199

Fixes: commit 40cfe6da0861ca6360f670e254c71ed923a0402f

v2v/parse_libvirt_xml.ml
@@ -267,7 +267,11 @@ let parse_libvirt_xml ?bandwidth ?conn xml =
         | Some "aio" -> "raw" (* Xen wierdness *)
         | Some format -> format
         | None ->
-           error (f_"<disk><driver type=\"format\"> attribute is missing from the libvirt XML") in
+           (* Some libvirt drivers don't set the format.  Typically
+            * this is the vpx/esx driver (see RHBZ#2026199).  We
+            * can assume "raw", as it will be overwritten later.
+            *)
+           "raw" in
 
       (* The <disk type='...'> attribute may be 'block', 'file',
        * 'network' or 'volume'.  We ignore any other types.
@@ -338,7 +342,7 @@ let parse_libvirt_xml ?bandwidth ?conn xml =
             match xpath_string "/volume/target/format/@type" with
             | Some format -> format
             | None ->
-               error (f_"<volume><target>.<format type=\"format\"> attribute is missing from the libvirt XML of volume %s") vol in
+               error (f_"<volume><target><format type=\"format\"> attribute is missing from the libvirt XML of volume %s") vol in
 
           (match xpath_string "/volume/@type" with
           | None | Some "file" ->