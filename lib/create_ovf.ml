libguestfs/virt-v2v

commit 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f

v2v: Modular virt-v2v
Split virt-v2v into several cooperating helper programs.  Use disk
image pipelines on both the input and output sides even when accessing
local files.  Expose the NBD sockets.  Use nbdcopy for the copy step.
Some features have been removed and we intend to add those back later
(see TODO file).

For the original plan to split virt-v2v, see:
https://listman.redhat.com/archives/libguestfs/2020-November/msg00022.html

Thanks: Ming Xie, Tingting Zheng, Nir Soffer, Eric Blake, Martin Kletzander

This change is made up of many separate commits done during
development.  The history of those commit messages is preserved below,
but the individual commits do not make too much sense so they have
been squashed into a single large change.

v2v: Move library-ish parts of virt-v2v into lib/ subdirectory

In preparation for splitting virt-v2v, moving library-ish parts of the
code that we wish to reuse in the new helpers into the lib/
subdirectory.

This is neutral code refactoring.

lib: Define format for metadata

In a previous iteration of the virt-v2v split I proposed using an open
format for metadata such as XML, and actually implemented much of it.

However to keep this change simple, and because no one except us is
supposed to be generating or consuming this metadata, this commit
replaces the open format with a simple OCaml serialization of an
opaque version string + the struct (eg. Types.sources).  The opaque
version string is there to ensure binary compatibility between the
helpers and to discourage people from trying to write or consume the
metadata.

Note: The metadata is not ABI and will change arbitrarily between
releases.  If you need to write or consume the metadata it's best to
talk to us about what you're trying to do.

inputs: Create helper-v2v-input-disk

As part of splitting up virt-v2v create input helpers.  This is the
first and simplest input helper which implements the “virt-v2v -i disk”

lib/create_ovf.mli
@@ -1,0 +1,66 @@
+(* virt-v2v
+ * Copyright (C) 2009-2020 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+type ovf_flavour =
+  | OVirt
+  | RHVExportStorageDomain
+
+(** The string representation of available OVF flavours. *)
+val ovf_flavours : string list
+
+(** Convert from a string to the corresponding OVF flavour.
+
+    Throw [Invalid_argument] if the string does not match any
+    valid flavour. *)
+val ovf_flavour_of_string : string -> ovf_flavour
+
+(** Convert an OVF flavour to its string representation. *)
+val ovf_flavour_to_string : ovf_flavour -> string
+
+(** Create OVF and related files for RHV.
+
+    The format for RHV export storage domain is described in:
+    http://resources.ovirt.org/old-site-files/Ovirt_ovf_format.odt
+
+    The format understood by oVirt has no known documentation.
+
+    OVF isn't a real standard, so it's likely that if we ever had to
+    create OVF for another target management system then we would need
+    to heavily modify or even duplicate this code. *)
+
+val create_ovf : Types.source -> Types.inspect ->
+                 Types.target_meta -> int64 list ->
+                 Types.output_allocation -> string -> string -> string list ->
+                 string list -> string ->  ovf_flavour -> DOM.doc
+(** Create the OVF file.
+
+    Actually a {!DOM} document is created, not a file.  It can be written
+    to the desired output location using {!DOM.doc_to_chan}. *)
+
+val create_meta_files : Types.output_allocation -> string -> string -> string list -> int64 list -> string list
+(** Create the .meta file associated with each target.
+
+    Note this does not write them, since output_rhv has to do a
+    permissions dance when writing files.  Instead the contents of each
+    file is returned (one per target), and they must be written to
+    [target_file ^ ".meta"]. *)
+
+(**/**)
+
+(* For use by v2v_unit_tests only. *)
+val get_ostype : Types.inspect -> string

tests/test-v2v-o-rhv.ovf.expected
@@ -1,8 +1,8 @@
 <?xml version='1.0' encoding='utf-8'?>
 <ovf:Envelope xmlns:rasd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData' xmlns:vssd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:ovf='http://schemas.dmtf.org/ovf/envelope/1/' xmlns:ovirt='http://www.ovirt.org/ovf' ovf:version='0.9'>
-  <!-- generated by virt-v2v -->
+  <!-- generated by helper-v2v-output -->
   <References>
-    <File ovf:href='#DISK_ID#/#VOL_ID#' ovf:id='#VOL_ID#' ovf:description='generated by virt-v2v' ovf:size='#SIZE#'/>
+    <File ovf:href='#DISK_ID#/#VOL_ID#' ovf:id='#VOL_ID#' ovf:description='generated by helper-v2v-output'/>
   </References>
   <Section xsi:type='ovf:NetworkSection_Type'>
     <Info>List of networks</Info>
@@ -10,13 +10,13 @@
   </Section>
   <Section xsi:type='ovf:DiskSection_Type'>
     <Info>List of Virtual Disks</Info>
-    <Disk ovf:diskId='#VOL_ID#' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='#DISK_ID#/#VOL_ID#' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='RAW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True' ovf:actual_size='1'/>
+    <Disk ovf:diskId='#VOL_ID#' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='#DISK_ID#/#VOL_ID#' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='RAW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True'/>
   </Section>
   <Content ovf:id='out' xsi:type='ovf:VirtualSystem_Type'>
     <Name>windows</Name>
     <TemplateId>00000000-0000-0000-0000-000000000000</TemplateId>
     <TemplateName>Blank</TemplateName>
-    <Description>generated by virt-v2v</Description>
+    <Description>generated by helper-v2v-output</Description>
     <Domain/>
     <CreationDate>#DATE#</CreationDate>
     <IsInitilized>True</IsInitilized>

tests/test-v2v-o-vdsm-options.ovf.expected
@@ -1,8 +1,8 @@
 <?xml version='1.0' encoding='utf-8'?>
 <ovf:Envelope xmlns:rasd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData' xmlns:vssd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:ovf='http://schemas.dmtf.org/ovf/envelope/1/' xmlns:ovirt='http://www.ovirt.org/ovf' ovf:version='0.9'>
-  <!-- generated by virt-v2v -->
+  <!-- generated by helper-v2v-output -->
   <References>
-    <File ovf:href='VOL' ovf:id='VOL' ovf:description='generated by virt-v2v' ovf:size='#SIZE#'/>
+    <File ovf:href='VOL' ovf:id='VOL' ovf:description='generated by helper-v2v-output'/>
   </References>
   <NetworkSection>
     <Info>List of networks</Info>
@@ -10,13 +10,13 @@
   </NetworkSection>
   <DiskSection>
     <Info>List of Virtual Disks</Info>
-    <Disk ovf:diskId='IMAGE' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='VOL' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='COW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True' ovf:actual_size='1'/>
+    <Disk ovf:diskId='IMAGE' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='VOL' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='COW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True'/>
   </DiskSection>
   <VirtualSystem ovf:id='VM'>
     <Name>windows</Name>
     <TemplateId>00000000-0000-0000-0000-000000000000</TemplateId>
     <TemplateName>Blank</TemplateName>
-    <Description>generated by virt-v2v</Description>
+    <Description>generated by helper-v2v-output</Description>
     <Domain/>
     <CreationDate>#DATE#</CreationDate>
     <IsInitilized>True</IsInitilized>

v2v/create_ovf.mli
@@ -1,66 +1,0 @@
-(* virt-v2v
- * Copyright (C) 2009-2020 Red Hat Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *)
-
-type ovf_flavour =
-  | OVirt
-  | RHVExportStorageDomain
-
-(** The string representation of available OVF flavours. *)
-val ovf_flavours : string list
-
-(** Convert from a string to the corresponding OVF flavour.
-
-    Throw [Invalid_argument] if the string does not match any
-    valid flavour. *)
-val ovf_flavour_of_string : string -> ovf_flavour
-
-(** Convert an OVF flavour to its string representation. *)
-val ovf_flavour_to_string : ovf_flavour -> string
-
-(** Create OVF and related files for RHV.
-
-    The format for RHV export storage domain is described in:
-    http://resources.ovirt.org/old-site-files/Ovirt_ovf_format.odt
-
-    The format understood by oVirt has no known documentation.
-
-    OVF isn't a real standard, so it's likely that if we ever had to
-    create OVF for another target management system then we would need
-    to heavily modify or even duplicate this code. *)
-
-val create_ovf : Types.source -> Types.inspect ->
-                 Types.target_meta -> Types.target list ->
-                 Types.output_allocation -> string -> string list ->
-                 string list -> string ->  ovf_flavour -> DOM.doc
-(** Create the OVF file.
-
-    Actually a {!DOM} document is created, not a file.  It can be written
-    to the desired output location using {!DOM.doc_to_chan}. *)
-
-val create_meta_files : Types.output_allocation -> string -> string list -> (string * Types.overlay) list -> string list
-(** Create the .meta file associated with each target.
-
-    Note this does not write them, since output_rhv has to do a
-    permissions dance when writing files.  Instead the contents of each
-    file is returned (one per target), and they must be written to
-    [target_file ^ ".meta"]. *)
-
-(**/**)
-
-(* For use by v2v_unit_tests only. *)
-val get_ostype : Types.inspect -> string

commit a2a4f7a09996a5e66d027d0d9692e083eb0a8128

lib/create_ovf: populate "actual size" attributes again
Commit 255722cbf39a ("v2v: Modular virt-v2v", 2021-09-07) removed the
following attributes from the OVF output:

- ovf:Envelope/References/File/@ovf:size
- ovf:Envelope/Section[@xsi:type='ovf:DiskSection_Type']/Disk/@ovf:actual_size

Unfortunately, ovirt-engine considers the second one mandatory; without
it, ovirt-engine refuses to import the OVF.

Restore both attributes, using the utility functions added to the Utils
module previously.

(If we do not have the information necessary to fill in @ovf:actual_size,
we still have to generate the attribute, only with empty contents.
Ovirt-engine does cope with that.)

Fixes: 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f
Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2027598
Signed-off-by: Laszlo Ersek <lersek@redhat.com>
Message-Id: <20211210113537.10907-4-lersek@redhat.com>
Acked-by: Richard W.M. Jones <rjones@redhat.com>

lib/create_ovf.ml
@@ -531,7 +531,7 @@ let rec create_ovf source inspect
           { output_name; guestcaps; target_firmware; target_nics }
           sizes
           output_alloc output_format
-          sd_uuid image_uuids vol_uuids vm_uuid ovf_flavour =
+          sd_uuid image_uuids vol_uuids dir vm_uuid ovf_flavour =
   assert (List.length sizes = List.length vol_uuids);
 
   let memsize_mb = source.s_memory /^ 1024L /^ 1024L in
@@ -745,7 +745,7 @@ let rec create_ovf source inspect
 
   (* Add disks to the OVF XML. *)
   add_disks sizes guestcaps output_alloc output_format
-    sd_uuid image_uuids vol_uuids ovf_flavour ovf;
+    sd_uuid image_uuids vol_uuids dir ovf_flavour ovf;
 
   (* Old virt-v2v ignored removable media. XXX *)
 
@@ -791,7 +791,7 @@ and get_flavoured_section ovf ovirt_path rhv_path rhv_path_attr = function
 
 (* This modifies the OVF DOM, adding a section for each disk. *)
 and add_disks sizes guestcaps output_alloc output_format
-    sd_uuid image_uuids vol_uuids ovf_flavour ovf =
+    sd_uuid image_uuids vol_uuids dir ovf_flavour ovf =
   let references =
     let nodes = path_to_nodes ovf ["ovf:Envelope"; "References"] in
     match nodes with
@@ -839,13 +839,7 @@ and add_disks sizes guestcaps output_alloc output_format
         b /^ 1073741824L
       in
       let size_gb = bytes_to_gb size in
-      (* virt-v2v 1.4x used to try to collect the actual size of the
-       * sparse disk.  It would be possible to get this information
-       * accurately now by reading the extent map of the output disk
-       * (this function is called during finalization), but we don't
-       * yet do that. XXX
-       *)
-      let actual_size_gb = None in
+      let actual_size = get_disk_allocated ~dir ~disknr:i in
 
       let format_for_rhv =
         match output_format with
@@ -867,13 +861,11 @@ and add_disks sizes guestcaps output_alloc output_format
           "ovf:id", vol_uuid;
           "ovf:description", generated_by;
         ] in
-        (* See note above about actual_size_gb
-        (match t.target_overlay.ov_stats.target_actual_size with
+        (match actual_size with
          | None -> ()
          | Some actual_size ->
             List.push_back attrs ("ovf:size", Int64.to_string actual_size)
         );
-        *)
         e "File" !attrs [] in
       append_child disk references;
 
@@ -899,11 +891,16 @@ and add_disks sizes guestcaps output_alloc output_format
           "ovf:disk-type", "System"; (* RHBZ#744538 *)
           "ovf:boot", if is_bootable_drive then "True" else "False";
         ] in
-        (match actual_size_gb with
-         | None -> ()
-         | Some actual_size_gb ->
-            List.push_back attrs ("ovf:actual_size", Int64.to_string actual_size_gb)
-        );
+        (* Ovirt-engine considers the "ovf:actual_size" attribute mandatory. If
+         * we don't know the actual size, we must create the attribute with
+         * empty contents.
+         *)
+        List.push_back attrs
+          ("ovf:actual_size",
+           match actual_size with
+            | None -> ""
+            | Some actual_size -> Int64.to_string (bytes_to_gb actual_size)
+          );
         e "Disk" !attrs [] in
       append_child disk disk_section;
 

lib/create_ovf.mli
@@ -46,7 +46,7 @@ val ovf_flavour_to_string : ovf_flavour -> string
 val create_ovf : Types.source -> Types.inspect ->
                  Types.target_meta -> int64 list ->
                  Types.output_allocation -> string -> string -> string list ->
-                 string list -> string ->  ovf_flavour -> DOM.doc
+                 string list -> string -> string -> ovf_flavour -> DOM.doc
 (** Create the OVF file.
 
     Actually a {!DOM} document is created, not a file.  It can be written

output/output_rhv.ml
@@ -183,7 +183,7 @@ and rhv_finalize dir source inspect target_meta
   (* Create the metadata. *)
   let ovf =
     Create_ovf.create_ovf source inspect target_meta sizes
-      output_alloc output_format esd_uuid image_uuids vol_uuids vm_uuid
+      output_alloc output_format esd_uuid image_uuids vol_uuids dir vm_uuid
       Create_ovf.RHVExportStorageDomain in
 
   (* Write it to the metadata file. *)

output/output_rhv_upload.ml
@@ -419,7 +419,7 @@ and rhv_upload_finalize dir source inspect target_meta
   (* Create the metadata. *)
   let ovf =
     Create_ovf.create_ovf source inspect target_meta disk_sizes
-      Sparse output_format sd_uuid disk_uuids vol_uuids vm_uuid
+      Sparse output_format sd_uuid disk_uuids vol_uuids dir vm_uuid
       OVirt in
   let ovf = DOM.doc_to_string ovf in
 

output/output_vdsm.ml
@@ -201,6 +201,7 @@ and vdsm_finalize dir source inspect target_meta
               output_alloc output_format dd_uuid
               image_uuids
               vol_uuids
+              dir
               vm_uuid
               ovf_flavour in
 

tests/test-v2v-o-rhv.ovf.expected
@@ -2,7 +2,7 @@
 <ovf:Envelope xmlns:rasd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData' xmlns:vssd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:ovf='http://schemas.dmtf.org/ovf/envelope/1/' xmlns:ovirt='http://www.ovirt.org/ovf' ovf:version='0.9'>
   <!-- generated by virt-v2v -->
   <References>
-    <File ovf:href='#DISK_ID#/#VOL_ID#' ovf:id='#VOL_ID#' ovf:description='generated by virt-v2v'/>
+    <File ovf:href='#DISK_ID#/#VOL_ID#' ovf:id='#VOL_ID#' ovf:description='generated by virt-v2v' ovf:size='#SIZE#'/>
   </References>
   <Section xsi:type='ovf:NetworkSection_Type'>
     <Info>List of networks</Info>
@@ -10,7 +10,7 @@
   </Section>
   <Section xsi:type='ovf:DiskSection_Type'>
     <Info>List of Virtual Disks</Info>
-    <Disk ovf:diskId='#VOL_ID#' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='#DISK_ID#/#VOL_ID#' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='RAW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True'/>
+    <Disk ovf:diskId='#VOL_ID#' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='#DISK_ID#/#VOL_ID#' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='RAW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True' ovf:actual_size='1'/>
   </Section>
   <Content ovf:id='out' xsi:type='ovf:VirtualSystem_Type'>
     <Name>windows</Name>

tests/test-v2v-o-vdsm-options.ovf.expected
@@ -2,7 +2,7 @@
 <ovf:Envelope xmlns:rasd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData' xmlns:vssd='http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:ovf='http://schemas.dmtf.org/ovf/envelope/1/' xmlns:ovirt='http://www.ovirt.org/ovf' ovf:version='0.9'>
   <!-- generated by virt-v2v -->
   <References>
-    <File ovf:href='VOL' ovf:id='VOL' ovf:description='generated by virt-v2v'/>
+    <File ovf:href='VOL' ovf:id='VOL' ovf:description='generated by virt-v2v' ovf:size='#SIZE#'/>
   </References>
   <NetworkSection>
     <Info>List of networks</Info>
@@ -10,7 +10,7 @@
   </NetworkSection>
   <DiskSection>
     <Info>List of Virtual Disks</Info>
-    <Disk ovf:diskId='IMAGE' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='VOL' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='COW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True'/>
+    <Disk ovf:diskId='IMAGE' ovf:size='1' ovf:capacity='536870912' ovf:fileRef='VOL' ovf:parentRef='' ovf:vm_snapshot_id='#UUID#' ovf:volume-format='COW' ovf:volume-type='Sparse' ovf:format='http://en.wikipedia.org/wiki/Byte' ovf:disk-interface='VirtIO' ovf:disk-type='System' ovf:boot='True' ovf:actual_size='1'/>
   </DiskSection>
   <VirtualSystem ovf:id='VM'>
     <Name>windows</Name>