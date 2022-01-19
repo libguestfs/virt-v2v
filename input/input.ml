libguestfs/virt-v2v

commit 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f

v2v: Modular virt-v2v
Split virt-v2v into several cooperating helper programs.  Use disk
image pipelines on both the input and output sides even when accessing
local files.  Expose the NBD sockets.  Use nbdcopy for the copy step.
Some features have been removed and we intend to add those back later
(see TODO file).

For the original plan to split virt-v2v, see:
https://listman.redhat.com/archives/libguestfs/2020-November/msg00022.html

Thanks: Ming Xie, Tingting Zheng, Nir Soffer, Eric Blake, Martin Kletzander

This change is made up of many separate commits done during
development.  The history of those commit messages is preserved below,
but the individual commits do not make too much sense so they have
been squashed into a single large change.

v2v: Move library-ish parts of virt-v2v into lib/ subdirectory

In preparation for splitting virt-v2v, moving library-ish parts of the
code that we wish to reuse in the new helpers into the lib/
subdirectory.

This is neutral code refactoring.

lib: Define format for metadata

In a previous iteration of the virt-v2v split I proposed using an open
format for metadata such as XML, and actually implemented much of it.

However to keep this change simple, and because no one except us is
supposed to be generating or consuming this metadata, this commit
replaces the open format with a simple OCaml serialization of an
opaque version string + the struct (eg. Types.sources).  The opaque
version string is there to ensure binary compatibility between the
helpers and to discourage people from trying to write or consume the
metadata.

Note: The metadata is not ABI and will change arbitrarily between
releases.  If you need to write or consume the metadata it's best to
talk to us about what you're trying to do.

inputs: Create helper-v2v-input-disk

As part of splitting up virt-v2v create input helpers.  This is the
first and simplest input helper which implements the “virt-v2v -i disk”

input/input.mli
@@ -1,0 +1,17 @@
+(* helper-v2v-input
+ * Copyright (C) 2009-2021 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)

output/output.mli
@@ -1,0 +1,19 @@
+(* helper-v2v-output
+ * Copyright (C) 2009-2021 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+(* empty *)

tests/test-v2v-i-vmx-1.expected
@@ -1,4 +1,3 @@
-
 Source guest information (--print-source option):
 
     source name: BZ1308535_21disks
@@ -15,27 +14,27 @@ hypervisor type: vmware
           video: vmvga
           sound: 
 disks:
-	/BZ1308535_21disks.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_1.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_2.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_3.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_4.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_5.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_6.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_7.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_8.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_9.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_10.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_11.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_12.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_13.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_14.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_15.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_16.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_17.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_18.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_19.vmdk (vmdk) [scsi]
-	/BZ1308535_21disks_20.vmdk (vmdk) [scsi]
+	0 [scsi]
+	1 [scsi]
+	2 [scsi]
+	3 [scsi]
+	4 [scsi]
+	5 [scsi]
+	6 [scsi]
+	7 [scsi]
+	8 [scsi]
+	9 [scsi]
+	10 [scsi]
+	11 [scsi]
+	12 [scsi]
+	13 [scsi]
+	14 [scsi]
+	15 [scsi]
+	16 [scsi]
+	17 [scsi]
+	18 [scsi]
+	19 [scsi]
+	20 [scsi]
 removable media:
 	CD-ROM [ide] in slot 2
 NICs:

tests/test-v2v-i-vmx-2.expected
@@ -1,4 +1,3 @@
-
 Source guest information (--print-source option):
 
     source name: Fedora 20
@@ -15,7 +14,7 @@ hypervisor type: vmware
           video: vmvga
           sound: 
 disks:
-	/Fedora 20.vmdk (vmdk) [scsi]
+	0 [scsi]
 removable media:
 
 NICs:

tests/test-v2v-i-vmx-3.expected
@@ -1,4 +1,3 @@
-
 Source guest information (--print-source option):
 
     source name: RHEL 7.1 UEFI
@@ -15,7 +14,7 @@ hypervisor type: vmware
           video: vmvga
           sound: 
 disks:
-	/RHEL 7.1 UEFI.vmdk (vmdk) [scsi]
+	0 [scsi]
 removable media:
 	CD-ROM [ide] in slot 2
 NICs:

commit 924aa8b70a64e9b076f8f56328a2b49650ab713c

Restore message about setting up the input and output
Old virt-v2v would print a summary of the input and output options
before connecting to the input/output, looking something like this:

  [   0.2] Opening the source -i libvirt -ic [etc]

This gave reassurance that virt-v2v was doing something in the case
where the source was slow or unreachable.  In particular if you use
-i libvirt with a vCenter URL, and the URL is wrong, libvirt hangs for
a few minutes without printing anything.

Modular virt-v2v rearranged things so the connecting phase was silent,
which meant that in the case above virt-v2v appeared to hang for a few
minutes printing nothing at all.

This change adds to_string functions to all the input and output
methods and uses them to print a message like:

  [   0.0] Setting up the source: -i libvirt -ic [etc]

The hang still happens, but at least it's now clear where it's hanging.

Note the old "Opening the source" message now refers to libguestfs
connecting to the NBD source disk pipeline.

Typical full output looks like this:

  $ virt-v2v -i disk /var/tmp/fedora-35.img -o disk -os /var/tmp/out
  [   0.0] Setting up the source: -i disk /var/tmp/fedora-35.img
  [   1.1] Opening the source
  [   5.9] Inspecting the source
  [  11.5] Checking for sufficient free disk space in the guest
  [  11.5] Converting Fedora Linux 35 (Thirty Five) to run on KVM
  virt-v2v: warning: /files/boot/grub2/device.map/hd0 references unknown
  device "vda".  You may have to fix this entry manually after conversion.
  virt-v2v: This guest has virtio drivers installed.
  [  57.4] Mapping filesystem data to avoid copying unused and blank areas
  [  61.0] Closing the overlay
  [  61.7] Assigning disks to buses
  [  61.7] Checking if the guest needs BIOS or UEFI to boot
  [  61.7] Setting up the destination: -o disk -os /var/tmp/out
  [  62.8] Copying disk 1/1
  █ 100% [****************************************]
  [  81.7] Creating output metadata
  [  81.7] Finishing off

Reported-by: Xiaodai Wang
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2041886
Fixes: commit 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f

input/input.ml
@@ -26,6 +26,7 @@ type options = {
 }
 
 module type INPUT = sig
+  val to_string : options -> string list -> string
   val setup : string -> options -> string list -> Types.source
   val query_input_options : unit -> unit
 end

input/input.mli
@@ -26,6 +26,10 @@ type options = {
 }
 
 module type INPUT = sig
+  val to_string : options -> string list -> string
+  (** [to_string options args] converts the source to a printable
+      string (for messages). *)
+
   val setup : string -> options -> string list -> Types.source
   (** [setup dir options args]
 

input/input_disk.ml
@@ -142,6 +142,8 @@ and detect_local_input_format { input_format } filenames =
      get_format formats
 
 module Disk = struct
+  let to_string options args = String.concat " " ("-i disk" :: args)
+
   let setup dir options args =
     disk_source dir options args
 

input/input_libvirt.ml
@@ -129,6 +129,14 @@ and libvirt_xml_source _ args =
   source, disks
 
 module Libvirt_ = struct
+  let to_string options args =
+    let xs = "-i libvirt" :: args in
+    let xs =
+      match options.input_conn with
+      | Some ic -> ("-ic " ^ ic) :: xs
+      | None -> xs in
+    String.concat " " xs
+
   let setup dir options args =
     let source, data = libvirt_source options args in
     libvirt_servers dir data;
@@ -139,6 +147,8 @@ module Libvirt_ = struct
 end
 
 module LibvirtXML = struct
+  let to_string options args = String.concat " " ("-i libvirtxml" :: args)
+
   let setup dir options args =
     let source, data = libvirt_xml_source options args in
     libvirt_servers dir data;

input/input_ova.ml
@@ -229,6 +229,8 @@ and error_missing_href href =
   error (f_"-i ova: OVF references file ‘%s’ which was not found in the OVA archive") href
 
 module OVA = struct
+  let to_string options args = String.concat " " ("-i ova" :: args)
+
   let setup dir options args =
     ova_source dir options args
 

input/input_vcenter_https.ml
@@ -117,6 +117,15 @@ let rec vcenter_https_source dir options args =
   source
 
 module VCenterHTTPS = struct
+  let to_string options args =
+    let xs = args in
+    let xs =
+      match options.input_conn with
+      | Some ic -> ("-ic " ^ ic) :: xs
+      | None -> xs in
+    let xs = "-i libvirt" :: xs in
+    String.concat " " xs
+
   let setup dir options args =
     vcenter_https_source dir options args
 

input/input_vddk.ml
@@ -193,6 +193,15 @@ and vddk_source dir options args =
   source
 
 module VDDK = struct
+  let to_string options args =
+    let xs = "-it vddk" :: args in
+    let xs =
+      match options.input_conn with
+      | Some ic -> ("-ic " ^ ic) :: xs
+      | None -> xs in
+    let xs = "-i libvirt" :: xs in
+    String.concat " " xs
+
   let setup dir options args =
     vddk_source dir options args
 

input/input_vmx.ml
@@ -118,6 +118,8 @@ and absolute_path_from_other_file other_filename filename =
   else (Filename.dirname (absolute_path other_filename)) // filename
 
 module VMX = struct
+  let to_string options args = String.concat " " ("-i vmx" :: args)
+
   let setup dir options args =
     vmx_source dir options args
 

input/input_xen_ssh.ml
@@ -112,6 +112,15 @@ let rec xen_ssh_source dir options args =
   source
 
 module XenSSH = struct
+  let to_string options args =
+    let xs = args in
+    let xs =
+      match options.input_conn with
+      | Some ic -> ("-ic " ^ ic) :: xs
+      | None -> xs in
+    let xs = "-i libvirt" :: xs in
+    String.concat " " xs
+
   let setup dir options args =
     xen_ssh_source dir options args
 

output/output.ml
@@ -38,6 +38,7 @@ type options = {
 
 module type OUTPUT = sig
   type t
+  val to_string : options -> string
   val setup : string -> options -> Types.source -> t
   val finalize : string -> options ->
                  Types.source -> Types.inspect -> Types.target_meta ->

output/output.mli
@@ -30,6 +30,10 @@ module type OUTPUT = sig
   type t
   (** Opaque data used by the output mode. *)
 
+  val to_string : options -> string
+  (** [to_string options] converts the destination to a printable
+      string (for messages). *)
+
   val setup : string -> options -> Types.source -> t
   (** [setup dir options source]
 

output/output_disk.ml
@@ -96,6 +96,12 @@ and disk_finalize dir source inspect target_meta
 module Disk = struct
   type t = unit
 
+  let to_string options =
+    "-o disk" ^
+      match options.output_storage with
+      | Some os -> " -os " ^ os
+      | None -> ""
+
   let setup dir options source =
     if options.output_options <> [] then
       error (f_"no -oo (output options) are allowed here");

output/output_glance.ml
@@ -122,6 +122,8 @@ and glance_finalize dir source inspect target_meta output_format tmpdir =
 module Glance = struct
   type t = string
 
+  let to_string options = "-o glance"
+
   let setup dir options source =
     if options.output_options <> [] then
       error (f_"no -oo (output options) are allowed here");

output/output_json.ml
@@ -134,6 +134,12 @@ and json_path os output_name json_disks_pattern i =
 module Json = struct
   type t = unit
 
+  let to_string options =
+    "-o json" ^
+      match options.output_storage with
+      | Some os -> " -os " ^ os
+      | None -> ""
+
   let setup dir options source =
     let data = json_parse_options options in
     let output_name = get_output_name options source in

output/output_libvirt.ml
@@ -198,6 +198,12 @@ and target_features_of_capabilities_doc doc arch =
 module Libvirt_ = struct
   type t = string * string
 
+  let to_string options =
+    "-o libvirt" ^
+      match options.output_storage with
+      | Some os -> " -os " ^ os
+      | None -> ""
+
   let setup dir options source =
     if options.output_options <> [] then
       error (f_"no -oo (output options) are allowed here");

output/output_null.ml
@@ -76,6 +76,8 @@ and null_servers dir disks output_name =
 module Null = struct
   type t = unit
 
+  let to_string options = "-o null"
+
   let setup dir options source =
     if options.output_options <> [] then
       error (f_"no -oo (output options) are allowed here");

output/output_openstack.ml
@@ -462,6 +462,22 @@ and iso_time =
 module Openstack = struct
   type t = string list
 
+  let to_string options =
+    (* Try to get the server-id since it seems useful to display
+     * that for diagnostics.
+     *)
+    let server_id = ref None in
+    List.iter (
+      function
+      | "server-id", v -> server_id := Some v
+      | _ -> ()
+    ) options.output_options;
+
+    "-o openstack" ^
+      (match !server_id with
+       | None -> ""
+       | Some id -> sprintf " -oo server-id=%s" id)
+
   let setup dir options source =
     let data = openstack_parse_options options in
     let output_name = get_output_name options source in

output/output_qemu.ml
@@ -315,6 +315,12 @@ and qemu_finalize dir source inspect target_meta
 module QEMU = struct
   type t = unit
 
+  let to_string options =
+    "-o qemu" ^
+      match options.output_storage with
+      | Some os -> " -os " ^ os
+      | None -> ""
+
   let setup dir options source =
     let data = qemu_parse_options options in
     let output_name = get_output_name options source in

output/output_rhv.ml
@@ -266,6 +266,8 @@ and check_storage_domain domain_class os mp =
 module RHV = struct
   type t = string * string * string * string list * string list * int64 list
 
+  let to_string options = "-o rhv"
+
   let setup dir options source =
     if options.output_options <> [] then
       error (f_"no -oo (output options) are allowed here");

output/output_rhv_upload.ml
@@ -444,6 +444,15 @@ module RHVUpload = struct
            JSON.field list * string option * string option *
            string option * string
 
+  let to_string options =
+    "-o rhv-upload" ^
+      (match options.output_conn with
+       | Some oc -> " -oc " ^ oc
+       | None -> "") ^
+      (match options.output_storage with
+       | Some os -> " -os " ^ os
+       | None -> "")
+
   let setup dir options source =
     let data = rhv_upload_parse_options options in
     let output_name = get_output_name options source in

output/output_vdsm.ml
@@ -212,6 +212,8 @@ and vdsm_finalize dir source inspect target_meta
 module VDSM = struct
   type t = string * string * int64 list
 
+  let to_string options = "-o vdsm"
+
   let setup dir options source =
     let data = vdsm_parse_options options in
     let output_name = get_output_name options source in

v2v/v2v.ml
@@ -532,6 +532,8 @@ read the man page virt-v2v(1).
   } in
 
   (* Start the input module (runs an NBD server in the background). *)
+  message (f_"Setting up the source: %s")
+    (Input_module.to_string input_options args);
   let source = Input_module.setup tmpdir input_options args in
 
   (* If --print-source then print the source metadata and exit. *)
@@ -548,6 +550,8 @@ read the man page virt-v2v(1).
   unlink (tmpdir // "convert");
 
   (* Start the output module (runs an NBD server in the background). *)
+  message (f_"Setting up the destination: %s")
+    (Output_module.to_string output_options);
   let output_t = Output_module.setup tmpdir output_options source in
 
   (* Debug the v2vdir. *)

tests/test-v2v-i-vmx-1.expected
@@ -1,3 +1,4 @@
+
 Source guest information (--print-source option):
 
     source name: BZ1308535_21disks

tests/test-v2v-i-vmx-2.expected
@@ -1,3 +1,4 @@
+
 Source guest information (--print-source option):
 
     source name: Fedora 20

tests/test-v2v-i-vmx-3.expected
@@ -1,3 +1,4 @@
+
 Source guest information (--print-source option):
 
     source name: RHEL 7.1 UEFI

tests/test-v2v-i-vmx-4.expected
@@ -1,3 +1,4 @@
+
 Source guest information (--print-source option):
 
     source name: Windows 7 x64

tests/test-v2v-i-vmx-5.expected
@@ -1,3 +1,4 @@
+
 Source guest information (--print-source option):
 
     source name: MSEdge - Win10_preview

tests/test-v2v-i-vmx.sh
@@ -29,6 +29,8 @@ set -x
 
 skip_if_skipped
 
+export LANG=C
+
 export VIRT_TOOLS_DATA_DIR="$srcdir/../test-data/fake-virt-tools"
 export VIRTIO_WIN="$srcdir/../test-data/fake-virtio-win"
 
@@ -52,6 +54,7 @@ for i in 1 2 3 4 5; do
     # Normalize the print-source output.
     mv test-v2v-i-vmx-$i.actual test-v2v-i-vmx-$i.actual.old
     sed \
+        -e "s,.*Setting up the source.*,," \
         -e "s,.*Opening the source.*,," \
         -e "s,$(pwd),," \
         < test-v2v-i-vmx-$i.actual.old > test-v2v-i-vmx-$i.actual

tests/test-v2v-print-source.sh
@@ -27,6 +27,8 @@ set -x
 skip_if_skipped
 requires test -f ../test-data/phony-guests/windows.img
 
+export LANG=C
+
 d=test-v2v-print-source.d
 rm -rf $d
 cleanup_fn rm -r $d
@@ -39,6 +41,7 @@ $VG virt-v2v --debug-gc \
 
 mv $d/output $d/output.orig
 < $d/output.orig \
+grep -v 'Setting up the source' |
 grep -v 'Opening the source' |
 grep -v 'Source guest information' |
 sed -e 's,/.*/windows.img,windows.img,' |