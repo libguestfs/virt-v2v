libguestfs/virt-v2v

commit 4f6b143c1cb32a29cbd3ff04635ba220e5db82ed

output: -o libvirt, qemu: Use correct device name for vsock
According to https://libvirt.org/formatdomain.html#vsock it's <vsock/>
not <viosock/>.  There is also no model="none", we just omit it if not
present.

According to https://wiki.qemu.org/Features/VirtioVsock the qemu
parameter is -device vhost-vsock-pci.

Fixes: 05f780c16f0135c657615520c2245b42de1efc3e
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

output/output_qemu.ml
@@ -277,9 +277,7 @@ and qemu_finalize dir source inspect target_meta
   if guestcaps.gcaps_isa_pvpanic then
     arg_list "-device" ["pvpanic"; "ioport=0x505"];
   if guestcaps.gcaps_virtio_socket then
-    arg "-viosock" "virtio"
-  else
-    arg "-viosock" "none";
+    arg "-device" "vhost-vsock-pci";
 
   (* Add a serial console to Linux guests. *)
   if inspect.i_type = "linux" then

output/create_libvirt_xml.ml
@@ -511,12 +511,8 @@ let create_libvirt_xml ?pool source inspect
         e "address" ["type", "isa"; "iobase", "0x505"] []
       ]
     );
-  List.push_back devices (
-    e "viosock"
-      ["model",
-        if guestcaps.gcaps_virtio_socket then virtio_model else "none"]
-       []
-  );
+  if guestcaps.gcaps_virtio_socket then
+    List.push_back devices (e "vsock" ["model", virtio_model] []);
 
   (* Standard devices added to every guest. *)
   List.push_back_list devices [

tests/test-v2v-i-ova.xml
@@ -46,7 +46,6 @@
       <backend model='random'>/dev/urandom</backend>
     </rng>
     <memballoon model='virtio-transitional'/>
-    <viosock model='none'/>
     <input type='tablet' bus='usb'/>
     <input type='mouse' bus='ps2'/>
     <console type='pty'/>

commit e3226dd431e7476cae2d0ca54b2332344de2d201

-o qemu: Fix vsock device by choosing a semi-random guest-cid
If a virtio-vsock device was installed or present in the guest kernel
then -o qemu mode would add the parameter -device vhost-vsock-pci to
the qemu command line.  This fails with:

  qemu-system-x86_64: -device vhost-vsock-pci: guest-cid property must be greater than 2

Thanks: Stefano Garzarella
Fixes: commit 4f6b143c1cb32a29cbd3ff04635ba220e5db82ed
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

output/output_qemu.ml
@@ -292,8 +292,30 @@ module QEMU = struct
       arg "-device" "virtio-balloon";
     if guestcaps.gcaps_isa_pvpanic then
       arg_list "-device" ["pvpanic"; "ioport=0x505"];
-    if guestcaps.gcaps_virtio_socket then
-      arg "-device" "vhost-vsock-pci";
+    if guestcaps.gcaps_virtio_socket then (
+      (* qemu requires a free guest CID to be chosen.  If you use libvirt
+       * then it does this by iterating over the CIDs doing
+       * ioctl(fd, VHOST_VSOCK_SET_GUEST_CID, &val) on each one until
+       * it finds a free CID.  See:
+       * https://bugzilla.redhat.com/show_bug.cgi?id=1291851#c6
+       *
+       * As that is essentially impossible to do from the shell script,
+       * instead assign a semi-random one here.  Using the PID of
+       * virt-v2v means that we're most likely to assign an unused
+       * CID, especially with modern Linux which has a very large
+       * PID space.  Note that CID must be [3..UINT32_MAX-1] and
+       * max PID in Linux is 2^22.
+       *)
+      let pid = getpid () in
+      let pid = max 3 pid in
+      (* In OCaml 4.13 we could use this, if we were worried about a
+         future system having very large PIDs:
+         let pid = Int64.( min (max 3L (of_int (getpid ())))
+                               (of_int32 Int32.max_int) ) in
+       *)
+      let guest_cid = sprintf "guest-cid=%d" pid in
+      arg_list "-device" ["vhost-vsock-pci"; guest_cid ]
+    );
 
     (* Add a serial console to Linux guests. *)
     if inspect.i_type = "linux" then