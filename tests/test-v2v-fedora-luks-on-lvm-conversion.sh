e4efe4b7d240b66b1d53fbe5a127f4f5966f6903
Laszlo Ersek <lersek@redhat.com>
2022-06-14 13:27:09 +0200

    tests: add LUKS-on-LVM test
    Port guestfs-tools commit 27da4b0c4991 ("inspector: add LUKS-on-LVM test",
    2022-02-28) to virt-v2v. While at it, account for virt-v2v commit
    fd7cd0c0fd22 ("test-data/phony-guests: Increase size of root filesystem",
    2022-06-08).
    
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20220614112709.12210-4-lersek@redhat.com>
    Acked-by: Richard W.M. Jones <rjones@redhat.com>

---
+++ tests/test-v2v-fedora-luks-on-lvm-conversion.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -
+# libguestfs virt-v2v test script
+# Copyright (C) 2014-2022 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test virt-v2v (Phony) Fedora conversion.
+
+set -e
+
+source ./functions.sh
+set -e
+set -x
+
+skip_if_skipped
+f=../test-data/phony-guests/fedora-luks-on-lvm.img
+requires test -f $f
+
+keys=(--key /dev/VG/Root:key:FEDORA-Root
+      --key /dev/VG/LV1:key:FEDORA-LV1
+      --key /dev/VG/LV2:key:FEDORA-LV2
+      --key /dev/VG/LV3:key:FEDORA-LV3)
+
+$VG virt-v2v --debug-gc -i disk $f -o null "${keys[@]}"
