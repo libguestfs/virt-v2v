libguestfs/virt-v2v

commit 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f

v2v: Modular virt-v2v
Split virt-v2v into several cooperating helper programs.  Use disk
image pipelines on both the input and output sides even when accessing
local files.  Expose the NBD sockets.  Use nbdcopy for the copy step.
Some features have been removed and we intend to add those back later
(see TODO file).

For the original plan to split virt-v2v, see:
https://listman.redhat.com/archives/libguestfs/2020-November/msg00022.html

Thanks: Ming Xie, Tingting Zheng, Nir Soffer, Eric Blake, Martin Kletzander

This change is made up of many separate commits done during
development.  The history of those commit messages is preserved below,
but the individual commits do not make too much sense so they have
been squashed into a single large change.

v2v: Move library-ish parts of virt-v2v into lib/ subdirectory

In preparation for splitting virt-v2v, moving library-ish parts of the
code that we wish to reuse in the new helpers into the lib/
subdirectory.

This is neutral code refactoring.

lib: Define format for metadata

In a previous iteration of the virt-v2v split I proposed using an open
format for metadata such as XML, and actually implemented much of it.

However to keep this change simple, and because no one except us is
supposed to be generating or consuming this metadata, this commit
replaces the open format with a simple OCaml serialization of an
opaque version string + the struct (eg. Types.sources).  The opaque
version string is there to ensure binary compatibility between the
helpers and to discourage people from trying to write or consume the
metadata.

Note: The metadata is not ABI and will change arbitrarily between
releases.  If you need to write or consume the metadata it's best to
talk to us about what you're trying to do.

inputs: Create helper-v2v-input-disk

As part of splitting up virt-v2v create input helpers.  This is the
first and simplest input helper which implements the “virt-v2v -i disk”

TODO
@@ -1,3 +1,26 @@
+virt-v2v --in-place support
+---------------------------
+
+This was temporarily dropped when I modularized virt-v2v.  It is
+intended to add this back, but with some changes.  I would like to get
+rid of the idea of configuring the guest based on the "input" libvirt
+XML, since that was and is a hack.  Instead we should have a new top
+level "virt-v2v-in-place" program (compare v2v/v2v.ml) which would
+parse new command line arguments and use the existing input and
+convert helpers to modify a local disk image.  We might also consider
+writing a new, simple input helper just for in-place which only works
+for local files and does not use the COW filter.
+
+virt-v2v -o disk|qemu -oo compressed
+------------------------------------
+
+This was temporarily dropped when I modularized virt-v2v.  It would
+not be too difficult to add it back.  The following is the qemu-nbd
+command required (to be run as the output helper) which creates a
+compressed qcow2 disk image:
+
+$ qemu-nbd --image-opts driver=compress,file.driver=qcow2,file.file.driver=file,file.file.filename=new.qcow2
+
 virt-v2v -o rhv-upload
 ----------------------
 

v2v/output_libvirt.ml
@@ -1,200 +1,0 @@
-(* virt-v2v
- * Copyright (C) 2009-2020 Red Hat Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *)
-
-open Printf
-
-open Std_utils
-open Tools_utils
-open Common_gettext.Gettext
-
-open Types
-open Utils
-open Xpath_helpers
-open Create_libvirt_xml
-
-let arch_is_sane_or_die =
-  let rex = PCRE.compile ~caseless:true "^[-_a-z0-9]+$" in
-  fun arch -> assert (PCRE.matches rex arch)
-
-let target_features_of_capabilities_doc doc arch =
-  let xpathctx = Xml.xpath_new_context doc in
-  let expr =
-    (* Check the arch is sane.  It comes from untrusted input.  This
-     * avoids XPath injection below.
-     *)
-    arch_is_sane_or_die arch;
-    (* NB: Pay attention to the square brackets.  This returns the
-     * <guest> nodes!
-     *)
-    sprintf "/capabilities/guest[arch[@name='%s']/domain/@type='kvm']" arch in
-  let obj = Xml.xpath_eval_expression xpathctx expr in
-
-  if Xml.xpathobj_nr_nodes obj < 1 then (
-    (* Old virt-v2v used to die here, but that seems unfair since the
-     * user has gone through conversion before we reach here.
-     *)
-    warning (f_"the target hypervisor does not support a %s KVM guest") arch;
-    []
-  ) else (
-    let node (* first matching <guest> *) = Xml.xpathobj_node obj 0 in
-    Xml.xpathctx_set_current_context xpathctx node;
-
-    (* Get guest/features/* nodes. *)
-    let features = xpath_get_nodes xpathctx "features/*" in
-    List.map Xml.node_name features
-  )
-
-class output_libvirt oc output_pool =
-object (self)
-  inherit output
-
-  val mutable capabilities_doc = None
-  val mutable pool_name = None
-  val libvirt_conn = lazy (Libvirt.Connect.connect ?name:oc ())
-
-  method private conn : Libvirt.rw Libvirt.Connect.t =
-    Lazy.force libvirt_conn
-
-  method as_options =
-    match oc with
-    | None -> sprintf "-o libvirt -os %s" output_pool
-    | Some uri -> sprintf "-o libvirt -oc %s -os %s" uri output_pool
-
-  method prepare_targets source_name overlays _ =
-    (* Get the capabilities from libvirt. *)
-    let xml =
-      try
-        Libvirt.Connect.get_capabilities self#conn
-      with
-        Libvirt.Virterror { message } ->
-          error (f_"cannot get libvirt hypervisor capabilities: %s")
-            (Option.default "" message) in
-    debug "libvirt capabilities XML:\n%s" xml;
-
-    (* This just checks that the capabilities XML is well-formed,
-     * early so that we catch parsing errors before conversion.
-     *)
-    let doc = Xml.parse_memory xml in
-
-    (* Stash the capabilities XML, since we cannot get the bits we
-     * need from it until we know the guest architecture, which happens
-     * after conversion.
-     *)
-    capabilities_doc <- Some doc;
-
-    (* Does the domain already exist on the target?  (RHBZ#889082) *)
-    if Libvirt_utils.domain_exists self#conn source_name then
-        error (f_"a libvirt domain called ‘%s’ already exists on the target.\n\nIf using virt-v2v directly, use the ‘-on’ option to select a different name. Or delete the existing domain on the target using the ‘virsh undefine’ command.\n\nIf using virt-p2v, select a different ‘Name’ in the ‘Target properties’. Or delete the existing domain on the target using the ‘virsh undefine’ command.")
-          source_name;
-
-    (* Connect to output libvirt instance and check that the pool exists
-     * and dump out its XML.
-     *)
-    let pool = Libvirt_utils.get_pool self#conn output_pool in
-    let xml = Libvirt.Pool.get_xml_desc (Libvirt.Pool.const pool) in
-    let doc = Xml.parse_memory xml in
-    let xpathctx = Xml.xpath_new_context doc in
-    let xpath_string = xpath_string xpathctx in
-
-    (* We can only output to a pool of type 'dir' (directory). *)
-    if xpath_string "/pool/@type" <> Some "dir" then
-      error (f_"-o libvirt: output pool ‘%s’ is not a directory (type='dir').  See virt-v2v-output-local(1)") output_pool;
-    let target_path =
-      match xpath_string "/pool/target/path/text()" with
-      | None ->
-         error (f_"-o libvirt: output pool ‘%s’ does not have /pool/target/path element.  See virt-v2v-output-local(1)") output_pool
-      | Some dir when not (is_directory dir) ->
-         error (f_"-o libvirt: output pool ‘%s’ has type='dir' but the /pool/target/path element is not a local directory.  See virt-v2v-output-local(1)") output_pool
-      | Some dir -> dir in
-    (* Get the name of the pool, since we have to use that
-     * (and not the UUID) in the XML of the guest.
-     *)
-    pool_name <- Some (Libvirt.Pool.get_name (Libvirt.Pool.const pool));
-
-    (* Set up the targets. *)
-    List.map (
-      fun (_, ov) ->
-        TargetFile (target_path // source_name ^ "-" ^ ov.ov_sd)
-    ) overlays
-
-  method create_metadata source inspect target_meta targets =
-    (match target_meta.target_firmware with
-     | TargetBIOS -> ()
-     | TargetUEFI ->
-        (* XXX Can remove this method when libvirt supports
-         * <loader type="efi"/> since then it will be up to
-         * libvirt to check this.
-         *)
-        error_unless_uefi_firmware target_meta.guestcaps.gcaps_arch
-    );
-
-    (* We copied directly into the final pool directory.  However we
-     * have to tell libvirt.
-     *)
-    (try
-      let pool = Libvirt_utils.get_pool self#conn output_pool in
-      Libvirt.Pool.refresh (Libvirt.Pool.const pool)
-    with
-      Libvirt.Virterror { message } ->
-        warning (f_"could not refresh libvirt pool ‘%s’: %s")
-          output_pool (Option.default "" message)
-    );
-
-    let pool_name =
-      match pool_name with
-      | None -> output_pool
-      | Some n -> n in
-
-    (* Parse the capabilities XML in order to get the supported features. *)
-    let doc =
-      match capabilities_doc with
-      | None -> assert false
-      | Some doc -> doc in
-    let target_features =
-      target_features_of_capabilities_doc doc
-        target_meta.guestcaps.gcaps_arch in
-
-    (* Create the metadata. *)
-    let doc =
-      create_libvirt_xml ~pool:pool_name source inspect target_meta
-        target_features targets in
-
-    let tmpfile, chan = Filename.open_temp_file "v2vlibvirt" ".xml" in
-    DOM.doc_to_chan chan doc;
-    close_out chan;
-
-    if verbose () then (
-      eprintf "resulting XML for libvirt:\n%!";
-      DOM.doc_to_chan stderr doc;
-      eprintf "\n%!";
-    );
-
-    (* Define the domain in libvirt. *)
-    (try
-      ignore (Libvirt.Domain.define_xml self#conn (DOM.doc_to_string doc));
-      (try Unix.unlink tmpfile with _ -> ())
-    with
-      Libvirt.Virterror { message } ->
-        warning (f_"could not define libvirt domain: %s.\nThe libvirt XML is still available in ‘%s’.  Try running ‘virsh -c %s define %s’ yourself instead.")
-          (Option.default "" message) tmpfile
-          (Libvirt.Connect.get_uri self#conn) tmpfile
-    );
-end
-
-let output_libvirt = new output_libvirt
-let () = Modules_list.register_output_module "libvirt"

v2v/output_qemu.ml
@@ -1,286 +1,0 @@
-(* virt-v2v
- * Copyright (C) 2009-2020 Red Hat Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *)
-
-open Printf
-
-open Std_utils
-open Tools_utils
-open Common_gettext.Gettext
-
-open Types
-open Utils
-
-let find_target_disk targets { s_disk_id = id } =
-  try List.find (fun t -> t.target_overlay.ov_source.s_disk_id = id) targets
-  with Not_found -> assert false
-
-class output_qemu dir qemu_boot =
-object
-  inherit output
-
-  method as_options =
-    sprintf "-o qemu -os %s%s" dir (if qemu_boot then " --qemu-boot" else "")
-
-  method prepare_targets source_name overlays _ =
-    List.map (
-      fun (_, ov) ->
-        TargetFile (dir // source_name ^ "-" ^ ov.ov_sd)
-    ) overlays
-
-  method create_metadata source inspect
-           { guestcaps; output_name;
-             target_buses; target_firmware; target_nics } targets =
-    (match target_firmware with
-     | TargetBIOS -> ()
-     | TargetUEFI -> error_unless_uefi_firmware guestcaps.gcaps_arch
-    );
-
-    let file = dir // output_name ^ ".sh" in
-
-    let uefi_firmware =
-      match target_firmware with
-      | TargetBIOS -> None
-      | TargetUEFI -> Some (find_uefi_firmware guestcaps.gcaps_arch) in
-    let machine, secure_boot_required =
-      match guestcaps.gcaps_machine, uefi_firmware with
-      | _, Some { Uefi.flags }
-           when List.mem Uefi.UEFI_FLAG_SECURE_BOOT_REQUIRED flags ->
-         (* Force machine type to Q35 because PC does not support
-          * secure boot.  We must remove this when we get the
-          * correct machine type from libosinfo in future. XXX
-          *)
-         Q35, true
-      | machine, _ ->
-         machine, false in
-    let smm = secure_boot_required in
-
-    let machine =
-      match machine with
-      | I440FX -> "pc"
-      | Q35 -> "q35"
-      | Virt -> "virt" in
-
-    (* Construct the command line.  Note that the [Qemuopts]
-     * module deals with shell and qemu comma quoting.
-     *)
-    let cmd = Qemuopts.create () in
-    Qemuopts.set_binary_by_arch cmd (Some guestcaps.gcaps_arch);
-
-    let flag = Qemuopts.flag cmd
-    and arg = Qemuopts.arg cmd
-    and arg_noquote = Qemuopts.arg_noquote cmd
-    and arg_list = Qemuopts.arg_list cmd in
-
-    flag "-no-user-config"; flag "-nodefaults";
-    arg "-name" output_name;
-
-    (match source.s_genid with
-     | None -> ()
-     | Some genid ->
-        arg_list "-device" ["vmgenid"; sprintf "guid=%s" genid; "id=vmgenid0"]
-    );
-
-    arg_list "-machine" (machine ::
-                         (if smm then ["smm=on"] else []) @
-                         ["accel=kvm:tcg"]);
-
-    (match uefi_firmware with
-     | None -> ()
-     | Some { Uefi.code } ->
-        if secure_boot_required then
-          arg_list "-global"
-                   ["driver=cfi.pflash01"; "property=secure"; "value=on"];
-        arg_list "-drive"
-                 ["if=pflash"; "format=raw"; "file=" ^ code; "readonly"];
-        arg_noquote "-drive" "if=pflash,format=raw,file=\"$uefi_vars\"";
-    );
-
-    arg "-m" (Int64.to_string (source.s_memory /^ 1024L /^ 1024L));
-    if source.s_vcpu > 1 then (
-      (match source.s_cpu_topology with
-       | None ->
-          arg "-smp" (string_of_int source.s_vcpu)
-       | Some { s_cpu_sockets; s_cpu_cores; s_cpu_threads } ->
-         let args = [
-           sprintf "cpus=%d" source.s_vcpu;
-           sprintf "sockets=%d" s_cpu_sockets;
-           sprintf "cores=%d" s_cpu_cores;
-           sprintf "threads=%d" s_cpu_threads;
-         ] in
-         arg_list "-smp" args
-      );
-    );
-
-    let make_disk if_name i = function
-    | BusSlotEmpty -> ()
-
-    | BusSlotDisk d ->
-       (* Find the corresponding target disk. *)
-       let t = find_target_disk targets d in
-
-       let target_file =
-         match t.target_file with
-         | TargetFile s -> s
-         | TargetURI _ -> assert false in
-       arg_list "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
-                          "if=" ^ if_name; "index=" ^ string_of_int i;
-                          "media=disk"]
-
-    | BusSlotRemovable { s_removable_type = CDROM } ->
-       arg_list "-drive" ["format=raw"; "if=" ^ if_name;
-                          "index=" ^ string_of_int i; "media=cdrom"]
-
-    | BusSlotRemovable { s_removable_type = Floppy } ->
-       arg_list "-drive" ["format=raw"; "if=" ^ if_name;
-                          "index=" ^ string_of_int i; "media=floppy"]
-    in
-    Array.iteri (make_disk "virtio") target_buses.target_virtio_blk_bus;
-    Array.iteri (make_disk "ide") target_buses.target_ide_bus;
-
-    let make_scsi i = function
-    | BusSlotEmpty -> ()
-
-    | BusSlotDisk d ->
-       (* Find the corresponding target disk. *)
-       let t = find_target_disk targets d in
-
-       let target_file =
-         match t.target_file with
-         | TargetFile s -> s
-         | TargetURI _ -> assert false in
-       arg_list "-drive" ["file=" ^ target_file; "format=" ^ t.target_format;
-                          "if=scsi"; "bus=0"; "unit=" ^ string_of_int i;
-                          "media=disk"]
-
-    | BusSlotRemovable { s_removable_type = CDROM } ->
-       arg_list "-drive" ["format=raw"; "if=scsi"; "bus=0";
-                          "unit=" ^ string_of_int i; "media=cdrom"]
-
-    | BusSlotRemovable { s_removable_type = Floppy } ->
-       arg_list "-drive" ["format=raw"; "if=scsi"; "bus=0";
-                          "unit=" ^ string_of_int i; "media=floppy"]
-    in
-    Array.iteri make_scsi target_buses.target_scsi_bus;
-
-    (* XXX Highly unlikely that anyone cares, but the current
-     * code ignores target_buses.target_floppy_bus.
-     *)
-
-    let net_bus =
-      match guestcaps.gcaps_net_bus with
-      | Virtio_net -> "virtio-net-pci"
-      | E1000 -> "e1000"
-      | RTL8139 -> "rtl8139" in
-    List.iteri (
-      fun i nic ->
-        arg_list "-netdev" ["user"; "id=net" ^ string_of_int i];
-        arg_list "-device" ([net_bus;
-                             sprintf "netdev=net%d" i] @
-                             (match nic.s_mac with
-                              | None -> []
-                              | Some mac -> ["mac=" ^ mac]))
-    ) target_nics;
-
-    (* Add a display. *)
-    (match source.s_display with
-    | None -> ()
-    | Some display ->
-      (match display.s_display_type with
-      | Window ->
-         arg "-display" "gtk"
-      | VNC ->
-         arg "-display" "vnc=:0"
-      | Spice ->
-         arg_list "-spice" [sprintf "port=%d"
-                                    (match display.s_port with
-                                     | None -> 5900
-                                     | Some p -> p);
-                            "addr=127.0.0.1"]
-      );
-      arg "-vga"
-          (match guestcaps.gcaps_video with Cirrus -> "cirrus" | QXL -> "qxl")
-    );
-
-    (* Add a sound card. *)
-    (match source.s_sound with
-     | None -> ()
-     | Some { s_sound_model = model } ->
-        if qemu_supports_sound_card model then (
-          match model with
-          | AC97      -> arg "-device" "AC97"
-          | ES1370    -> arg "-device" "ES1370"
-          | ICH6      -> arg "-device" "intel-hda"; arg "-device" "hda-duplex"
-          (* XXX ich9 is a q35-only device, so it's not likely
-             that this will work unless we can force q35 above: *)
-          | ICH9      -> arg "-device" "ich9-intel-hda"
-          | PCSpeaker -> arg "-soundhw" "pcspk" (* not qdev-ified *)
-          | SB16      -> arg "-device" "sb16"
-          | USBAudio  -> arg "-device" "usb-audio"
-        )
-    );
-
-    (* Add the miscellaneous KVM devices. *)
-    if guestcaps.gcaps_virtio_rng then (
-      arg_list "-object" ["rng-random"; "filename=/dev/urandom"; "id=rng0"];
-      arg_list "-device" ["virtio-rng-pci"; "rng=rng0"];
-    );
-    if guestcaps.gcaps_virtio_balloon then
-      arg "-balloon" "virtio"
-    else
-      arg "-balloon" "none";
-    if guestcaps.gcaps_isa_pvpanic then
-      arg_list "-device" ["pvpanic"; "ioport=0x505"];
-    if guestcaps.gcaps_virtio_socket then
-      arg "-viosock" "virtio"
-    else
-      arg "-viosock" "none";
-
-    (* Add a serial console to Linux guests. *)
-    if inspect.i_type = "linux" then
-      arg "-serial" "stdio";
-
-    (* Write the output file. *)
-    with_open_out file (
-      fun chan ->
-        let fpf fs = fprintf chan fs in
-        fpf "#!/bin/sh -\n";
-        fpf "\n";
-
-        (match uefi_firmware with
-         | None -> ()
-         | Some { Uefi.vars = vars_template } ->
-            fpf "# Make a copy of the UEFI variables template\n";
-            fpf "uefi_vars=\"$(mktemp)\"\n";
-            fpf "cp %s \"$uefi_vars\"\n" (quote vars_template);
-            fpf "\n"
-        );
-
-        Qemuopts.to_chan cmd chan
-    );
-
-    Unix.chmod file 0o755;
-
-    (* If --qemu-boot option was specified then we should boot the guest. *)
-    if qemu_boot then (
-      let cmd = sprintf "%s &" (quote file) in
-      ignore (shell_command cmd)
-    )
-end
-
-let output_qemu = new output_qemu
-let () = Modules_list.register_output_module "qemu"

commit 53690a0c602a4286fdb9408fdf6a01cc352697ec

-o disk, -o libvirt, -o qemu: Implement -of qcow2 -oo compressed
For various output modes, implement -oo compressed which can be used
to generate compressed qcow2 files.  This option was dropped when
modularizing virt-v2v, and required changes to nbdcopy which are
finally upstream in libnbd >= 1.13.5.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2047660
Fixes: commit 255722cbf39afc0b012e2ac00d16fa6ba2f8c21f
Reported-by: Xiaodai Wang
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

TODO
@@ -1,17 +1,3 @@
-virt-v2v -o disk|qemu -oo compressed
-------------------------------------
-
-This was temporarily dropped when I modularized virt-v2v.  It would
-not be too difficult to add it back.  The following is the qemu-nbd
-command required (to be run as the output helper) which creates a
-compressed qcow2 disk image:
-
-$ qemu-nbd --image-opts driver=compress,file.driver=qcow2,file.file.driver=file,file.file.filename=new.qcow2
-
-Note this requires fixes in nbdcopy so it obeys the advertised block
-alignment:
-https://lists.gnu.org/archive/html/qemu-block/2022-01/threads.html#00729
-
 virt-v2v -o rhv-upload
 ----------------------
 

output/output_disk.ml
@@ -30,7 +30,7 @@ open Create_libvirt_xml
 open Output
 
 module Disk = struct
-  type poptions = Types.output_allocation * string * string * string
+  type poptions = bool * Types.output_allocation * string * string * string
 
   type t = unit
 
@@ -41,11 +41,21 @@ module Disk = struct
       | None -> ""
 
   let query_output_options () =
-    printf (f_"No output options can be used in this mode.\n")
+    printf (f_"Output options that can be used with -o disk:
+
+  -oo compressed      Compress the output file (used only with -of qcow2)
+")
 
   let parse_options options source =
-    if options.output_options <> [] then
-      error (f_"no -oo (output options) are allowed here");
+    let compressed = ref false in
+    List.iter (
+      function
+      | "compressed", "" -> compressed := true
+      | "compressed", v -> compressed := bool_of_string v
+      | k, _ ->
+         error (f_"-o disk: unknown output option ‘-oo %s’") k
+    ) options.output_options;
+
     if options.output_password <> None then
       error_option_cannot_be_used_in_output_mode "local" "-op";
 
@@ -60,11 +70,13 @@ module Disk = struct
 
     let output_name = Option.default source.s_name options.output_name in
 
-    options.output_alloc, options.output_format, output_name, output_storage
+    !compressed, options.output_alloc, options.output_format,
+    output_name, output_storage
 
   let setup dir options source =
     let disks = get_disks dir in
-    let output_alloc, output_format, output_name, output_storage = options in
+    let compressed, output_alloc, output_format, output_name, output_storage =
+      options in
 
     List.iter (
       fun (i, size) ->
@@ -73,11 +85,12 @@ module Disk = struct
 
         (* Create the actual output disk. *)
         let outdisk = disk_path output_storage output_name i in
-        output_to_local_file output_alloc output_format outdisk size socket
+        output_to_local_file ~compressed output_alloc output_format
+          outdisk size socket
     ) disks
 
   let finalize dir options () source inspect target_meta =
-    let output_alloc, output_format, output_name, output_storage = options in
+    let _, output_alloc, output_format, output_name, output_storage = options in
 
     (* Convert metadata to libvirt XML. *)
     (match target_meta.target_firmware with

output/output_libvirt.ml
@@ -32,7 +32,7 @@ open Create_libvirt_xml
 open Output
 
 module Libvirt_ = struct
-  type poptions = Libvirt.rw Libvirt.Connect.t Lazy.t *
+  type poptions = Libvirt.rw Libvirt.Connect.t Lazy.t * bool *
                   Types.output_allocation * string * string * string
 
   type t = string * string
@@ -44,11 +44,21 @@ module Libvirt_ = struct
       | None -> ""
 
   let query_output_options () =
-    printf (f_"No output options can be used in this mode.\n")
+    printf (f_"Output options that can be used with -o libvirt:
+
+  -oo compressed      Compress the output file (used only with -of qcow2)
+")
 
   let parse_options options source =
-    if options.output_options <> [] then
-      error (f_"no -oo (output options) are allowed here");
+    let compressed = ref false in
+    List.iter (
+      function
+      | "compressed", "" -> compressed := true
+      | "compressed", v -> compressed := bool_of_string v
+      | k, _ ->
+         error (f_"-o disk: unknown output option ‘-oo %s’") k
+    ) options.output_options;
+
     if options.output_password <> None then
       error_option_cannot_be_used_in_output_mode "libvirt" "-op";
 
@@ -59,12 +69,13 @@ module Libvirt_ = struct
 
     let output_name = Option.default source.s_name options.output_name in
 
-    (conn, options.output_alloc, options.output_format, output_name,
-     output_pool)
+    (conn, !compressed, options.output_alloc, options.output_format,
+     output_name, output_pool)
 
   let setup dir options source =
     let disks = get_disks dir in
-    let conn, output_alloc, output_format, output_name, output_pool = options in
+    let conn, compressed, output_alloc, output_format,
+        output_name, output_pool = options in
     let conn = Lazy.force conn in
 
     (* Get the capabilities from libvirt. *)
@@ -119,13 +130,15 @@ module Libvirt_ = struct
 
         (* Create the actual output disk. *)
         let outdisk = target_path // output_name ^ "-sd" ^ (drive_name i) in
-        output_to_local_file output_alloc output_format outdisk size socket
+        output_to_local_file ~compressed output_alloc output_format
+          outdisk size socket
     ) disks;
 
     (capabilities_xml, pool_name)
 
   let rec finalize dir options t source inspect target_meta =
-    let conn, output_alloc, output_format, output_name, output_pool = options in
+    let conn, _, output_alloc, output_format, output_name, output_pool =
+      options in
     let capabilities_xml, pool_name = t in
 
     (match target_meta.target_firmware with

output/output_qemu.ml
@@ -29,7 +29,8 @@ open Utils
 open Output
 
 module QEMU = struct
-  type poptions = bool * Types.output_allocation * string * string * string
+  type poptions = bool * bool *
+                  Types.output_allocation * string * string * string
 
   type t = unit
 
@@ -42,6 +43,7 @@ module QEMU = struct
   let query_output_options () =
     printf (f_"Output options (-oo) which can be used with -o qemu:
 
+  -oo compressed      Compress the output file (used only with -of qcow2)
   -oo qemu-boot       Boot the guest in qemu after conversion
 ")
 
@@ -49,19 +51,19 @@ module QEMU = struct
     if options.output_password <> None then
       error_option_cannot_be_used_in_output_mode "qemu" "-op";
 
-    let qemu_boot = ref false in
+    let compressed = ref false
+    and qemu_boot = ref false in
     List.iter (
-      fun (k, v) ->
-        match k with
-        | "qemu-boot" ->
-           if v = "" || v = "true" then qemu_boot := true
-           else if v = "false" then qemu_boot := false
-           else
-             error (f_"-o qemu: use -oo qemu-boot[=true|false]")
-        | k ->
-           error (f_"-o qemu: unknown output option ‘-oo %s’") k
-      ) options.output_options;
-    let qemu_boot = !qemu_boot in
+      function
+      | "compressed", "" -> compressed := true
+      | "compressed", v -> compressed := bool_of_string v
+      | "qemu-boot", "" -> qemu_boot := true
+      | "qemu-boot", v -> qemu_boot := bool_of_string v
+      | k, _ ->
+         error (f_"-o qemu: unknown output option ‘-oo %s’") k
+    ) options.output_options;
+    let compressed = !compressed
+    and qemu_boot = !qemu_boot in
 
     (* -os must be set to a directory. *)
     let output_storage =
@@ -74,12 +76,13 @@ module QEMU = struct
 
     let output_name = Option.default source.s_name options.output_name in
 
-    (qemu_boot, options.output_alloc, options.output_format,
+    (compressed, qemu_boot, options.output_alloc, options.output_format,
      output_name, output_storage)
 
   let setup dir options source =
     let disks = get_disks dir in
-    let _, output_alloc, output_format, output_name, output_storage = options in
+    let compressed, _, output_alloc, output_format,
+        output_name, output_storage = options in
 
     List.iter (
       fun (i, size) ->
@@ -88,11 +91,12 @@ module QEMU = struct
 
         (* Create the actual output disk. *)
         let outdisk = disk_path output_storage output_name i in
-        output_to_local_file output_alloc output_format outdisk size socket
+        output_to_local_file ~compressed output_alloc output_format
+          outdisk size socket
     ) disks
 
   let finalize dir options () source inspect target_meta =
-    let qemu_boot, output_alloc, output_format,
+    let _, qemu_boot, output_alloc, output_format,
         output_name, output_storage = options in
 
     let { guestcaps; target_buses; target_firmware } = target_meta in