libguestfs/virt-v2v

commit 1e0d615a883e4056b757d041a1ad1bbd39df3b3c

build: Don't write valgrind log files to tmp/ directory
Inherited from libguestfs, we wrote valgrind log files into the tmp/
directory during tests.  However this makes it very difficult to
connect a particular log file to a particular failing test.  As with
nbdkit let's just write the valgrind output to stderr, where it will
end up in the test log file (ie. tests/test-suite.log or tests/*.log).

I also adjusted the valgrind parameters so they more closely match
those used by nbdkit.

m4/guestfs-progs.m4
@@ -64,7 +64,7 @@ AC_CHECK_PROG([VALGRIND],[valgrind],[valgrind],[no])
 AS_IF([test "x$VALGRIND" != "xno"],[
     # Substitute the whole valgrind command.
     # --read-inline-info=no is a temporary workaround for RHBZ#1662656.
-    VG='libtool --mode=execute $(VALGRIND) --vgdb=no --log-file=$(abs_top_builddir)/tmp/valgrind-%q{T}-%p.log --leak-check=full --error-exitcode=119 --suppressions=$(abs_top_srcdir)/valgrind-suppressions --trace-children=no --child-silent-after-fork=yes --run-libc-freeres=no --read-inline-info=no'
+    VG='libtool --mode=execute $(VALGRIND) --vgdb=no --leak-check=full --show-leak-kinds=all --error-exitcode=119 --suppressions=$(abs_top_srcdir)/valgrind-suppressions --trace-children=no --child-silent-after-fork=yes --run-libc-freeres=no --num-callers=100 --read-inline-info=no'
     ],[
     # No valgrind, so substitute VG with something that will break.
     VG=VALGRIND_IS_NOT_INSTALLED

run.in
@@ -57,10 +57,6 @@ prepend()
 s="$(cd @abs_srcdir@ && pwd)"
 b="$(cd @abs_builddir@ && pwd)"
 
-# Set T to current date & time (mainly for valgrind logfile name).
-T=`date +%Y%m%d.%H.%M.%S`
-export T
-
 # Set tmpdir and cachedir so the appliance doesn't conflict with
 # globally installed libguestfs.
 #
@@ -77,11 +73,14 @@ prepend PATH "$b/in-place"
 export PATH
 
 # This is a cheap way to find some use-after-free and uninitialized
-# read problems when using glibc.
-random_val="$(@AWK@ 'BEGIN{srand(); print 1+int(255*rand())}' < /dev/null)"
-LD_PRELOAD="${LD_PRELOAD:+"$LD_PRELOAD:"}libc_malloc_debug.so.0"
-GLIBC_TUNABLES=glibc.malloc.check=1:glibc.malloc.perturb=$random_val
-export LD_PRELOAD GLIBC_TUNABLES
+# read problems when using glibc.  But if we are valgrinding then
+# don't use this because it can stop valgrind from working.
+if [ -z "$VG" ]; then
+    random_val="$(@AWK@ 'BEGIN{srand(); print 1+int(255*rand())}' < /dev/null)"
+    LD_PRELOAD="${LD_PRELOAD:+"$LD_PRELOAD:"}libc_malloc_debug.so.0"
+    GLIBC_TUNABLES=glibc.malloc.check=1:glibc.malloc.perturb=$random_val
+    export LD_PRELOAD GLIBC_TUNABLES
+fi
 
 # Do we have libtool?  If we have it then we can use it to make
 # running valgrind simpler.  However don't depend on it.

tmp/.gitignore
@@ -3,4 +3,3 @@
 /libguestfs??????/
 /run-*
 /v2vovl*.qcow2
-/valgrind-*.log

commit c3769ffa2a1bcc007ac3eca1638ab1a321d73a9d

valgrind: Remove --show-leak-kinds=all
In commit 1e0d615a88 ("build: Don't write valgrind log files to tmp/
directory") I added this option without really thinking too much about
it.  However it makes the output of valgrind much harder to read
because it includes many traces which are not counted as true leaks.

Fixes: commit 1e0d615a883e4056b757d041a1ad1bbd39df3b3c

m4/guestfs-progs.m4
@@ -64,7 +64,7 @@ AC_CHECK_PROG([VALGRIND],[valgrind],[valgrind],[no])
 AS_IF([test "x$VALGRIND" != "xno"],[
     # Substitute the whole valgrind command.
     # --read-inline-info=no is a temporary workaround for RHBZ#1662656.
-    VG='libtool --mode=execute $(VALGRIND) --vgdb=no --leak-check=full --show-leak-kinds=all --error-exitcode=119 --suppressions=$(abs_top_srcdir)/valgrind-suppressions --trace-children=no --child-silent-after-fork=yes --run-libc-freeres=no --num-callers=100 --read-inline-info=no'
+    VG='libtool --mode=execute $(VALGRIND) --vgdb=no --leak-check=full --error-exitcode=119 --suppressions=$(abs_top_srcdir)/valgrind-suppressions --trace-children=no --child-silent-after-fork=yes --run-libc-freeres=no --num-callers=100 --read-inline-info=no'
     ],[
     # No valgrind, so substitute VG with something that will break.
     VG=VALGRIND_IS_NOT_INSTALLED