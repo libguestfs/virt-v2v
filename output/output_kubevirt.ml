libguestfs/virt-v2v

commit 8a9c914544a49bed13eb5baf42290f835bdee7b5

-o kubevirt: Error on invalid output guest names
Kubevirt supports something like RFC 1123 names (without the length
restriction).  Helpfully it prints the regexp that it uses the
validate the names, so just use the same regexp.

Note that virt-v2v never renames guests (since that would add
unpredictability for automation).  You must use the -on option to
rename the guest if the name is wrong.  Hence this is an error, not a
warning or an attempt to rename the guest.

Reported-by: Ming Xie
Fixes: commit bfa62b4683d312fc2fa9bb3c08963fc4846831b9
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2162332

output/output_kubevirt.ml
@@ -29,6 +29,11 @@ open Utils
 open Output
 open Create_kubevirt_yaml
 
+(* Valid output names for Kubevirt (RHBZ#2162332). *)
+let rfc1123_re =
+  PCRE.compile ~anchored:true
+    "[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
+
 module Kubevirt = struct
   type poptions = output_allocation * string * string * string
 
@@ -59,6 +64,12 @@ module Kubevirt = struct
       | Some d -> d in
 
     let output_name = Option.default source.s_name options.output_name in
+
+    if not (PCRE.matches rfc1123_re output_name) then
+      error (f_"-o kubevirt: the guest name must contain only lowercase \
+                alphanumeric characters, '-' or '.', and must start and \
+                end with an alphanumeric character.  Rerun virt-v2v with \
+                the '-on name' option to rename it.");
 
     options.output_alloc, options.output_format, output_name, output_storage
 

commit 050a0ba714ddf2a5d81515c886032016aa75342c

-o kubevirt: Replace PCRE ~anchored with ^...$
Because this regexp was not anchored at both ends it would still
report a match for incorrect names.

Update common submodule to get this commit:

    mlpcre: Remove ~anchored trap

    PCRE2_ANCHORED only anchors the regexp at the start (ie.  equivalent
    to adding ^ at the beginning).  It *does not* anchor it at the end as
    well (for which there is a separate PCRE2_ENDANCHORED which has
    various traps of its own).

    Everywhere I've tried to use ~anchored I've fallen into the trap of
    believing it anchors both ends, causing actual bugs.  So remove it
    completely from the bindings.

    Replace ~anchored:true with ^...$ around the regular expression.

Fixes: commit 8a9c914544a49bed13eb5baf42290f835bdee7b5
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2162332
Reported-by: Ming Xie

output/output_kubevirt.ml
@@ -31,8 +31,8 @@ open Create_kubevirt_yaml
 
 (* Valid output names for Kubevirt (RHBZ#2162332). *)
 let rfc1123_re =
-  PCRE.compile ~anchored:true
-    "[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
+  PCRE.compile
+    "^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$"
 
 module Kubevirt = struct
   type poptions = bool * output_allocation * string * string * string