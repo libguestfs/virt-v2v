libguestfs/virt-v2v

commit ade62ebdb50e32a05c336ffcf4c08c6996a3af36

v2v: Add support for virt-customize options
For higher level tools making further arbitrary changes to the guest
post-conversion, especially adding more firstboot scripts, it's useful
if we have various features from virt-customize.  Particularly the
ability to upload and run scripts.  Since we have the virt-customize
code in the common/ submodule, the easiest thing to do is to add the
virt-customize options, the same way it is done in virt-builder and
virt-sysprep.

Also updates the common submodule to get:

Richard W.M. Jones (1):
      mlcustomize: Move virt-customize modules to mlcustomize/

docs/Makefile.am
@@ -76,6 +76,7 @@ stamp-virt-v2v.pod: virt-v2v.pod
 	  --man virt-v2v.1 \
 	  --html $(top_builddir)/website/virt-v2v.1.html \
 	  --path $(top_srcdir)/common/options \
+	  --insert $(top_srcdir)/common/mlcustomize/customize-options.pod:__CUSTOMIZE_OPTIONS__ \
 	  --license GPLv2+ \
 	  --warning safe \
 	  $<

in-place/in_place.ml
@@ -180,6 +180,13 @@ let rec main () =
     [ L"root" ],     Getopt.String ("ask|... ", set_root_choice),
                                     s_"How to choose root filesystem";
   ] in
+
+  (* Append virt-customize options. *)
+  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec =
+    List.map (fun (spec, _, _) -> spec) customize_argspec in
+  let argspec = argspec @ customize_argspec in
+
   let args = ref [] in
   let anon_fun s = List.push_front s args in
   let usage_msg =
@@ -197,6 +204,7 @@ A short summary of the options is given below.  For detailed help please
 read the man page virt-v2v-in-place(1).
 ")
       prog in
+
   let opthandle = create_standard_options argspec ~anon_fun ~key_opts:true ~machine_readable:true usage_msg in
   Getopt.parse opthandle.getopt;
 
@@ -222,6 +230,7 @@ read the man page virt-v2v-in-place(1).
     | Some "virtio-scsi" -> Virtio_SCSI
     | Some driver ->
        error (f_"unknown block driver ‘--block-driver %s’") driver in
+  let customize_ops = get_customize_ops () in
   let input_conn = !input_conn in
   let input_mode = !input_mode in
   let print_source = !print_source in
@@ -241,6 +250,7 @@ read the man page virt-v2v-in-place(1).
       pr "io\n";
       pr "mac-option\n";
       pr "mac-ip-option\n";
+      pr "customize-ops\n";
       pr "input:disk\n";
       pr "input:libvirt\n";
       pr "input:libvirtxml\n";
@@ -311,6 +321,7 @@ read the man page virt-v2v-in-place(1).
     network_map;
     root_choice;
     static_ips;
+    customize_ops;
   } in
 
   (* Before starting the input module, check there is sufficient

inspector/inspector.ml
@@ -183,6 +183,13 @@ let rec main () =
     [ S 'O' ],       Getopt.String ("output.xml", set_output_file_option),
                                     s_"Set the output filename";
   ] in
+
+  (* Append virt-customize options. *)
+  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec =
+    List.map (fun (spec, _, _) -> spec) customize_argspec in
+  let argspec = argspec @ customize_argspec in
+
   let args = ref [] in
   let anon_fun s = List.push_front s args in
   let usage_msg =
@@ -195,6 +202,7 @@ A short summary of the options is given below.  For detailed help please
 read the man page virt-v2v-inspector(1).
 ")
       prog in
+
   let opthandle = create_standard_options argspec ~anon_fun ~key_opts:true
                     ~machine_readable:true usage_msg in
   Getopt.parse opthandle.getopt;
@@ -215,6 +223,7 @@ read the man page virt-v2v-inspector(1).
 
   (* Dereference the arguments. *)
   let args = List.rev !args in
+  let customize_ops = get_customize_ops () in
   let output_file = !output_file in
   let input_conn = !input_conn in
   let input_mode = !input_mode in
@@ -239,6 +248,7 @@ read the man page virt-v2v-inspector(1).
       pr "io\n";
       pr "mac-option\n";
       pr "mac-ip-option\n";
+      pr "customize-ops\n";
       pr "input:disk\n";
       pr "input:libvirt\n";
       pr "input:libvirtxml\n";
@@ -332,6 +342,7 @@ read the man page virt-v2v-inspector(1).
     network_map;
     root_choice;
     static_ips;
+    customize_ops;
   } in
 
   (* Before starting the input module, check there is sufficient

v2v/v2v.ml
@@ -312,6 +312,13 @@ let rec main () =
     [ L"vmtype" ],   Getopt.String ("-", vmtype_warning),
       s_"Ignored for backwards compatibility";
   ] in
+
+  (* Append virt-customize options. *)
+  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec =
+    List.map (fun (spec, _, _) -> spec) customize_argspec in
+  let argspec = argspec @ customize_argspec in
+
   let args = ref [] in
   let anon_fun s = List.push_front s args in
   let usage_msg =
@@ -336,6 +343,7 @@ A short summary of the options is given below.  For detailed help please
 read the man page virt-v2v(1).
 ")
       prog in
+
   let opthandle = create_standard_options argspec ~anon_fun ~key_opts:true
                     ~machine_readable:true usage_msg in
   Getopt.parse opthandle.getopt;
@@ -362,6 +370,7 @@ read the man page virt-v2v(1).
     | Some "virtio-scsi" -> Virtio_SCSI
     | Some driver ->
        error (f_"unknown block driver ‘--block-driver %s’") driver in
+  let customize_ops = get_customize_ops () in
   let input_conn = !input_conn in
   let input_mode = !input_mode in
   let input_transport =
@@ -398,6 +407,7 @@ read the man page virt-v2v(1).
       pr "mac-option\n";
       pr "bandwidth-option\n";
       pr "mac-ip-option\n";
+      pr "customize-ops\n";
       pr "input:disk\n";
       pr "input:libvirt\n";
       pr "input:libvirtxml\n";
@@ -541,6 +551,7 @@ read the man page virt-v2v(1).
     network_map;
     root_choice;
     static_ips;
+    customize_ops;
   } in
 
   (* Before starting the input module, check there is sufficient

docs/test-v2v-docs.sh
@@ -24,6 +24,53 @@ set -x
 
 skip_if_skipped
 
+# We don't explicitly document the virt-customize options in the
+# synopsis, so don't give an error in the test.
+virt_customize_options=\
+--append-line,\
+--chmod,\
+--chown,\
+--commands-from-file,\
+--copy,\
+--copy-in,\
+--delete,\
+--edit,\
+--firstboot,\
+--firstboot-command,\
+--firstboot-install,\
+--hostname,\
+--inject-blnsvr,\
+--inject-qemu-ga,\
+--inject-virtio-win,\
+--install,\
+--link,\
+--mkdir,\
+--move,\
+--no-logfile,\
+--no-selinux-relabel,\
+--password,\
+--password-crypto,\
+--root-password,\
+--run,\
+--run-command,\
+--scrub,\
+--selinux-relabel,\
+--sm-attach,\
+--sm-credentials,\
+--sm-register,\
+--sm-remove,\
+--sm-unregister,\
+--ssh-inject,\
+--tar-in,\
+--timezone,\
+--touch,\
+--truncate,\
+--truncate-recursive,\
+--uninstall,\
+--update,\
+--upload,\
+--write
+
 $srcdir/../podcheck.pl virt-v2v.pod virt-v2v \
   --path $srcdir/../common/options \
   --ignore=\
@@ -57,7 +104,8 @@ $srcdir/../podcheck.pl virt-v2v.pod virt-v2v \
 --vdsm-ovf-output,\
 --vdsm-vm-uuid,\
 --vdsm-vol-uuid,\
---vmtype
+--vmtype,\
+$virt_customize_options
 
 $srcdir/../podcheck.pl virt-v2v-in-place.pod virt-v2v-in-place \
   --path $srcdir/../common/options \
@@ -74,7 +122,8 @@ $srcdir/../podcheck.pl virt-v2v-in-place.pod virt-v2v-in-place \
 --on,\
 --oo,\
 --op,\
---os
+--os,\
+$virt_customize_options
 
 $srcdir/../podcheck.pl virt-v2v-inspector.pod virt-v2v-inspector \
   --path $srcdir/../common/options \
@@ -83,4 +132,5 @@ $srcdir/../podcheck.pl virt-v2v-inspector.pod virt-v2v-inspector \
 --if,\
 --io,\
 --ip,\
---it
+--it,\
+$virt_customize_options

in-place/Makefile.am
@@ -75,6 +75,7 @@ OCAMLCLIBS = \
 	-lqemuopts \
 	$(LIBGUESTFS_LIBS) \
 	$(LIBVIRT_LIBS) \
+	$(LIBCRYPT_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(JANSSON_LIBS) \
 	$(LIBOSINFO_LIBS) \

inspector/Makefile.am
@@ -75,6 +75,7 @@ OCAMLCLIBS = \
 	-lqemuopts \
 	$(LIBGUESTFS_LIBS) \
 	$(LIBVIRT_LIBS) \
+	$(LIBCRYPT_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(JANSSON_LIBS) \
 	$(LIBOSINFO_LIBS) \

v2v/Makefile.am
@@ -78,6 +78,7 @@ OCAMLCLIBS = \
 	-lqemuopts \
 	$(LIBGUESTFS_LIBS) \
 	$(LIBVIRT_LIBS) \
+	$(LIBCRYPT_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(JANSSON_LIBS) \
 	$(LIBOSINFO_LIBS) \

commit f720aff7d690ead787b2ec9d4dabdff2b2923efd

v2v: Don't include virt-customize --inject* options
Commit ade62ebdb5 ("v2v: Add support for virt-customize options")
included --inject* options that covered facilities for injecting QEMU
guest agent etc which are already part of virt-v2v.  We shouldn't add
the options for that.

The generator in libguestfs was changed so we can now exclude those
options from virt-v2v.

Update common submodule to include:

Richard W.M. Jones (1):
      mlcustomize: Update generated options for virt-v2v

Fixes: commit ade62ebdb50e32a05c336ffcf4c08c6996a3af36

docs/Makefile.am
@@ -76,7 +76,7 @@ stamp-virt-v2v.pod: virt-v2v.pod
 	  --man virt-v2v.1 \
 	  --html $(top_builddir)/website/virt-v2v.1.html \
 	  --path $(top_srcdir)/common/options \
-	  --insert $(top_srcdir)/common/mlcustomize/customize-options.pod:__CUSTOMIZE_OPTIONS__ \
+	  --insert $(top_srcdir)/common/mlcustomize/v2v-customize-options.pod:__CUSTOMIZE_OPTIONS__ \
 	  --license GPLv2+ \
 	  --warning safe \
 	  $<

in-place/in_place.ml
@@ -182,7 +182,8 @@ let rec main () =
   ] in
 
   (* Append virt-customize options. *)
-  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec, get_customize_ops =
+    Customize_cmdline.argspec ~v2v:true () in
   let customize_argspec =
     List.map (fun (spec, _, _) -> spec) customize_argspec in
   let argspec = argspec @ customize_argspec in

inspector/inspector.ml
@@ -185,7 +185,8 @@ let rec main () =
   ] in
 
   (* Append virt-customize options. *)
-  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec, get_customize_ops =
+    Customize_cmdline.argspec ~v2v:true () in
   let customize_argspec =
     List.map (fun (spec, _, _) -> spec) customize_argspec in
   let argspec = argspec @ customize_argspec in

v2v/v2v.ml
@@ -314,7 +314,8 @@ let rec main () =
   ] in
 
   (* Append virt-customize options. *)
-  let customize_argspec, get_customize_ops = Customize_cmdline.argspec () in
+  let customize_argspec, get_customize_ops =
+    Customize_cmdline.argspec ~v2v:true () in
   let customize_argspec =
     List.map (fun (spec, _, _) -> spec) customize_argspec in
   let argspec = argspec @ customize_argspec in

docs/test-v2v-docs.sh
@@ -39,9 +39,6 @@ virt_customize_options=\
 --firstboot-command,\
 --firstboot-install,\
 --hostname,\
---inject-blnsvr,\
---inject-qemu-ga,\
---inject-virtio-win,\
 --install,\
 --link,\
 --mkdir,\