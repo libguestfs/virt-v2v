libguestfs/virt-v2v

commit c992fcfdece6d8d7d2e333fd21a0dc31bfa1c623

bugs-in-changelog.sh: List Jira IDs and github issues in output
(cherry picked from libguestfs commit e97c6f59336f4bf2d710135710d41915052e0a53)

bugs-in-changelog.sh
@@ -57,3 +57,31 @@ bugzilla \
         sprintf ("=item L<https://bugzilla.redhat.com/%s>\n\n%s\n",
                  $1, $2)
         }xe'
+
+# We can't fetch Jira subjects or github issues, but we can at least
+# list them.
+jiraids=$(
+    git log "$1" |
+    grep -Eio 'RHEL-[0-9]+' |
+    sort -u
+)
+
+for id in $jiraids ; do
+    echo "=item L<https://issues.redhat.com/browse/$id>"
+    echo
+    echo "XXX"
+    echo
+done
+
+issues=$(
+    git log "$1" |
+    grep -Eio 'https?://github\.com/libguestfs/libguestfs/issues/[0-9]+' |
+    sort -u
+)
+
+for issue in $issues ; do
+    echo "=item L<$issue>"
+    echo
+    echo "XXX"
+    echo
+done

commit 83f4d06ae4b5eae2a46910af0e9ff555c3cd4956

bugs-in-changelog.sh: Fix github issue tracker URL
Fixes: commit c992fcfdece6d8d7d2e333fd21a0dc31bfa1c623

bugs-in-changelog.sh
@@ -75,7 +75,7 @@ done
 
 issues=$(
     git log "$1" |
-    grep -Eio 'https?://github\.com/libguestfs/libguestfs/issues/[0-9]+' |
+    grep -Eio 'https?://github\.com/libguestfs/virt-v2v/issues/[0-9]+' |
     sort -u
 )
 