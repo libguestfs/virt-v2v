libguestfs/virt-v2v

commit 095bf10035914a75aac574185afb54de1e68c895

-o libvirt: Use <os firmware='efi'> finally
Libvirt has long supported <os firmware='efi'>, so use that in
preference to hard coding UEFI paths in the libvirt output.

See: https://libvirt.org/formatdomain.html#bios-bootloader
Fixes: https://github.com/libguestfs/virt-v2v/issues/45

output/create_libvirt_xml.ml
@@ -1,5 +1,5 @@
 (* virt-v2v
- * Copyright (C) 2009-2020 Red Hat Inc.
+ * Copyright (C) 2009-2024 Red Hat Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -218,23 +218,6 @@ let create_libvirt_xml ?pool source inspect
 
   List.push_back_list body [ e "cpu" !cpu_attrs !cpu ];
 
-  let uefi_firmware =
-    match target_firmware with
-    | TargetBIOS -> None
-    | TargetUEFI -> Some (find_uefi_firmware guestcaps.gcaps_arch) in
-  let machine, secure_boot_required =
-    match guestcaps.gcaps_machine, uefi_firmware with
-    | _, Some { Uefi.flags = flags }
-         when List.mem Uefi.UEFI_FLAG_SECURE_BOOT_REQUIRED flags ->
-       (* Force machine type to Q35 because PC does not support
-        * secure boot.  We must remove this when we get the
-        * correct machine type from libosinfo in future. XXX
-        *)
-       Q35, true
-    | machine, _ ->
-       machine, false in
-  let smm = secure_boot_required in
-
   (* We have the machine features of the guest when it was on the
    * source hypervisor (source.s_features).  We have the set of
    * hypervisor features supported by the target (target_features).
@@ -257,13 +240,6 @@ let create_libvirt_xml ?pool source inspect
     StringSet.inter(*section*) force_features target_features in
   let features = StringSet.union features force_features in
 
-  (* Add <smm> feature if UEFI requires it.  Note that libvirt
-   * capabilities doesn't list this feature even if it is supported
-   * by qemu, so we have to blindly add it, which might cause libvirt
-   * to fail. (XXX)
-   *)
-  let features = if smm then StringSet.add "smm" features else features in
-
   let features = List.sort compare (StringSet.elements features) in
 
   List.push_back_list body [
@@ -274,8 +250,13 @@ let create_libvirt_xml ?pool source inspect
   let os_section =
     let os = ref [] in
 
+    let firmware =
+      match target_firmware with
+      | TargetBIOS -> []
+      | TargetUEFI -> [ "firmware", "efi" ] in
+
     let machine =
-      match machine with
+      match guestcaps.gcaps_machine with
       | I440FX -> "pc"
       | Q35 -> "q35"
       | Virt -> "virt" in
@@ -285,18 +266,11 @@ let create_libvirt_xml ?pool source inspect
                               "machine", machine]
                       [PCData "hvm"]);
 
-    let loader =
-      match uefi_firmware with
-      | None -> []
-      | Some { Uefi.code = code; vars = vars_template } ->
-         let secure =
-           if secure_boot_required then [ "secure", "yes" ] else [] in
-         [ e "loader" (["readonly", "yes"; "type", "pflash"] @ secure)
-             [ PCData code ];
-           e "nvram" ["template", vars_template] [] ] in
-
-    List.push_back_list os loader;
-    e "os" [] !os in
+    (* We used to sometimes put <loader secure='yes'/> here.  However
+     * that is likely wrong since by its nature virt-v2v can't really
+     * support secure boot ever.  So for now we omit it.
+     *)
+    e "os" firmware !os in
 
   (* The <clock> section. *)
   let clock_section =

output/output_disk.ml
@@ -1,5 +1,5 @@
 (* virt-v2v
- * Copyright (C) 2009-2021 Red Hat Inc.
+ * Copyright (C) 2009-2024 Red Hat Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -91,17 +91,6 @@ module Disk = struct
 
   let finalize dir options () source inspect target_meta =
     let _, output_alloc, output_format, output_name, output_storage = options in
-
-    (* Convert metadata to libvirt XML. *)
-    (match target_meta.target_firmware with
-     | TargetBIOS -> ()
-     | TargetUEFI ->
-        (* XXX Can remove this method when libvirt supports
-         * <loader type="efi"/> since then it will be up to
-         * libvirt to check this.
-         *)
-        error_unless_uefi_firmware target_meta.guestcaps.gcaps_arch
-    );
 
     (* We don't know what target features the hypervisor supports, but
      * assume a common set that libvirt supports.

output/output_libvirt.ml
@@ -1,5 +1,5 @@
 (* virt-v2v
- * Copyright (C) 2009-2021 Red Hat Inc.
+ * Copyright (C) 2009-2024 Red Hat Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -150,16 +150,6 @@ module Libvirt_ = struct
     let conn, _, output_alloc, output_format, output_name, output_pool =
       options in
     let capabilities_xml, pool_name = t in
-
-    (match target_meta.target_firmware with
-     | TargetBIOS -> ()
-     | TargetUEFI ->
-        (* XXX Can remove this method when libvirt supports
-         * <loader type="efi"/> since then it will be up to
-         * libvirt to check this.
-         *)
-        error_unless_uefi_firmware target_meta.guestcaps.gcaps_arch
-    );
 
     let conn = Lazy.force conn in
 

commit edbbf51200ab17b8573bdee051b6021675d4f741

-o libvirt: Remove incorrect comment about secure boot
Fixes: commit 095bf10035914a75aac574185afb54de1e68c895

output/create_libvirt_xml.ml
@@ -294,10 +294,6 @@ let create_libvirt_xml ?pool source inspect
                               "machine", machine]
                       [PCData "hvm"]);
 
-    (* We used to sometimes put <loader secure='yes'/> here.  However
-     * that is likely wrong since by its nature virt-v2v can't really
-     * support secure boot ever.  So for now we omit it.
-     *)
     e "os" firmware_attribute !os in
 
   (* The <clock> section. *)