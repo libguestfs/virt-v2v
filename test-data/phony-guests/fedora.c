453e38ce02118fc81fe9ec9ca8f81ae341c93fe7
Richard W.M. Jones <rjones@redhat.com>
2025-02-19 15:15:30 +0000

    test-data: phony fedora: Add simple static /bin/sh
    This is able to do enough to run commands via g#sh and related calls
    inside the phony Fedora image.
    
    (cherry picked from
    guestfs-tools commit 4c5854f54e1da0d96807acb1b047bbf34694a0bb)
    
    (cherry picked from commit de073524ec9fbfa179f2721c36013b2bb312e73f)

--- test-data/phony-guests/fedora.c
+++ test-data/phony-guests/fedora.c
@@ -16,13 +16,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/* This is "just enough" of a binary to look like RPM and dracut, as
- * far as virt-v2v is concerned.
+/* This is "just enough" of a binary to look like /bin/sh, RPM and
+ * dracut, as far as virt-v2v is concerned.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /* NB: This is also defined in make-fedora-img.pl */
 #define KVER "5.19.0-0.rc1.14.fc37.x86_64"
@@ -32,6 +33,27 @@ get_basename (const char *str)
 {
   const char *ret = strrchr (str, '/');
   return ret == NULL ? str : ret + 1;
+}
+
+static void
+add_str (char ***argv, size_t *argc, char *str)
+{
+  (*argc)++;
+  (*argv) = realloc (*argv, *argc * sizeof (char *));
+  (*argv)[*argc-1] = str;
+}
+
+static void
+add_null (char ***argv, size_t *argc)
+{
+  add_str (argv, argc, NULL);
+}
+
+static void
+add (char ***argv, size_t *argc, const char *s, size_t len)
+{
+  char *copy = strndup (s, len);
+  add_str (argv, argc, copy);
 }
 
 int
@@ -57,10 +79,54 @@ main (int argc, char *argv[])
            strcmp (get_basename (argv[0]), "dracut") == 0) {
     // do nothing, pretend to rebuild the initramfs
   }
+  else if (argc == 3 &&
+           strcmp (get_basename (argv[0]), "sh") == 0 &&
+           strcmp (argv[1], "-c") == 0) {
+    /* Split the command and execute it.  Only handles trivial cases. */
+    char *cmd = argv[2];
+    char **cmdv = NULL;
+    size_t i, cmdvlen = 0, n;
+    const size_t len = strlen (cmd);
+
+    for (i = 0; i < len;) {
+      switch (cmd[i]) {
+      case ' ': case '\t':
+        i++;
+        continue;
+
+      case '"':
+        n = strcspn (&cmd[i+1], "\"");
+        add (&cmdv, &cmdvlen, &cmd[i+1], n);
+        i += n+2;
+        break;
+
+      case '\'':
+        n = strcspn (&cmd[i+1], "'");
+        add (&cmdv, &cmdvlen, &cmd[i+1], n);
+        i += n+2;
+        break;
+
+      default:
+        n = strcspn (&cmd[i], " \t");
+        add (&cmdv, &cmdvlen, &cmd[i], n);
+        i += n;
+      }
+    }
+    add_null (&cmdv, &cmdvlen);
+
+    execvp (cmdv[0], cmdv);
+    perror (cmdv[0]);
+    exit (EXIT_FAILURE);
+  }
   else {
-    fprintf (stderr, "phony Fedora: unknown command\n");
-    exit (1);
+    int i;
+
+    fprintf (stderr, "ERROR: test-data/phony-guests/fedora.c: "
+             "unexpected command:\n");
+    for (i = 0; i < argc; ++i)
+      fprintf (stderr, "argv[%d] = %s\n", i, argv[i]);
+    exit (EXIT_FAILURE);
   }
 
-  exit (0);
+  exit (EXIT_SUCCESS);
 }
