9bdfa5cb785b31e74e165299dda14ead8b36779f
Cole Robinson <crobinso@redhat.com>
2025-03-12 10:58:35 -0400

    tests: add test-phony-win*.sh infrastructure
    This will make it easier to add testing for multiple phony windows
    versions. Demonstrate this with win2k22.img
    
    The test case is similar to test-windows-conversion.sh, but will also
    verify that the expected osinfo-db value is detected.
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

---
+++ tests/test-windows-phony.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -
+# libguestfs virt-v2v test script
+# Copyright (C) 2014 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test virt-v2v (Phony) Windows conversion.
+
+set -e
+
+source ./functions.sh
+set -e
+set -x
+
+guestname="$1"
+if [ -z "$guestname" ]; then
+    echo "$script: guestname parameter not set, don't run this test directly."
+    exit 1
+fi
+f=../test-data/phony-guests/$guestname.img
+
+skip_if_skipped $script
+requires test -s $f
+
+export VIRT_TOOLS_DATA_DIR="$srcdir/../test-data/fake-virt-tools"
+export VIRTIO_WIN="$srcdir/../test-data/fake-virtio-win/drivers"
+
+d=test-phony-$guestname.d
+rm -rf $d
+cleanup_fn rm -r $d
+mkdir $d
+
+$VG virt-v2v --debug-gc \
+    -i disk $f \
+    -o local -os $d
+
+# Test the libvirt XML metadata and a disk was created.
+test -f $d/$guestname.xml
+test -f $d/$guestname-sda
+
+# Test some aspects of the target disk image.
+script="$d/test.fish"
+expected="$d/expected"
+response="$d/response"
+
+mktest ()
+{
+    local cmd="$1" exp="$2"
+
+    echo "echo '$cmd'" >> "$script"
+    echo "$cmd" >> "$expected"
+
+    echo "$cmd" >> "$script"
+    echo "$exp" >> "$expected"
+}
+
+:> "$script"
+:> "$expected"
+
+firstboot_dir="/Program Files/Guestfs/Firstboot"
+mktest "is-dir \"$firstboot_dir\"" true
+mktest "is-file \"$firstboot_dir/firstboot.bat\"" true
+mktest "is-dir \"$firstboot_dir/scripts\"" true
+virtio_dir="/Windows/Drivers/VirtIO"
+mktest "ls \"$virtio_dir\"" "$(cat test-phony-$guestname-ls.txt)"
+mktest "inspect-get-osinfo /dev/sda2" "$guestname"
+
+guestfish --ro -a "$d/$guestname-sda" -i < "$script" > "$response"
+diff -u "$expected" "$response"
