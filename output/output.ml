libguestfs/virt-v2v

commit d50966c2a480bda033f6e63bb797f86c13d576bd

lib: Make Nbdkit mini-library handle mutable
Previously the Nbdkit.cmd handle was immutable, which is nice from a
purity point of view but made for a lot of boilerplate.  If we make it
mutable we can make the code a little smaller and easier to read and
write.

There is no significant change here and this is mostly refactoring.
However I also cleaned up the way environment variables are done
(LANG=C was duplicated before), and also the handling of the verbose
flag.

output/output.ml
@@ -92,16 +92,12 @@ let output_to_local_file ?(changeuid = fun f -> f ())
 
   match output_format with
   | "raw" ->
-     let cmd = Nbdkit.new_cmd in
-     let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-     let cmd = Nbdkit.set_plugin cmd "file" in
-     let cmd = Nbdkit.add_arg cmd "file" filename in
-     let cmd =
-       if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
-         let cmd = Nbdkit.add_arg cmd "cache" "none" in
-         cmd
-       )
-       else cmd in
+     let cmd = Nbdkit.create "file" in
+     Nbdkit.add_arg cmd "file" filename;
+     if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
+       let cmd = Nbdkit.add_arg cmd "cache" "none" in
+       cmd
+     );
      let _, pid = Nbdkit.run_unix ~socket cmd in
 
      (* --exit-with-parent should ensure nbdkit is cleaned

input/input_disk.ml
@@ -98,18 +98,13 @@ let rec disk_source dir options args =
 
       match input_format with
       | "raw" ->
-         let cmd = Nbdkit.new_cmd in
-         let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-         let cmd = Nbdkit.set_plugin cmd "file" in
-         let cmd = Nbdkit.add_filter cmd "cow" in
-         let cmd = Nbdkit.add_arg cmd "file" disk in
-         let cmd =
-           if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
-             let cmd = Nbdkit.add_arg cmd "fadvise" "sequential" in
-             let cmd = Nbdkit.add_arg cmd "cache" "none" in
-             cmd
-           )
-           else cmd in
+         let cmd = Nbdkit.create "file" in
+         Nbdkit.add_filter cmd "cow";
+         Nbdkit.add_arg cmd "file" disk;
+         if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
+           Nbdkit.add_arg cmd "fadvise" "sequential";
+           Nbdkit.add_arg cmd "cache" "none";
+         );
          let _, pid = Nbdkit.run_unix ~socket cmd in
 
          (* --exit-with-parent should ensure nbdkit is cleaned

input/input_libvirt.ml
@@ -72,12 +72,10 @@ and libvirt_servers dir disks =
       match d_type with
       (* Forward to another NBD server using nbdkit-nbd-plugin. *)
       | NBD (hostname, port) ->
-         let cmd = Nbdkit.new_cmd in
-         let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-         let cmd = Nbdkit.set_plugin cmd "nbd" in
-         let cmd = Nbdkit.add_filter cmd "cow" in
-         let cmd = Nbdkit.add_arg cmd "hostname" hostname in
-         let cmd = Nbdkit.add_arg cmd "port" (string_of_int port) in
+         let cmd = Nbdkit.create "nbd" in
+         Nbdkit.add_filter cmd "cow";
+         Nbdkit.add_arg cmd "hostname" hostname;
+         Nbdkit.add_arg cmd "port" (string_of_int port);
          let _, pid = Nbdkit.run_unix ~socket cmd in
 
          (* --exit-with-parent should ensure nbdkit is cleaned
@@ -99,18 +97,13 @@ and libvirt_servers dir disks =
       | BlockDev filename | LocalFile filename ->
          match format with
          | Some "raw" ->
-            let cmd = Nbdkit.new_cmd in
-            let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-            let cmd = Nbdkit.set_plugin cmd "file" in
-            let cmd = Nbdkit.add_filter cmd "cow" in
-            let cmd = Nbdkit.add_arg cmd "file" filename in
-            let cmd =
-              if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
-                let cmd = Nbdkit.add_arg cmd "fadvise" "sequential" in
-                let cmd = Nbdkit.add_arg cmd "cache" "none" in
-                cmd
-              )
-              else cmd in
+            let cmd = Nbdkit.create "file" in
+            Nbdkit.add_filter cmd "cow";
+            Nbdkit.add_arg cmd "file" filename;
+            if Nbdkit.version nbdkit_config >= (1, 22, 0) then (
+              Nbdkit.add_arg cmd "fadvise" "sequential";
+              Nbdkit.add_arg cmd "cache" "none";
+            );
             let _, pid = Nbdkit.run_unix ~socket cmd in
 
             (* --exit-with-parent should ensure nbdkit is cleaned

input/nbdkit_curl.ml
@@ -52,75 +52,61 @@ let create_curl ?bandwidth ?cookie_script ?cookie_script_renew ?cor
   );
 
   (* Construct the nbdkit command. *)
-  let cmd = Nbdkit.new_cmd in
-  let cmd = Nbdkit.set_plugin cmd "curl" in
-
-  (* Environment.  We always add LANG=C. *)
-  let cmd = Nbdkit.add_env cmd "LANG" "C" in
-
-  (* Other flags. *)
-  let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-
-  let cmd = Nbdkit.add_arg cmd "url" url in
+  let cmd = Nbdkit.create "curl" in
+  Nbdkit.add_arg cmd "url" url;
 
   (* https://bugzilla.redhat.com/show_bug.cgi?id=1146007#c10 *)
-  let cmd = Nbdkit.add_arg cmd "timeout" "2000" in
-  let cmd =
-    match cookie_script with
-    | Some s -> Nbdkit.add_arg cmd "cookie-script" s
-    | None -> cmd in
-  let cmd =
-    match cookie_script_renew with
-    | Some i -> Nbdkit.add_arg cmd "cookie-script-renew" (string_of_int i)
-    | None -> cmd in
-  let cmd = if not sslverify then Nbdkit.add_arg cmd "sslverify" "false"
-            else cmd in
+  Nbdkit.add_arg cmd "timeout" "2000";
+  (match cookie_script with
+   | Some s -> Nbdkit.add_arg cmd "cookie-script" s
+   | None -> ());
+  (match cookie_script_renew with
+   | Some i -> Nbdkit.add_arg cmd "cookie-script-renew" (string_of_int i)
+   | None -> ());
+  if not sslverify then Nbdkit.add_arg cmd "sslverify" "false";
 
   (* For lots of extra debugging, uncomment one or both lines. *)
-  (* let cmd = Nbdkit.add_arg cmd "--debug" "curl.verbose=1" in *)
-  (* let cmd = Nbdkit.add_arg cnd "--debug" "curl.scripts=1" in *)
+  (* Nbdkit.add_arg cmd "--debug" "curl.verbose=1"; *)
+  (* Nbdkit.add_arg cnd "--debug" "curl.scripts=1"; *)
 
   (* Retry filter (if it exists) can be used to get around brief
    * interruptions in service.  It must be closest to the plugin.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "retry" in
+  Nbdkit.add_filter_if_available cmd "retry";
 
   (* Caching extents speeds up qemu-img, especially its consecutive
    * block_status requests with req_one=1.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "cacheextents" in
+  Nbdkit.add_filter_if_available cmd "cacheextents";
 
   (* IMPORTANT! Add the COW filter.  It must be furthest away
    * except for the rate filter.
    *)
-  let cmd = Nbdkit.add_filter cmd "cow" in
+  Nbdkit.add_filter cmd "cow";
 
   (* Add the cow-on-read flag if supported. *)
-  let cmd =
-    match cor with
-    | None -> cmd
-    | Some cor ->
-       if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
-         Nbdkit.add_arg cmd "cow-on-read" cor
-       else cmd in
+  (match cor with
+   | None -> ()
+   | Some cor ->
+      if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
+        Nbdkit.add_arg cmd "cow-on-read" cor
+  );
 
   (* Add the rate filter.  This must be furthest away so that
    * we don't end up rate-limiting internal nbdkit operations.
    *)
-  let cmd =
-    if Nbdkit.probe_filter "rate" then (
-      match bandwidth with
-      | None -> cmd
-      | Some bandwidth ->
-         let cmd = Nbdkit.add_filter cmd "rate" in
-         match bandwidth with
-         | StaticBandwidth rate ->
-            Nbdkit.add_arg cmd "rate" rate
-         | DynamicBandwidth (None, filename) ->
-            Nbdkit.add_arg cmd "rate-file" filename
-         | DynamicBandwidth (Some rate, filename) ->
-            Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
-    )
-    else cmd in
+  if Nbdkit.probe_filter "rate" then (
+    match bandwidth with
+    | None -> ()
+    | Some bandwidth ->
+       Nbdkit.add_filter cmd "rate";
+       match bandwidth with
+       | StaticBandwidth rate ->
+          Nbdkit.add_arg cmd "rate" rate
+       | DynamicBandwidth (None, filename) ->
+          Nbdkit.add_arg cmd "rate-file" filename
+       | DynamicBandwidth (Some rate, filename) ->
+          Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
+  );
 
   cmd

input/nbdkit_ssh.ml
@@ -58,93 +58,83 @@ let create_ssh ?bandwidth ?cor ~password ?port ~server ?user path =
   error_unless_nbdkit_min_version config;
 
   (* Construct the nbdkit command. *)
-  let cmd = Nbdkit.new_cmd in
-  let cmd = Nbdkit.set_plugin cmd "ssh" in
-
-  (* Other flags. *)
-  let cmd = Nbdkit.set_verbose cmd (verbose ()) in
-
-  let cmd = Nbdkit.add_arg cmd "host" server in
-  let cmd = Nbdkit.add_arg cmd "path" path in
-  let cmd =
-    match port with
-    | Some s -> Nbdkit.add_arg cmd "port" s
-    | None -> cmd in
-  let cmd =
-    match user with
-    | Some s -> Nbdkit.add_arg cmd "user" s
-    | None -> cmd in
+  let cmd = Nbdkit.create "ssh" in
+  Nbdkit.add_arg cmd "host" server;
+  Nbdkit.add_arg cmd "path" path;
+  (match port with
+   | Some s -> Nbdkit.add_arg cmd "port" s
+   | None -> ());
+  (match user with
+   | Some s -> Nbdkit.add_arg cmd "user" s
+   | None -> ());
 
   (* Retry filter (if it exists) can be used to get around brief
    * interruptions in service.  It must be closest to the plugin.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "retry" in
+  Nbdkit.add_filter_if_available cmd "retry";
 
   (* Caching extents speeds up qemu-img, especially its consecutive
    * block_status requests with req_one=1.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "cacheextents" in
+  Nbdkit.add_filter_if_available cmd "cacheextents";
 
   (* IMPORTANT! Add the COW filter.  It must be furthest away
    * except for the rate filter.
    *)
-  let cmd = Nbdkit.add_filter cmd "cow" in
+  Nbdkit.add_filter cmd "cow";
 
   (* Add the cow-on-read flag if supported. *)
-  let cmd =
-    match cor with
-    | None -> cmd
-    | Some cor ->
-       if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
-         Nbdkit.add_arg cmd "cow-on-read" cor
-       else cmd in
+  (match cor with
+   | None -> ()
+   | Some cor ->
+      if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
+        Nbdkit.add_arg cmd "cow-on-read" cor
+  );
 
   (* Add the rate filter.  This must be furthest away so that
    * we don't end up rate-limiting internal nbdkit operations.
    *)
-  let cmd =
-    if Nbdkit.probe_filter "rate" then (
-      match bandwidth with
-      | None -> cmd
-      | Some bandwidth ->
-         let cmd = Nbdkit.add_filter cmd "rate" in
-         match bandwidth with
-         | StaticBandwidth rate ->
-            Nbdkit.add_arg cmd "rate" rate
-         | DynamicBandwidth (None, filename) ->
-            Nbdkit.add_arg cmd "rate-file" filename
-         | DynamicBandwidth (Some rate, filename) ->
-            Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
-    )
-    else cmd in
+  if Nbdkit.probe_filter "rate" then (
+    match bandwidth with
+    | None -> ()
+    | Some bandwidth ->
+       Nbdkit.add_filter cmd "rate";
+       match bandwidth with
+       | StaticBandwidth rate ->
+          Nbdkit.add_arg cmd "rate" rate
+       | DynamicBandwidth (None, filename) ->
+          Nbdkit.add_arg cmd "rate-file" filename
+       | DynamicBandwidth (Some rate, filename) ->
+          Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
+  );
 
   (* Handle the password parameter specially. *)
-  let cmd =
-    match password with
-    | NoPassword -> cmd
-    | AskForPassword ->
-       (* Because we will start nbdkit in the background and then wait
-        * for 30 seconds for it to start up, we cannot use the
-        * password=- feature of nbdkit to read the password
-        * interactively (since in the words of the movie the user has
-        * only "30 seconds to comply").  In any case this feature broke
-        * in the VDDK plugin in nbdkit 1.18 and 1.20.  So in the
-        * AskForPassword case we read the password here.
-        *)
-       printf "password: ";
-       let open Unix in
-       let orig = tcgetattr stdin in
-       let tios = { orig with c_echo = false } in
-       tcsetattr stdin TCSAFLUSH tios; (* Disable echo. *)
-       let password = read_line () in
-       tcsetattr stdin TCSAFLUSH orig; (* Restore echo. *)
-       printf "\n";
-       let password_file = Filename.temp_file "v2vnbdkit" ".txt" in
-       On_exit.unlink password_file;
-       with_open_out password_file (fun chan -> output_string chan password);
-       Nbdkit.add_arg cmd "password" ("+" ^ password_file)
-    | PasswordFile password_file ->
-       Nbdkit.add_arg cmd "password" ("+" ^ password_file) in
+  (match password with
+   | NoPassword -> ()
+   | AskForPassword ->
+      (* Because we will start nbdkit in the background and then wait
+       * for 30 seconds for it to start up, we cannot use the
+       * password=- feature of nbdkit to read the password
+       * interactively (since in the words of the movie the user has
+       * only "30 seconds to comply").  In any case this feature broke
+       * in the VDDK plugin in nbdkit 1.18 and 1.20.  So in the
+       * AskForPassword case we read the password here.
+       *)
+      printf "password: ";
+      let open Unix in
+      let orig = tcgetattr stdin in
+      let tios = { orig with c_echo = false } in
+      tcsetattr stdin TCSAFLUSH tios; (* Disable echo. *)
+      let password = read_line () in
+      tcsetattr stdin TCSAFLUSH orig; (* Restore echo. *)
+      printf "\n";
+      let password_file = Filename.temp_file "v2vnbdkit" ".txt" in
+      On_exit.unlink password_file;
+      with_open_out password_file (fun chan -> output_string chan password);
+      Nbdkit.add_arg cmd "password" ("+" ^ password_file)
+   | PasswordFile password_file ->
+      Nbdkit.add_arg cmd "password" ("+" ^ password_file)
+  );
 
   cmd
 

input/nbdkit_vddk.ml
@@ -103,28 +103,21 @@ See also the virt-v2v-input-vmware(1) manual.") libNN
   error_unless_nbdkit_vddk_working ();
 
   (* Construct the nbdkit command. *)
-  let cmd = Nbdkit.new_cmd in
-  let cmd = Nbdkit.set_plugin cmd "vddk" in
-
-  (* Environment.  We always add LANG=C. *)
-  let cmd = Nbdkit.add_env cmd "LANG" "C" in
+  let cmd = Nbdkit.create "vddk" in
 
   (* Suppress datapath messages. *)
-  let cmd = Nbdkit.add_debug_flag cmd "vddk.datapath" "0" in
+  Nbdkit.add_debug_flag cmd "vddk.datapath" "0";
 
   (* Enable VDDK stats. *)
-  let cmd = Nbdkit.add_debug_flag cmd "vddk.stats" "1" in
+  Nbdkit.add_debug_flag cmd "vddk.stats" "1";
 
-  (* Other flags. *)
-  let cmd = Nbdkit.set_verbose cmd (verbose ()) in
+  Nbdkit.add_arg cmd "server" server;
+  Nbdkit.add_arg cmd "vm" (sprintf "moref=%s" moref);
+  Nbdkit.add_arg cmd "file" path;
 
   (* For VDDK we require some user.  If it's not supplied, assume root. *)
   let user = Option.default "root" user in
-
-  let cmd = Nbdkit.add_arg cmd "server" server in
-  let cmd = Nbdkit.add_arg cmd "user" user in
-  let cmd = Nbdkit.add_arg cmd "vm" (sprintf "moref=%s" moref) in
-  let cmd = Nbdkit.add_arg cmd "file" path in
+  Nbdkit.add_arg cmd "user" user;
 
   let password =
     match password_file with
@@ -132,33 +125,29 @@ See also the virt-v2v-input-vmware(1) manual.") libNN
     | Some password_file -> PasswordFile password_file in
 
   (* The passthrough parameters. *)
-  let passthru cmd name v =
-    match v with
-    | Some s -> Nbdkit.add_arg cmd name s
-    | None -> cmd
-  in
-  let cmd = passthru cmd "config" config in
-  let cmd = passthru cmd "libdir" libdir in
-  let cmd = passthru cmd "nfchostport" nfchostport in
-  let cmd = passthru cmd "port" port in
-  let cmd = passthru cmd "snapshot" snapshot in
-  let cmd = Nbdkit.add_arg cmd "thumbprint" thumbprint in
-  let cmd = passthru cmd "transports" transports in
+  let passthru cmd name v = Option.may (Nbdkit.add_arg cmd name) v in
+  passthru cmd "config" config;
+  passthru cmd "libdir" libdir;
+  passthru cmd "nfchostport" nfchostport;
+  passthru cmd "port" port;
+  passthru cmd "snapshot" snapshot;
+  Nbdkit.add_arg cmd "thumbprint" thumbprint; (* required *)
+  passthru cmd "transports" transports;
 
   (* Retry filter (if it exists) can be used to get around brief
    * interruptions in service.  It must be closest to the plugin.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "retry" in
+  Nbdkit.add_filter_if_available cmd "retry";
 
   (* Caching extents speeds up qemu-img, especially its consecutive
    * block_status requests with req_one=1.
    *)
-  let cmd = Nbdkit.add_filter_if_available cmd "cacheextents" in
+  Nbdkit.add_filter_if_available cmd "cacheextents";
 
   (* IMPORTANT! Add the COW filter.  It must be furthest away
    * except for the multi-conn and rate filters.
    *)
-  let cmd = Nbdkit.add_filter cmd "cow" in
+  Nbdkit.add_filter cmd "cow";
 
   (* The cow filter unconditionally enables multi-conn (because it is
    * safe).  However this causes an unintended consequence with the VDDK
@@ -172,75 +161,68 @@ See also the virt-v2v-input-vmware(1) manual.") libNN
    * multi-conn-mode=disable on top which disables multi-conn
    * advertisement.
    *)
-  let cmd =
-    if Nbdkit.probe_filter "multi-conn" then (
-      let cmd = Nbdkit.add_filter cmd "multi-conn" in
-      let cmd = Nbdkit.add_arg cmd "multi-conn-mode" "disable" in
-      cmd
-    )
-    else cmd in
+  if Nbdkit.probe_filter "multi-conn" then (
+    Nbdkit.add_filter cmd "multi-conn";
+    Nbdkit.add_arg cmd "multi-conn-mode" "disable";
+  );
 
   (* If the filter supports it, enable cow-block-size (added in
    * nbdkit 1.27.6).  This helps to reduce fragmentated small
    * extent and read requests.
    *)
-  let cmd =
-    if Nbdkit.probe_filter_parameter "cow" "cow-block-size" then
-      Nbdkit.add_arg cmd "cow-block-size" "4096"
-    else cmd in
+  if Nbdkit.probe_filter_parameter "cow" "cow-block-size" then
+    Nbdkit.add_arg cmd "cow-block-size" "4096";
 
   (* Add the cow-on-read flag if supported. *)
-  let cmd =
-    match cor with
-    | None -> cmd
-    | Some cor ->
-       if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
+  (match cor with
+   | None -> ()
+   | Some cor ->
+      if Nbdkit.probe_filter_parameter "cow" "cow-on-read=.*/PATH" then
          Nbdkit.add_arg cmd "cow-on-read" cor
-       else cmd in
+  );
 
   (* Add the rate filter.  This must be furthest away so that
    * we don't end up rate-limiting internal nbdkit operations.
    *)
-  let cmd =
-    if Nbdkit.probe_filter "rate" then (
-      match bandwidth with
-      | None -> cmd
-      | Some bandwidth ->
-         let cmd = Nbdkit.add_filter cmd "rate" in
-         match bandwidth with
-         | StaticBandwidth rate ->
-            Nbdkit.add_arg cmd "rate" rate
-         | DynamicBandwidth (None, filename) ->
-            Nbdkit.add_arg cmd "rate-file" filename
-         | DynamicBandwidth (Some rate, filename) ->
-            Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
-    )
-    else cmd in
+  if Nbdkit.probe_filter "rate" then (
+    match bandwidth with
+    | None -> ()
+    | Some bandwidth ->
+       Nbdkit.add_filter cmd "rate";
+       match bandwidth with
+       | StaticBandwidth rate ->
+          Nbdkit.add_arg cmd "rate" rate
+       | DynamicBandwidth (None, filename) ->
+          Nbdkit.add_arg cmd "rate-file" filename
+       | DynamicBandwidth (Some rate, filename) ->
+          Nbdkit.add_args cmd ["rate", rate; "rate-file", filename]
+  );
 
   (* Handle the password parameter specially. *)
-  let cmd =
-    match password with
-    | AskForPassword ->
-       (* Because we will start nbdkit in the background and then wait
-        * for 30 seconds for it to start up, we cannot use the
-        * password=- feature of nbdkit to read the password
-        * interactively (since in the words of the movie the user has
-        * only "30 seconds to comply").  In any case this feature broke
-        * in the VDDK plugin in nbdkit 1.18 and 1.20.  So in the
-        * AskForPassword case we read the password here.
-        *)
-       printf "password: ";
-       let open Unix in
-       let orig = tcgetattr stdin in
-       let tios = { orig with c_echo = false } in
-       tcsetattr stdin TCSAFLUSH tios; (* Disable echo. *)
-       let password = read_line () in
-       tcsetattr stdin TCSAFLUSH orig; (* Restore echo. *)
-       printf "\n";
-       let password_file = Filename.temp_file "v2vnbdkit" ".txt" in
-       On_exit.unlink password_file;
-       with_open_out password_file (fun chan -> output_string chan password);
-       Nbdkit.add_arg cmd "password" ("+" ^ password_file)
-    | PasswordFile password_file ->
-       Nbdkit.add_arg cmd "password" ("+" ^ password_file) in
+  (match password with
+   | AskForPassword ->
+      (* Because we will start nbdkit in the background and then wait
+       * for 30 seconds for it to start up, we cannot use the
+       * password=- feature of nbdkit to read the password
+       * interactively (since in the words of the movie the user has
+       * only "30 seconds to comply").  In any case this feature broke
+       * in the VDDK plugin in nbdkit 1.18 and 1.20.  So in the
+       * AskForPassword case we read the password here.
+       *)
+      printf "password: ";
+      let open Unix in
+      let orig = tcgetattr stdin in
+      let tios = { orig with c_echo = false } in
+      tcsetattr stdin TCSAFLUSH tios; (* Disable echo. *)
+      let password = read_line () in
+      tcsetattr stdin TCSAFLUSH orig; (* Restore echo. *)
+      printf "\n";
+      let password_file = Filename.temp_file "v2vnbdkit" ".txt" in
+      On_exit.unlink password_file;
+      with_open_out password_file (fun chan -> output_string chan password);
+      Nbdkit.add_arg cmd "password" ("+" ^ password_file)
+   | PasswordFile password_file ->
+      Nbdkit.add_arg cmd "password" ("+" ^ password_file)
+  );
+
   cmd

lib/nbdkit.ml
@@ -68,41 +68,39 @@ let probe_filter_parameter name regex =
   Sys.command cmd = 0
 
 type cmd = {
-  plugin : string option;
-  filters : string list;
-  args : (string * string) list; (* stored reversed *)
-  env : (string * string) list;
-  debug_flags : (string * string) list; (* stored reversed *)
-  readonly : bool;
-  threads : int;
+  plugin : string;
+  mutable filters : string list;
+  mutable args : (string * string) list; (* stored reversed *)
+  mutable env : (string * string) list;
+  mutable debug_flags : (string * string) list;
+  mutable readonly : bool;
+  mutable threads : int;
   verbose : bool;
 }
 
-let new_cmd = {
-  plugin = None;
+let create ?(quiet = false) plugin = {
+  plugin;
   filters = [];
   args = [];
   env = [ "LANG", "C" ];
   debug_flags = [];
   readonly = false;
   threads = 16;
-  verbose = false;
+  verbose = not quiet && verbose ()
 }
 
 let add_debug_flag cmd name value =
-  { cmd with debug_flags = (name, value) :: cmd.debug_flags }
+  cmd.debug_flags <- (name, value) :: cmd.debug_flags
 
-let set_readonly cmd v = { cmd with readonly = v }
-let set_threads cmd v = { cmd with threads = v }
-let set_verbose cmd v = { cmd with verbose = v }
-let set_plugin cmd v = { cmd with plugin = Some v }
-let add_filter cmd v = { cmd with filters = v :: cmd.filters }
-let add_arg cmd key value = { cmd with args = (key, value) :: cmd.args }
-let add_args cmd kvs = { cmd with args = List.rev kvs @ cmd.args }
-let add_env cmd name value = { cmd with env = (name, value) :: cmd.env }
+let set_readonly cmd v = cmd.readonly <- v
+let set_threads cmd v = cmd.threads <- v
+let add_filter cmd v = cmd.filters <- v :: cmd.filters
+let add_arg cmd key value = cmd.args <- (key, value) :: cmd.args
+let add_args cmd kvs = cmd.args <- List.rev kvs @ cmd.args
+let add_env cmd name value = cmd.env <- (name, value) :: cmd.env
 
 let add_filter_if_available cmd filter =
-  if probe_filter filter then add_filter cmd filter else cmd
+  if probe_filter filter then add_filter cmd filter
 
 let run_unix ?socket cmd =
   (* Create a temporary directory where we place the socket and PID file.
@@ -156,14 +154,12 @@ let run_unix ?socket cmd =
   List.iter (
     fun (name, value) ->
       add_arg "-D"; add_arg (sprintf "%s=%s" name value)
-  ) (List.rev cmd.debug_flags);
+  ) cmd.debug_flags;
   if cmd.readonly then add_arg "--readonly";
   if cmd.verbose then add_arg "--verbose";
   List.iter (fun filter -> add_arg "--filter"; add_arg filter) cmd.filters;
 
-  (match cmd.plugin with
-   | None -> assert false
-   | Some plugin -> add_arg plugin);
+  add_arg cmd.plugin;
   add_args_reversed (List.map (fun (k, v) -> sprintf "%s=%s" k v) cmd.args);
   let args = get_args () in
 

lib/nbdkit.mli
@@ -61,36 +61,35 @@ val probe_filter_parameter : string -> string -> bool
     in the output of [nbdkit --filter=filter null --help]. *)
 
 type cmd
-(** An nbdkit command line.  Note this type is immutable. *)
+(** An nbdkit command. *)
 
-val new_cmd : cmd
-(** Return an empty command line.  {!set_plugin} must be called. *)
+val create : ?quiet:bool -> string -> cmd
+(** Create a new nbdkit command.
 
-val add_debug_flag : cmd -> string -> string -> cmd
-val set_readonly : cmd -> bool -> cmd
-val set_threads : cmd -> int -> cmd
-val set_verbose : cmd -> bool -> cmd
+    The parameter is the required plugin name.  Normally the nbdkit
+    verbose ([-v]) flag is inherited from virt-v2v but exceptionally
+    you can use ~quiet:true to make nbdkit always quiet. *)
+
+val add_debug_flag : cmd -> string -> string -> unit
+val set_readonly : cmd -> bool -> unit
+val set_threads : cmd -> int -> unit
 (** Set various command line flags. *)
 
-val set_plugin : cmd -> string -> cmd
-(** Set the plugin name.  Use {!probe_plugin} first to check the
-    plugin is installed. *)
-
-val add_filter : cmd -> string -> cmd
+val add_filter : cmd -> string -> unit
 (** Add a filter.  Use {!probe_filter} first to check the filter
     is installed.  The filters are added closest to the plugin first. *)
 
-val add_filter_if_available : cmd -> string -> cmd
+val add_filter_if_available : cmd -> string -> unit
 (** Same as {!add_filter} but does the {!probe_filter} check and
     omits the filter if it's not available. *)
 
-val add_arg : cmd -> string -> string -> cmd
-val add_args : cmd -> (string * string) list -> cmd
+val add_arg : cmd -> string -> string -> unit
+val add_args : cmd -> (string * string) list -> unit
 (** Add a key=value argument(s) to the command line.
 
     The arguments are added left to right. *)
 
-val add_env : cmd -> string -> string -> cmd
+val add_env : cmd -> string -> string -> unit
 (** Add name=value environment variable. *)
 
 val run_unix : ?socket:string -> cmd -> string * int

commit 0be5d99ae71906d989cee3d48ada30fecd0f5888

output: Remove useless 'let cmd'
Commit d50966c2a4 ("lib: Make Nbdkit mini-library handle mutable")
made Nbdkit handles mutable, but didn't fix all instances where we
still used the pure handle.  Fix it here.

Fixes: commit d50966c2a480bda033f6e63bb797f86c13d576bd

output/output.ml
@@ -102,10 +102,8 @@ let output_to_local_file ?(changeuid = fun f -> f ()) ?(compressed = false)
     | "raw" ->
        let cmd = Nbdkit.create "file" in
        Nbdkit.add_arg cmd "file" filename;
-       if Nbdkit.version () >= (1, 22, 0) then (
-         let cmd = Nbdkit.add_arg cmd "cache" "none" in
-         cmd
-       );
+       if Nbdkit.version () >= (1, 22, 0) then
+         Nbdkit.add_arg cmd "cache" "none";
        let _, pid = Nbdkit.run_unix socket cmd in
        pid
 