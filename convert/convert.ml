libguestfs/virt-v2v

commit 78ffb68d4f1ef8af776c24537feaf832fd3ec7e9

convert; Run a filesystem check before and after conversion
Check filesystem integrity using fsck or equivalents before and after
conversion.  If the filesystem is broken then we fail the conversion,
as it would be unsafe.

This is currently implemented for btrfs, ext4 and xfs (but notably not
for NTFS).

Fixes: https://issues.redhat.com/browse/RHEL-91931

convert/convert.ml
@@ -81,6 +81,10 @@ let rec convert input_disks options source =
   (* Decrypt the disks. *)
   inspect_decrypt g options.ks;
 
+  (* Check (fsck) the filesystems before conversion. *)
+  message (f_"Checking filesystem integrity before conversion");
+  do_fsck g;
+
   (* Detect firmware. *)
   message (f_"Detecting if this guest uses BIOS or UEFI to boot");
   let i_firmware = Firmware.detect_firmware g in
@@ -109,6 +113,11 @@ let rec convert input_disks options source =
    *)
   message (f_"Mapping filesystem data to avoid copying unused and blank areas");
   do_fstrim g inspect;
+
+  (* Check (fsck) the filesystems after conversion. *)
+  g#umount_all ();
+  message (f_"Checking filesystem integrity after conversion");
+  do_fsck g;
 
   message (f_"Closing the overlay");
   g#umount_all ();
@@ -221,6 +230,43 @@ and do_fstrim g inspect =
                       \"Trimming\" in virt-v2v(1).\n\n\
                       Original message: %s") dev msg
       )
+  ) fses
+
+(* Perform fsck before and after conversion.
+ *
+ * If fsck returns an error then the conversion will fail.  We do not
+ * attempt to do any repairs.
+ *)
+and do_fsck g =
+  let fses = g#list_filesystems () in
+  List.iter (function
+      | dev, _ when String.starts_with "btrfsvol:" dev ->
+         (* Ignore btrfs volumes, since we should see the btrfs device
+          * somewhere else in the list and checking that is sufficient.
+          *)
+         ()
+
+      | dev, "btrfs" ->
+         (* btrfs-check would be the obvious thing, but the general
+          * opinion seems to be that it's broken, and you should use
+          * btrfs scrub instead.
+          *)
+         Fun.protect ~finally:g#umount_all (
+           fun () ->
+             g#mount_ro dev "/";
+             g#btrfs_scrub_full "/" ~readonly:true;
+         );
+
+      | dev, "ext4" ->
+         g#e2fsck dev (* ~correct:false is the default *)
+
+      | dev, "xfs" ->
+         if g#xfs_repair ~nomodify:true dev <> 0 then
+           error (f_"detected errors on the XFS filesystem on %s") dev
+
+      | _, _ ->
+         (* Ignore other filesystem types. *)
+         ()
   ) fses
 
 (* Conversion. *)

docs/virt-v2v-release-notes-2.8.pod
@@ -49,6 +49,12 @@ unnecessary debugging and SRIOV files.  Libosinfo is no longer used
 for this as the osinfo metadata used had grown out of date.  (Cole
 Robinson)
 
+We now check filesystem integrity (using tools like L<fsck(8)>) before
+and after conversion, to ensure both that we fully understand the
+filesystem before conversion and that no corruption has happened
+during conversion.  At the moment this is only implemented for Btrfs,
+ext4 and XFS.
+
 =head2 Features removed
 
 Handling of I<kmod-xenpv> has been removed.  This only applied to RHEL

m4/guestfs-libraries.m4
@@ -20,7 +20,8 @@ dnl Any C libraries required by virt-v2v.
 dnl Of course we need libguestfs.
 dnl
 dnl We need libguestfs 1.55.6 for guestfs_sh_out.
-PKG_CHECK_MODULES([LIBGUESTFS], [libguestfs >= 1.55.6])
+dnl We need libguestfs 1.55.12 for guestfs_btrfs_scrub_full.
+PKG_CHECK_MODULES([LIBGUESTFS], [libguestfs >= 1.55.12])
 printf "libguestfs version is "; $PKG_CONFIG --modversion libguestfs
 
 dnl And libnbd.

commit 2e7c03b967e2c5bbe01a0c9cc255238d19c46540

convert: Replay dirty log for XFS filesystems before running xfs_repair
Reported-by: Vera Wu
Thanks: Eric Sandeen, Ming Xie
Fixes: https://issues.redhat.com/browse/RHEL-95365
Fixes: commit 78ffb68d4f1ef8af776c24537feaf832fd3ec7e9

convert/convert.ml
@@ -83,7 +83,7 @@ let rec convert input_disks options source =
 
   (* Check (fsck) the filesystems before conversion. *)
   message (f_"Checking filesystem integrity before conversion");
-  do_fsck g;
+  do_fsck ~before:true g;
 
   (* Detect firmware. *)
   message (f_"Detecting if this guest uses BIOS or UEFI to boot");
@@ -237,7 +237,7 @@ and do_fstrim g inspect =
  * If fsck returns an error then the conversion will fail.  We do not
  * attempt to do any repairs.
  *)
-and do_fsck g =
+and do_fsck ?(before=false) g =
   let fses = g#list_filesystems () in
   List.iter (function
       | dev, _ when String.starts_with "btrfsvol:" dev ->
@@ -261,6 +261,16 @@ and do_fsck g =
          g#e2fsck ~forceno:true dev
 
       | dev, "xfs" ->
+         if before then (
+           (* xfs_repair cannot replay the dirty log, only the kernel can,
+            * so we must first mount then unmount the filesystem, and then
+            * we can run xfs_repair.  Unlike what is documented, xfs_repair
+            * doesn't return 2 in this case.  Mount r/o is fine as that
+            * will still replay the log (RHEL-95365)
+            *)
+           Fun.protect ~finally:g#umount_all (fun () -> g#mount_ro dev "/");
+         );
+
          if g#xfs_repair ~nomodify:true dev <> 0 then
            error (f_"detected errors on the XFS filesystem on %s") dev
 