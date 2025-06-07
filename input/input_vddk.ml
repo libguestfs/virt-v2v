libguestfs/virt-v2v

commit b49ee1436870f720cd0a4aee7593f38fa9f4089f

input: vddk: Use single nbdkit-vddk-plugin instance with exports
nbdkit >= 1.43.8 VDDK plugin supports a new 'export' parameter which
allows the client (virt-v2v) to select which file on the VMware server
to serve.  This allows us to run a single nbdkit instance (if nbdkit
has this feature), which is potentially more efficient for guests with
multiple disks.

This also requires nbdkit-cow-filter to be export-safe.  This change
was made in the same version of nbdkit, so checking for the new
'export' parameter is sufficient.

input/input_vddk.ml
@@ -410,34 +410,75 @@ See also the virt-v2v-input-vmware(1) manual.") libNN
       cmd
     in
 
-    (* Create an nbdkit instance for each disk. *)
-    let uris =
+    (* Collect all the VDDK filename(s) we will be accessing, one
+     * for each input disk.  This takes into account the
+     * '-io vddk-file' override.
+     *)
+    let files =
       List.combine disks file_overrides |>
-      List.mapi (
-        fun i ({ d_type }, file_override) ->
-          let socket = sprintf "%s/in%d" dir i in
-          On_exit.unlink socket;
+      List.map (
+        function
+        (* These should never happen? *)
+          | { d_type = BlockDev _ | NBD _ | HTTP _ }, _ ->
+             assert false
 
-          (match d_type with
-           | BlockDev _ | NBD _ | HTTP _ -> (* These should never happen? *)
-              assert false
+          | { d_type = LocalFile file }, None ->
+             (* The <source file=...> attribute returned by the libvirt
+              * VMX driver looks like "[datastore] path".  We can use it
+              * directly as the nbdkit file= parameter, and it is passed
+              * directly in this form to VDDK.
+              *)
+             file
 
-           | LocalFile orig_file ->
-              (* If -io vddk-file, override it here. *)
-              let file = Option.value file_override ~default:orig_file in
+          | { d_type = LocalFile _ }, Some file_override ->
+             (* If -io vddk-file, override it here. *)
+             file_override
+      ) in
+    assert (files <> []);
 
-              (* The <source file=...> attribute returned by the libvirt
-               * VMX driver looks like "[datastore] path".  We can use it
-               * directly as the nbdkit file= parameter, and it is passed
-               * directly in this form to VDDK.
-               *)
-              let nbdkit = create_nbdkit_vddk () in
-              Nbdkit.add_arg nbdkit "file" file;
-              let _, pid = Nbdkit.run_unix socket nbdkit in
-              On_exit.kill pid
-          );
+    let uris =
+      (* If nbdkit-vddk-plugin has the 'export' feature (added in
+       * nbdkit 1.43.8) then we only have to run a single
+       * instance of nbdkit.
+       *)
+      if Nbdkit.probe_plugin_parameter "vddk" "export=" then (
+        let wildcard =
+          match files with
+          | [] -> assert false (* can't happen, see assert above *)
+          | [f] -> f
+          | files ->
+             (* Calculate the longest common prefix across all the files,
+              * then set the wildcard to this.
+              * XXX May not work if there are subdirectories?
+              * XXX Is every file we want to read called *.vmdk?
+              *)
+             let prefix = String.longest_common_prefix files in
+             fnmatch_escape prefix ^ "*.vmdk" in
 
-          NBD_URI.Unix (socket, None)
+        let socket = sprintf "%s/in0" dir in
+        On_exit.unlink socket;
+
+        let nbdkit = create_nbdkit_vddk () in
+        Nbdkit.add_arg nbdkit "export" wildcard;
+        let _, pid = Nbdkit.run_unix socket nbdkit in
+        On_exit.kill pid;
+
+        (* Use NBD export names to select the right disk to read. *)
+        List.map (fun file -> NBD_URI.Unix (socket, Some file)) files
+      ) else (
+        (* Create an nbdkit instance for each disk. *)
+        List.mapi (
+          fun i file ->
+            let socket = sprintf "%s/in%d" dir i in
+            On_exit.unlink socket;
+
+            let nbdkit = create_nbdkit_vddk () in
+            Nbdkit.add_arg nbdkit "file" file;
+            let _, pid = Nbdkit.run_unix socket nbdkit in
+            On_exit.kill pid;
+
+            NBD_URI.Unix (socket, None)
+        ) files
       ) in
 
     source, uris

lib/utils.ml
@@ -54,6 +54,21 @@ let uri_quote str =
   done;
   String.concat "" (List.rev !xs)
 
+(* Escape characters like [?] and [*] which are special for fnmatch(3). *)
+let fnmatch_escape str =
+  let len = String.length str in
+  let xs = ref [] in
+  for i = 0 to len-1 do
+    xs :=
+      (match str.[i] with
+       | '[' | ']' | '?' | '*' as c ->
+          sprintf "\\%c" c
+       | c ->
+          String.make 1 c
+      ) :: !xs
+  done;
+  String.concat "" (List.rev !xs)
+
 (* Map guest architecture found by inspection to the architecture
  * that KVM must emulate.  Note for x86 we assume a 64 bit hypervisor.
  *)

lib/utils.mli
@@ -33,6 +33,9 @@ val have_selinux : bool
 val uri_quote : string -> string
 (** Take a string and perform %xx escaping as used in some parts of URLs. *)
 
+val fnmatch_escape : string -> string
+(** Escape characters like [?] and [*] which are special for fnmatch(3).  *)
+
 val kvm_arch : string -> string
 (** Map guest architecture found by inspection to the architecture
     that KVM must emulate.  Note for x86 we assume a 64 bit hypervisor. *)

commit 076727e55f4d4fed246097d3f89ebfe83e3de88f

input/input_vddk.ml: Escape export parameter even if only a single disk
nbdkit-vddk-plugin requires that the export parameter is wildcard-
escaped, in particular that '[' is escaped as '\['.  This error:

  nbdkit: vddk[1]: error: access denied: client requested "[esx8.0-matrix] esx8.0-rhel9.6-x86_64-efi/esx8.0-rhel9.6-x86_64-efi.vmdk" which does not match export="[esx8.0-matrix] esx8.0-rhel9.6-x86_64-efi/esx8.0-rhel9.6-x86_64-efi.vmdk"

was caused because we neglected to escape the parameter at all if
there was only one disk.

Fixes: commit b49ee1436870f720cd0a4aee7593f38fa9f4089f

input/input_vddk.ml
@@ -454,7 +454,8 @@ See also the virt-v2v-input-vmware(1) manual.") libNN
               * XXX Is every file we want to read called *.vmdk?
               *)
              let prefix = String.longest_common_prefix files in
-             fnmatch_escape prefix ^ "*.vmdk" in
+             prefix ^ "*.vmdk" in
+        let wildcard = fnmatch_escape wildcard in
 
         let socket = sprintf "%s/in0" dir in
         On_exit.unlink socket;