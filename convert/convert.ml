libguestfs/virt-v2v

commit ca6ec6317e20a633315f783a8ba4ece3c2fc01f2

convert: Detect target boot device for Linux guests
If the guest is Linux, try to detect the boot device, so we can set
<boot order='N'> appropriately.  We do this for BIOS or UEFI here, but
this only really matters for SeaBIOS.

Suggested-by: Gerd Hoffmann
Fixes: https://issues.redhat.com/browse/RHEL-108991

convert/convert.ml
@@ -94,6 +94,10 @@ let rec convert input_disks options source =
   let root = Choose_root.choose_root options.root_choice g in
   let inspect = Mount_filesystems.mount_filesystems g root in
 
+  (* Detect boot device. *)
+  message (f_"Detecting the boot device");
+  let target_boot_device = get_target_boot_device g inspect in
+
   let mpstats = get_mpstats g in
   check_guest_free_space inspect mpstats;
 
@@ -136,7 +140,7 @@ let rec convert input_disks options source =
 
   (* Create target metadata file. *)
   let target_meta = { guestcaps; target_buses; target_nics;
-                      target_firmware; target_boot_device = None } in
+                      target_firmware; target_boot_device } in
 
   (* This is a good place to dump everything we know about the guest. *)
   if verbose () then debug_info source inspect target_meta mpstats;
@@ -360,6 +364,37 @@ and get_target_firmware i_firmware guestcaps source output =
    | TargetUEFI -> info (f_"This guest requires UEFI on the target to boot."));
 
   target_firmware
+
+and get_target_boot_device g inspect =
+  (* We only do it for Linux, as most likely Windows never(?) boots
+   * from any drive other than C:.  We can revisit this decision
+   * if someone reports a bug.
+   *)
+  match inspect.i_type with
+  | "linux" ->
+     (try
+        (* In sane cases, the Grub stage1/boot.img (ie. the boot sector) is
+         * always on the same drive as /boot.  So we can just find out
+         * where /boot is mounted and use that.
+         *)
+        let boot_mountpoint = List.assoc "/boot" inspect.i_mountpoints in
+        let boot_device = g#part_to_dev boot_mountpoint in
+        let boot_device = g#device_index boot_device in
+        Some boot_device
+      with
+      | Not_found -> None
+      | G.Error msg
+           (* Returned by part_to_dev if the /boot mountpoint is not
+            * a partition name.
+            *)
+           when String.find msg "device name is not a partition" >= 0 -> None
+      | G.Error msg
+           (* Returned by device_index if the /boot device is not
+            * a normal drive name (eg. /dev/mdX).
+            *)
+           when String.find msg "device not found" >= 0 -> None
+     )
+  | _ -> None
 
 (* After conversion we dump as much information about the guest
  * as we can in one place.  Note this is only called when verbose

commit cbb4ec80edf75bc07625c0309826112503253c9c

convert: Look for GRUB signature first to identify boot device
In commit ca6ec6317e ("convert: Detect target boot device for Linux
guests") we tried to identify the boot order by looking for the device
containing the /boot filesystem.  However during our testing we found
a guest where GRUB was installed on /dev/sda but the /boot partition
was found on /dev/sdd.  This guest would not boot with the boot order
indicating that disk 4 (sdd) was the boot disk.

Change how we search for the boot device by first trying to find a
GRUB signature in the boot sector of any disk.  The first matching
disk (if any) would then be picked as the boot device.  If this fails
then we fall back to looking at the device containing /boot as before.

This also updates the common module to get:

  Richard W.M. Jones (1):
      mlstdutils: Export List.find_opt

Reported-by: Ming Xie
Thanks: Gerd Hoffmann
Fixes: commit ca6ec6317e20a633315f783a8ba4ece3c2fc01f2
Fixes: https://issues.redhat.com/browse/RHEL-108991
See-also: https://thestarman.pcministry.com/asm/mbr/GRUB.htm

convert/convert.ml
@@ -395,35 +395,54 @@ and get_target_firmware i_firmware guestcaps source output =
   target_firmware
 
 and get_target_boot_device g inspect =
-  (* We only do it for Linux, as most likely Windows never(?) boots
-   * from any drive other than C:.  We can revisit this decision
-   * if someone reports a bug.
-   *)
-  match inspect.i_type with
-  | "linux" ->
-     (try
-        (* In sane cases, the Grub stage1/boot.img (ie. the boot sector) is
-         * always on the same drive as /boot.  So we can just find out
-         * where /boot is mounted and use that.
-         *)
-        let boot_mountpoint = List.assoc "/boot" inspect.i_mountpoints in
-        let boot_device = g#part_to_dev boot_mountpoint in
-        let boot_device = g#device_index boot_device in
-        Some boot_device
-      with
-      | Not_found -> None
-      | G.Error msg
-           (* Returned by part_to_dev if the /boot mountpoint is not
-            * a partition name.
-            *)
-           when String.find msg "device name is not a partition" >= 0 -> None
-      | G.Error msg
-           (* Returned by device_index if the /boot device is not
-            * a normal drive name (eg. /dev/mdX).
-            *)
-           when String.find msg "device not found" >= 0 -> None
-     )
-  | _ -> None
+  with_return (fun {return} ->
+    (* We only do it for Linux, as most likely Windows never(?) boots
+     * from any drive other than C:.  We can revisit this decision
+     * if someone reports a bug.
+     *)
+    if inspect.i_type <> "linux" then return None;
+
+    (* Look for "GRUB" signature in the boot sector of each disk.
+     * If we find it, choose that disk.
+     *)
+    let devices = g#list_devices () |> Array.to_list in
+    let boot_device = List.find_opt (has_grub_signature g) devices in
+    let boot_device = Option.map g#device_index boot_device in
+    if boot_device <> None then return boot_device;
+
+    (* If that fails, in sane cases, the Grub stage1/boot.img (ie. the boot
+     * sector) is always on the same drive as /boot.  So we can just find
+     * out where /boot is mounted and use that.
+     *)
+    get_device_of_boot_filesystem g inspect
+  )
+
+and has_grub_signature g dev =
+  let boot_sector = g#pread_device dev 512 0_L in
+  let r = String.find boot_sector "GRUB" >= 0 in
+  debug "has_grub_signature: \"GRUB\" signature on %s? %b" dev r;
+  r
+
+and get_device_of_boot_filesystem g inspect =
+  try
+    let boot_mountpoint = List.assoc "/boot" inspect.i_mountpoints in
+    let boot_device = g#part_to_dev boot_mountpoint in
+    debug "get_device_of_boot_filesystem: found /boot filesystem on device %s"
+      boot_device;
+    let boot_device = g#device_index boot_device in
+    Some boot_device
+  with
+  | Not_found -> None
+    (* Returned by part_to_dev if the /boot mountpoint is not
+     * a partition name.
+     *)
+  | G.Error msg
+       when String.find msg "device name is not a partition" >= 0 -> None
+    (* Returned by device_index if the /boot device is not
+     * a normal drive name (eg. /dev/mdX).
+     *)
+  | G.Error msg
+       when String.find msg "device not found" >= 0 -> None
 
 (* After conversion we dump as much information about the guest
  * as we can in one place.  Note this is only called when verbose