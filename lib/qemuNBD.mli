libguestfs/virt-v2v

commit ff84a00fff55e95a72342ce228c04899ce31fa9a

lib: Make QemuNBD mini-library handle mutable
Similar change to the previous commit.

lib/qemuNBD.mli
@@ -31,17 +31,17 @@ val version : unit -> version
 type cmd
 (** A qemu-nbd command line.  Note this type is immutable. *)
 
-val new_cmd : cmd
-(** Return an empty command line.  {!set_disk} must be called. *)
+val create : string -> cmd
+(** Create a new qemu-nbd command.
 
-val set_snapshot : cmd -> bool -> cmd
+    The parameter is the required filename or URI of the
+    disk image to serve. *)
+
+val set_snapshot : cmd -> bool -> unit
 (** Set the snapshot [-s] flag. *)
 
-val set_format : cmd -> string option -> cmd
+val set_format : cmd -> string option -> unit
 (** Set the format [--format] parameter. *)
-
-val set_disk : cmd -> string -> cmd
-(** Set the filename of the disk image to serve. *)
 
 val run_unix : ?socket:string -> cmd -> string * int
 (** Start qemu-nbd command listening on a Unix domain socket,

input/input_disk.ml
@@ -113,10 +113,9 @@ let rec disk_source dir options args =
          On_exit.kill pid
 
       | format ->
-         let cmd = QemuNBD.new_cmd in
-         let cmd = QemuNBD.set_disk cmd disk in
-         let cmd = QemuNBD.set_snapshot cmd true in (* protective overlay *)
-         let cmd = QemuNBD.set_format cmd (Some format) in
+         let cmd = QemuNBD.create disk in
+         QemuNBD.set_snapshot cmd true; (* protective overlay *)
+         QemuNBD.set_format cmd (Some format);
          let _, pid = QemuNBD.run_unix ~socket cmd in
          On_exit.kill pid
   ) args;

input/input_libvirt.ml
@@ -113,10 +113,9 @@ and libvirt_servers dir disks =
 
          (* We use qemu-nbd for all other formats including auto-detect. *)
          | _ ->
-            let cmd = QemuNBD.new_cmd in
-            let cmd = QemuNBD.set_disk cmd filename in
-            let cmd = QemuNBD.set_snapshot cmd true in (* protective overlay *)
-            let cmd = QemuNBD.set_format cmd format in
+            let cmd = QemuNBD.create filename in
+            QemuNBD.set_snapshot cmd true; (* protective overlay *)
+            QemuNBD.set_format cmd format;
             let _, pid = QemuNBD.run_unix ~socket cmd in
             On_exit.kill pid
   ) disks

input/input_ova.ml
@@ -183,10 +183,9 @@ let rec ova_source dir options args =
       let socket = sprintf "%s/in%d" dir i in
       On_exit.unlink socket;
 
-      let cmd = QemuNBD.new_cmd in
-      let cmd = QemuNBD.set_disk cmd qemu_uri in
-      let cmd = QemuNBD.set_snapshot cmd true in (* protective overlay *)
-      let cmd = QemuNBD.set_format cmd None in (* auto-detect format *)
+      let cmd = QemuNBD.create qemu_uri in
+      QemuNBD.set_snapshot cmd true; (* protective overlay *)
+      QemuNBD.set_format cmd None; (* auto-detect format *)
       let _, pid = QemuNBD.run_unix ~socket cmd in
       On_exit.kill pid
   ) qemu_uris;

input/input_vmx.ml
@@ -56,12 +56,10 @@ let rec vmx_source dir options args =
          let socket = sprintf "%s/in%d" dir i in
          On_exit.unlink socket;
 
-         let cmd = QemuNBD.new_cmd in
-         let cmd =
-           QemuNBD.set_disk cmd
-             (absolute_path_from_other_file vmx_filename filename) in
-         let cmd = QemuNBD.set_snapshot cmd true in (* protective overlay *)
-         let cmd = QemuNBD.set_format cmd (Some "vmdk") in
+         let cmd = QemuNBD.create
+                     (absolute_path_from_other_file vmx_filename filename) in
+         QemuNBD.set_snapshot cmd true; (* protective overlay *)
+         QemuNBD.set_format cmd (Some "vmdk");
          let _, pid = QemuNBD.run_unix ~socket cmd in
          On_exit.kill pid
      ) filenames

lib/qemuNBD.ml
@@ -53,17 +53,14 @@ let version =
 
 type cmd = {
   disk : string;
-  snapshot : bool;
-  format : string option;
+  mutable snapshot : bool;
+  mutable format : string option;
 }
 
-let new_cmd = { disk = ""; snapshot = false; format = None }
+let create disk = { disk; snapshot = false; format = None }
 
-let set_snapshot cmd snap = { cmd with snapshot = snap }
-
-let set_format cmd format = { cmd with format = format }
-
-let set_disk cmd disk = { cmd with disk = disk }
+let set_snapshot cmd snap = cmd.snapshot <- snap
+let set_format cmd format = cmd.format <- format
 
 let run_unix ?socket { disk; snapshot; format } =
   assert (disk <> "");

output/output.ml
@@ -106,10 +106,9 @@ let output_to_local_file ?(changeuid = fun f -> f ())
      On_exit.kill pid
 
   | "qcow2" ->
-     let cmd = QemuNBD.new_cmd in
-     let cmd = QemuNBD.set_disk cmd filename in
-     let cmd = QemuNBD.set_snapshot cmd false in
-     let cmd = QemuNBD.set_format cmd (Some "qcow2") in
+     let cmd = QemuNBD.create filename in
+     QemuNBD.set_snapshot cmd false;
+     QemuNBD.set_format cmd (Some "qcow2");
      let _, pid = QemuNBD.run_unix ~socket cmd in
      On_exit.kill pid
 

commit 0fcea25ba1b2a41603fad1b6a589004a9325df41

lib/qemuNBD.mli: Fix docs, as this type is mutable
Fixes: commit ff84a00fff55e95a72342ce228c04899ce31fa9a
See-also: commit d50966c2a480bda033f6e63bb797f86c13d576bd

lib/qemuNBD.mli
@@ -29,7 +29,7 @@ val version : unit -> version
 (** Get the installed version of qemu-nbd. *)
 
 type cmd
-(** A qemu-nbd command line.  Note this type is immutable. *)
+(** A qemu-nbd command line. *)
 
 val create : string -> cmd
 (** Create a new qemu-nbd command.