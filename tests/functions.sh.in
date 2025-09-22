libguestfs/virt-v2v

commit cb7d1cd1cf1e6e29e9945227aff3162e3cc9fb6a

Add new virt-v2v-open tool
This can be used to open the virt-v2v input method and run an almost
arbitrary program on it.  For example you can run virt-inspector to
inspect the input (eg. to find root devices and other information for
the virt-v2v --root option), or guestfish.

Currently this only opens the input read-only, although read-write
access might be added in future.

Suggested-by: Martin Necas
Fixes: https://issues.redhat.com/browse/RHEL-88985

.gitignore
@@ -52,6 +52,7 @@ Makefile.in
 /docs/virt-v2v-input-vmware.1
 /docs/virt-v2v-input-xen.1
 /docs/virt-v2v-inspector.1
+/docs/virt-v2v-open.1
 /docs/virt-v2v-output-local.1
 /docs/virt-v2v-output-openstack.1
 /docs/virt-v2v-output-rhv.1
@@ -85,6 +86,8 @@ Makefile.in
 /missing
 /ocaml-dep.sh
 /ocaml-link.sh
+/open/.depend
+/open/virt-v2v-open
 /output/.depend
 /output/output_rhv_upload_*_source.ml
 /output/var_expander_tests

docs/Makefile.am
@@ -25,6 +25,7 @@ EXTRA_DIST = \
 	virt-v2v-input-vmware.pod \
 	virt-v2v-input-xen.pod \
 	virt-v2v-inspector.pod \
+	virt-v2v-open.pod \
 	virt-v2v-output-local.pod \
 	virt-v2v-output-openstack.pod \
 	virt-v2v-output-rhv.pod \
@@ -46,6 +47,7 @@ man_MANS = \
 	virt-v2v-input-vmware.1 \
 	virt-v2v-input-xen.1 \
 	virt-v2v-inspector.1 \
+	virt-v2v-open.1 \
 	virt-v2v-output-local.1 \
 	virt-v2v-output-openstack.1 \
 	virt-v2v-output-rhv.1 \
@@ -64,6 +66,7 @@ noinst_DATA = \
 	$(top_builddir)/website/virt-v2v-input-vmware.1.html \
 	$(top_builddir)/website/virt-v2v-input-xen.1.html \
 	$(top_builddir)/website/virt-v2v-inspector.1.html \
+	$(top_builddir)/website/virt-v2v-open.1.html \
 	$(top_builddir)/website/virt-v2v-output-local.1.html \
 	$(top_builddir)/website/virt-v2v-output-openstack.1.html \
 	$(top_builddir)/website/virt-v2v-output-rhv.1.html \
@@ -139,6 +142,18 @@ stamp-virt-v2v-inspector.pod: virt-v2v-inspector.pod
 	$(PODWRAPPER) \
 	  --man virt-v2v-inspector.1 \
 	  --html $(top_builddir)/website/virt-v2v-inspector.1.html \
+	  --path $(top_srcdir)/common/options \
+	  --license GPLv2+ \
+	  --warning safe \
+	  $<
+	touch $@
+
+virt-v2v-open.1 $(top_builddir)/website/virt-v2v-open.1.html: stamp-virt-v2v-open.pod
+
+stamp-virt-v2v-open.pod: virt-v2v-open.pod
+	$(PODWRAPPER) \
+	  --man virt-v2v-open.1 \
+	  --html $(top_builddir)/website/virt-v2v-open.1.html \
 	  --path $(top_srcdir)/common/options \
 	  --license GPLv2+ \
 	  --warning safe \

open/Makefile.am
@@ -1,0 +1,126 @@
+# libguestfs virt-v2v-open tool
+# Copyright (C) 2009-2025 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+include $(top_srcdir)/subdir-rules.mk
+
+EXTRA_DIST = \
+	$(SOURCES_MLI) \
+	$(SOURCES_ML) \
+	$(SOURCES_C)
+
+SOURCES_MLI = \
+	open.mli
+
+SOURCES_ML = \
+	open.ml
+
+SOURCES_C = \
+	dummy.c
+
+bin_PROGRAMS = virt-v2v-open
+
+virt_v2v_open_SOURCES = $(SOURCES_C)
+virt_v2v_open_CPPFLAGS = \
+	-DCAML_NAME_SPACE \
+	-I. \
+	-I$(top_builddir) \
+	-I$(shell $(OCAMLC) -where) \
+	-I$(top_srcdir)/lib
+virt_v2v_open_CFLAGS = \
+	-pthread \
+	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS) \
+	$(LIBVIRT_CFLAGS)
+
+BOBJECTS = $(SOURCES_ML:.ml=.cmo)
+XOBJECTS = $(BOBJECTS:.cmo=.cmx)
+
+OCAMLPACKAGES = \
+	-package str,unix,guestfs,libvirt,nbd \
+	-I $(top_builddir)/common/utils/.libs \
+	-I $(top_builddir)/common/qemuopts/.libs \
+	-I $(top_builddir)/gnulib/lib/.libs \
+	-I $(top_builddir)/lib \
+	-I $(top_builddir)/input \
+	-I $(top_builddir)/common/mlstdutils \
+	-I $(top_builddir)/common/mlutils \
+	-I $(top_builddir)/common/mlgettext \
+	-I $(top_builddir)/common/mlpcre \
+	-I $(top_builddir)/common/mlxml \
+	-I $(top_builddir)/common/mltools \
+	-I $(top_builddir)/common/mlcustomize \
+	-I $(top_builddir)/common/mldrivers
+if HAVE_OCAML_PKG_GETTEXT
+OCAMLPACKAGES += -package gettext-stub
+endif
+
+OCAMLCLIBS = \
+	-pthread \
+	-lqemuopts \
+	$(LIBGUESTFS_LIBS) \
+	$(LIBVIRT_LIBS) \
+	$(LIBCRYPT_LIBS) \
+	$(LIBXML2_LIBS) \
+	$(JSON_C_LIBS) \
+	$(LIBOSINFO_LIBS) \
+	$(LIBINTL) \
+	$(LIBNBD_LIBS) \
+	-lgnu
+
+OCAMLFLAGS = $(OCAML_FLAGS) $(OCAML_WARN_ERROR) -ccopt '$(CFLAGS)'
+
+if !HAVE_OCAMLOPT
+OBJECTS = $(BOBJECTS)
+else
+OBJECTS = $(XOBJECTS)
+endif
+
+OCAMLLINKFLAGS = \
+	mlstdutils.$(MLARCHIVE) \
+	mlgettext.$(MLARCHIVE) \
+	mlpcre.$(MLARCHIVE) \
+	mlxml.$(MLARCHIVE) \
+	mlcutils.$(MLARCHIVE) \
+	mltools.$(MLARCHIVE) \
+	mllibvirt.$(MLARCHIVE) \
+	mlcustomize.$(MLARCHIVE) \
+	mldrivers.$(MLARCHIVE) \
+	mlv2vlib.$(MLARCHIVE) \
+	mlinput.$(MLARCHIVE) \
+	$(LINK_CUSTOM_OCAMLC_ONLY)
+
+virt_v2v_open_DEPENDENCIES = \
+	$(OBJECTS) \
+	$(top_builddir)/input/mlinput.$(MLARCHIVE) \
+	$(top_builddir)/lib/mlv2vlib.$(MLARCHIVE) \
+	$(top_srcdir)/ocaml-link.sh
+virt_v2v_open_LINK = \
+	$(top_srcdir)/ocaml-link.sh -cclib '$(OCAMLCLIBS)' -- \
+	  $(OCAMLFIND) $(BEST) $(OCAMLFLAGS) $(OCAMLPACKAGES) $(OCAMLLINKFLAGS) \
+	  $(OBJECTS) -o $@
+
+# Data directory.
+
+virttoolsdatadir = $(datadir)/virt-tools
+
+# Dependencies.
+.depend: \
+	$(srcdir)/*.mli \
+	$(srcdir)/*.ml \
+	$(filter %.ml,$(BUILT_SOURCES))
+	$(top_builddir)/ocaml-dep.sh $^
+-include .depend

Makefile.am
@@ -44,6 +44,7 @@ SUBDIRS += convert
 SUBDIRS += v2v
 SUBDIRS += inspector
 SUBDIRS += in-place
+SUBDIRS += open
 
 SUBDIRS += tests
 
@@ -116,7 +117,8 @@ po/POTFILES: configure.ac
 po/POTFILES-ml: configure.ac
 	rm -f $@ $@-t
 	cd $(srcdir); \
-	find common/ml* lib in-place input inspector output v2v -name '*.ml' | \
+	find common/ml* lib in-place input inspector open output v2v \
+	    -name '*.ml' | \
 	grep -v '^common/mlprogress/' | \
 	grep -v '^common/mlvisit/' | \
 	grep -v '^lib/config.ml$$' | \

docs/virt-v2v-open.pod
@@ -1,0 +1,183 @@
+=head1 NAME
+
+virt-v2v-open - Open the virt-v2v input and run a program on it
+
+=head1 SYNOPSIS
+
+ virt-v2v-open [-i* options] guest --run 'program [--options] @@'
+
+ virt-v2v-open -i disk guest.img --run 'virt-inspector --format=raw @@'
+
+ virt-v2v-open -i disk guest.img --run 'guestfish --ro --format=raw @@ -i'
+
+=head1 DESCRIPTION
+
+Virt-v2v-open is a companion tool for L<virt-v2v(1)> and
+L<virt-v2v-inspector(1)> which can be used before conversion to open
+the input side of virt-v2v and run a program.
+
+Some uses for this include running L<virt-inspector(1)> directly on
+the source guest to find source operating systems (to use with the
+virt-v2v I<--root> option).  Or running guestfish to take a look
+inside the source guest before conversion.  You can follow that by
+running L<virt-v2v-inspector(1)> to estimate how much space would be
+needed to convert that guest, and if conversion of the guest is
+possible.
+
+This manual page only documents the program options, not all of the
+I<-i*> options which are the same as virt-v2v.  You should read
+L<virt-v2v(1)> first.
+
+Notes:
+
+=over 4
+
+=item *
+
+The source guest is always opened read-only.  You will not be able to
+make persistent changes.
+
+=item *
+
+L<virt-inspector(1)> can be used directly on local disk images.
+
+=back
+
+=head2 Selecting the input guest
+
+You can run virt-v2v-open with the same I<-i*> options as virt-v2v.
+(Don't use any I<-o*> options).  This will select the guest that you
+want to open.
+
+=head2 Running the program
+
+On the command line, put the program that you want to run on the
+source guest and any other parameters that program needs into the
+I<--run> parameter.  C<@@> in the parameter is substituted with
+I<-a DISK ...> for each source disk, in a way which is compatible with
+L<virt-inspector(1)> and L<guestfish(1)>.
+
+ virt-v2v-open [-i ...] guest \
+               --run 'virt-inspector --format=raw @@ > output.xml'
+
+ virt-v2v-open [-i ...] guest \
+               --run 'guestfish --ro --format=raw @@ -i'
+
+ virt-v2v-open [-i ...] guest \
+               --run 'guestfish --ro --format=raw @@ run : list-filesystems'
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--help>
+
+Display help.
+
+=item B<-v>
+
+=item B<--verbose>
+
+Enable verbose messages for debugging.
+
+=item B<-V>
+
+=item B<--version>
+
+Display version number and exit.
+
+=item B<-x>
+
+Enable tracing of libguestfs API calls.
+
+=item B<-i> ...
+
+=item B<-ic> ...
+
+=item B<-if> ...
+
+=item B<-io> ...
+
+=item B<-ip> ...
+
+=item B<-it> ...
+
+All of the I<-i*> options supported by virt-v2v are also supported by
+virt-v2v-open.
+
+=item B<--colors>
+
+=item B<--colours>
+
+=item B<--machine-readable>
+
+=item B<--machine-readable>=format
+
+=item B<-q>
+
+=item B<--quiet>
+
+=item B<--wrap>
+
+These options work in the same way as the equivalent virt-v2v options.
+
+=item B<--run> COMMAND
+
+The command that you want to run on the source guest.  C<@@> in the
+command is substituted with a list of I<-a DISK> options that is
+compatible with virt-inspector and guestfish.
+
+=back
+
+=head1 FILES
+
+Files used are the same as for virt-v2v.  See L<virt-v2v(1)/FILES>.
+
+=head1 ENVIRONMENT VARIABLES
+
+Environment variables used are the same as for virt-v2v.  See
+L<virt-v2v(1)/ENVIRONMENT VARIABLES>.
+
+=head1 SEE ALSO
+
+L<virt-v2v(1)>,
+L<virt-p2v(1)>,
+L<virt-inspector(1)>,
+L<virt-v2v-inspector(1)>,
+L<guestfs(3)>,
+L<guestfish(1)>,
+L<qemu-img(1)>,
+L<nbdkit(1)>,
+L<http://libguestfs.org/>.
+
+=head1 AUTHORS
+
+Matthew Booth
+
+Cédric Bosdonnat
+
+Laszlo Ersek
+
+Tomáš Golembiovský
+
+Shahar Havivi
+
+Richard W.M. Jones
+
+Roman Kagan
+
+Mike Latimer
+
+Nir Soffer
+
+Pino Toscano
+
+Xiaodai Wang
+
+Ming Xie
+
+Tingting Zheng
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009-2025 Red Hat Inc.

tests/Makefile.am
@@ -110,6 +110,8 @@ TESTS = \
 	test-oa-option-raw.sh \
 	test-of-option.sh \
 	test-on-option.sh \
+	test-open.sh \
+	test-open-encrypted.sh \
 	test-print-source.sh \
 	test-reject-blank-disk.sh \
 	test-rhbz1232192.sh \
@@ -307,6 +309,8 @@ EXTRA_DIST += \
 	test-oa-option-raw.sh \
 	test-of-option.sh \
 	test-on-option.sh \
+	test-open.sh \
+	test-open-encrypted.sh \
 	test-print-source.expected \
 	test-print-source.sh \
 	test-phony-win10-ls.txt \

configure.ac
@@ -156,6 +156,7 @@ AC_CONFIG_FILES([Makefile
                  inspector/Makefile
                  lib/Makefile
                  lib/config.ml
+                 open/Makefile
                  output/Makefile
                  po-docs/Makefile
                  po-docs/ja/Makefile

docs/virt-v2v-release-notes-2.8.pod
@@ -17,6 +17,11 @@ Virt-v2v 2.8.0 was released on S<B<XXX XXX 2025>>.
 
 =head2 New features
 
+New L<virt-v2v-open(1)> tool.  This can be used to open the virt-v2v
+source and run a user program on it.  For example L<virt-inspector(1)>
+or L<guestfish(1)> can be used to examine the source to discover what
+operating systems or files are present before conversion.
+
 Windows Server 2025 is supported (thanks Ming Xie).
 
 Two features have been added to enhance data integrity.  Firstly you

commit 5d61636cac811b70d121678033715e2458242a47

docs: Move tool documentation into tool directories
For virt-v2v-in-place, virt-v2v-inspector and virt-v2v-open, move the
tool manual into the same directory as the tool.  This convention is
used in guestfs-tools and elsewhere, to keep the documentation
alongside the tool.

This also adds doc tests for virt-v2v-open, which had accidentally
been omitted before.

Fixes: commit cb7d1cd1cf1e6e29e9945227aff3162e3cc9fb6a

.gitignore
@@ -48,11 +48,8 @@ Makefile.in
 /docs/stamp-virt-v2v*.pod
 /docs/virt-v2v.1
 /docs/virt-v2v-hacking.1
-/docs/virt-v2v-in-place.1
 /docs/virt-v2v-input-vmware.1
 /docs/virt-v2v-input-xen.1
-/docs/virt-v2v-inspector.1
-/docs/virt-v2v-open.1
 /docs/virt-v2v-output-local.1
 /docs/virt-v2v-output-openstack.1
 /docs/virt-v2v-output-ovirt.1
@@ -66,10 +63,14 @@ Makefile.in
 /.gitattributes
 /.git-module-status
 /in-place/.depend
+/in-place/stamp-virt-v2v-in-place.pod
 /in-place/virt-v2v-in-place
+/in-place/virt-v2v-in-place.1
 /input/.depend
 /inspector/.depend
+/inspector/stamp-virt-v2v-inspector.pod
 /inspector/virt-v2v-inspector
+/inspector/virt-v2v-inspector.1
 /installcheck.sh
 /install-sh
 /libtool
@@ -87,7 +88,9 @@ Makefile.in
 /ocaml-dep.sh
 /ocaml-link.sh
 /open/.depend
+/open/stamp-virt-v2v-open.pod
 /open/virt-v2v-open
+/open/virt-v2v-open.1
 /output/.depend
 /output/output_ovirt_upload_*_source.ml
 /output/var_expander_tests

docs/Makefile.am
@@ -21,11 +21,8 @@ EXTRA_DIST = \
 	test-docs.sh \
 	virt-v2v.pod \
 	virt-v2v-hacking.pod \
-	virt-v2v-in-place.pod \
 	virt-v2v-input-vmware.pod \
 	virt-v2v-input-xen.pod \
-	virt-v2v-inspector.pod \
-	virt-v2v-open.pod \
 	virt-v2v-output-local.pod \
 	virt-v2v-output-openstack.pod \
 	virt-v2v-output-ovirt.pod \
@@ -43,10 +40,7 @@ EXTRA_DIST = \
 man_MANS = \
 	virt-v2v.1 \
 	virt-v2v-hacking.1 \
-	virt-v2v-in-place.1 \
 	virt-v2v-input-vmware.1 \
-	virt-v2v-inspector.1 \
-	virt-v2v-open.1 \
 	virt-v2v-output-local.1 \
 	virt-v2v-output-openstack.1 \
 	virt-v2v-release-notes-1.42.1 \
@@ -60,10 +54,7 @@ man_MANS = \
 noinst_DATA = \
 	$(top_builddir)/website/virt-v2v.1.html \
 	$(top_builddir)/website/virt-v2v-hacking.1.html \
-	$(top_builddir)/website/virt-v2v-in-place.1.html \
 	$(top_builddir)/website/virt-v2v-input-vmware.1.html \
-	$(top_builddir)/website/virt-v2v-inspector.1.html \
-	$(top_builddir)/website/virt-v2v-open.1.html \
 	$(top_builddir)/website/virt-v2v-output-local.1.html \
 	$(top_builddir)/website/virt-v2v-output-openstack.1.html \
 	$(top_builddir)/website/virt-v2v-release-notes-1.42.1.html \
@@ -117,19 +108,6 @@ stamp-virt-v2v-hacking.pod: virt-v2v-hacking.pod
 	  $<
 	touch $@
 
-virt-v2v-in-place.1 $(top_builddir)/website/virt-v2v-in-place.1.html: stamp-virt-v2v-in-place.pod
-
-stamp-virt-v2v-in-place.pod: virt-v2v-in-place.pod
-	$(PODWRAPPER) \
-	  --pp "$(PP_M4)" \
-	  --man virt-v2v-in-place.1 \
-	  --html $(top_builddir)/website/virt-v2v-in-place.1.html \
-	  --path $(top_srcdir)/common/options \
-	  --license GPLv2+ \
-	  --warning safe \
-	  $<
-	touch $@
-
 virt-v2v-input-vmware.1 $(top_builddir)/website/virt-v2v-input-vmware.1.html: stamp-virt-v2v-input-vmware.pod
 
 stamp-virt-v2v-input-vmware.pod: virt-v2v-input-vmware.pod
@@ -153,30 +131,6 @@ stamp-virt-v2v-input-xen.pod: virt-v2v-input-xen.pod
 	  $<
 	touch $@
 endif
-
-virt-v2v-inspector.1 $(top_builddir)/website/virt-v2v-inspector.1.html: stamp-virt-v2v-inspector.pod
-
-stamp-virt-v2v-inspector.pod: virt-v2v-inspector.pod
-	$(PODWRAPPER) \
-	  --man virt-v2v-inspector.1 \
-	  --html $(top_builddir)/website/virt-v2v-inspector.1.html \
-	  --path $(top_srcdir)/common/options \
-	  --license GPLv2+ \
-	  --warning safe \
-	  $<
-	touch $@
-
-virt-v2v-open.1 $(top_builddir)/website/virt-v2v-open.1.html: stamp-virt-v2v-open.pod
-
-stamp-virt-v2v-open.pod: virt-v2v-open.pod
-	$(PODWRAPPER) \
-	  --man virt-v2v-open.1 \
-	  --html $(top_builddir)/website/virt-v2v-open.1.html \
-	  --path $(top_srcdir)/common/options \
-	  --license GPLv2+ \
-	  --warning safe \
-	  $<
-	touch $@
 
 virt-v2v-output-local.1 $(top_builddir)/website/virt-v2v-output-local.1.html: stamp-virt-v2v-output-local.pod
 

in-place/Makefile.am
@@ -20,7 +20,8 @@ include $(top_srcdir)/subdir-rules.mk
 EXTRA_DIST = \
 	$(SOURCES_MLI) \
 	$(SOURCES_ML) \
-	$(SOURCES_C)
+	$(SOURCES_C) \
+	virt-v2v-in-place.pod
 
 SOURCES_MLI = \
 	in_place.mli
@@ -129,3 +130,24 @@ virttoolsdatadir = $(datadir)/virt-tools
 	$(filter %.ml,$(BUILT_SOURCES))
 	$(top_builddir)/ocaml-dep.sh $^
 -include .depend
+
+# Manual pages.
+
+man_MANS = virt-v2v-in-place.1
+noinst_DATA = $(top_builddir)/website/virt-v2v-in-place.1.html
+
+virt-v2v-in-place.1 $(top_builddir)/website/virt-v2v-in-place.1.html: stamp-virt-v2v-in-place.pod
+
+stamp-virt-v2v-in-place.pod: virt-v2v-in-place.pod
+	$(PODWRAPPER) \
+	  --pp "$(PP_M4)" \
+	  --man virt-v2v-in-place.1 \
+	  --html $(top_builddir)/website/virt-v2v-in-place.1.html \
+	  --path $(top_srcdir)/common/options \
+	  --license GPLv2+ \
+	  --warning safe \
+	  $<
+	touch $@
+
+TESTS_ENVIRONMENT = $(top_builddir)/run --test
+TESTS = test-docs.sh

inspector/Makefile.am
@@ -20,7 +20,8 @@ include $(top_srcdir)/subdir-rules.mk
 EXTRA_DIST = \
 	$(SOURCES_MLI) \
 	$(SOURCES_ML) \
-	$(SOURCES_C)
+	$(SOURCES_C) \
+	virt-v2v-inspector.pod
 
 SOURCES_MLI = \
 	create_inspector_xml.mli \
@@ -129,3 +130,23 @@ virttoolsdatadir = $(datadir)/virt-tools
 	$(filter %.ml,$(BUILT_SOURCES))
 	$(top_builddir)/ocaml-dep.sh $^
 -include .depend
+
+# Manual pages.
+
+man_MANS = virt-v2v-inspector.1
+noinst_DATA = $(top_builddir)/website/virt-v2v-inspector.1.html
+
+virt-v2v-inspector.1 $(top_builddir)/website/virt-v2v-inspector.1.html: stamp-virt-v2v-inspector.pod
+
+stamp-virt-v2v-inspector.pod: virt-v2v-inspector.pod
+	$(PODWRAPPER) \
+	  --man virt-v2v-inspector.1 \
+	  --html $(top_builddir)/website/virt-v2v-inspector.1.html \
+	  --path $(top_srcdir)/common/options \
+	  --license GPLv2+ \
+	  --warning safe \
+	  $<
+	touch $@
+
+TESTS_ENVIRONMENT = $(top_builddir)/run --test
+TESTS = test-docs.sh

open/Makefile.am
@@ -20,7 +20,8 @@ include $(top_srcdir)/subdir-rules.mk
 EXTRA_DIST = \
 	$(SOURCES_MLI) \
 	$(SOURCES_ML) \
-	$(SOURCES_C)
+	$(SOURCES_C) \
+	virt-v2v-open.pod
 
 SOURCES_MLI = \
 	open.mli
@@ -124,3 +125,23 @@ virttoolsdatadir = $(datadir)/virt-tools
 	$(filter %.ml,$(BUILT_SOURCES))
 	$(top_builddir)/ocaml-dep.sh $^
 -include .depend
+
+# Manual pages.
+
+man_MANS = virt-v2v-open.1
+noinst_DATA = $(top_builddir)/website/virt-v2v-open.1.html
+
+virt-v2v-open.1 $(top_builddir)/website/virt-v2v-open.1.html: stamp-virt-v2v-open.pod
+
+stamp-virt-v2v-open.pod: virt-v2v-open.pod
+	$(PODWRAPPER) \
+	  --man virt-v2v-open.1 \
+	  --html $(top_builddir)/website/virt-v2v-open.1.html \
+	  --path $(top_srcdir)/common/options \
+	  --license GPLv2+ \
+	  --warning safe \
+	  $<
+	touch $@
+
+TESTS_ENVIRONMENT = $(top_builddir)/run --test
+TESTS = test-docs.sh

in-place/test-docs.sh
@@ -22,16 +22,14 @@ set -x
 
 skip_if_skipped
 
-$srcdir/../podcheck.pl virt-v2v.pod virt-v2v \
+$srcdir/../podcheck.pl virt-v2v-in-place.pod virt-v2v-in-place \
   --path $srcdir/../common/options \
   --ignore=\
---debug-overlay,\
 --ic,\
 --if,\
 --io,\
 --ip,\
 --it,\
---in-place,\
 --oa,\
 --oc,\
 --of,\

inspector/test-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 # libguestfs
-# Copyright (C) 2016-2025 Red Hat Inc.
+# Copyright (C) 2016 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,21 +22,12 @@ set -x
 
 skip_if_skipped
 
-$srcdir/../podcheck.pl virt-v2v.pod virt-v2v \
+$srcdir/../podcheck.pl virt-v2v-inspector.pod virt-v2v-inspector \
   --path $srcdir/../common/options \
   --ignore=\
---debug-overlay,\
 --ic,\
 --if,\
 --io,\
 --ip,\
 --it,\
---in-place,\
---oa,\
---oc,\
---of,\
---on,\
---oo,\
---op,\
---os,\
 $virt_customize_options

open/test-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 # libguestfs
-# Copyright (C) 2016-2025 Red Hat Inc.
+# Copyright (C) 2016 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,21 +22,12 @@ set -x
 
 skip_if_skipped
 
-$srcdir/../podcheck.pl virt-v2v.pod virt-v2v \
+$srcdir/../podcheck.pl virt-v2v-open.pod virt-v2v-open \
   --path $srcdir/../common/options \
   --ignore=\
---debug-overlay,\
+--format,\
 --ic,\
 --if,\
 --io,\
 --ip,\
---it,\
---in-place,\
---oa,\
---oc,\
---of,\
---on,\
---oo,\
---op,\
---os,\
-$virt_customize_options
+--it

tests/functions.sh.in
@@ -49,6 +49,50 @@ abs_top_srcdir="@abs_top_srcdir@"
 abs_builddir="@abs_builddir@"
 abs_top_builddir="@abs_top_builddir@"
 
+# List of virt-customize options, so we can skip testing these
+# options in */test-docs.sh.
+virt_customize_options=\
+--append-line,\
+--chmod,\
+--chown,\
+--commands-from-file,\
+--copy,\
+--copy-in,\
+--delete,\
+--edit,\
+--firstboot,\
+--firstboot-command,\
+--firstboot-install,\
+--hostname,\
+--install,\
+--link,\
+--mkdir,\
+--move,\
+--no-logfile,\
+--no-selinux-relabel,\
+--password,\
+--password-crypto,\
+--root-password,\
+--run,\
+--run-command,\
+--scrub,\
+--selinux-relabel,\
+--sm-attach,\
+--sm-credentials,\
+--sm-register,\
+--sm-remove,\
+--sm-unregister,\
+--ssh-inject,\
+--tar-in,\
+--timezone,\
+--touch,\
+--truncate,\
+--truncate-recursive,\
+--uninstall,\
+--update,\
+--upload,\
+--write
+
 # cleanup_fn cmd [args]
 #
 # Run the command ‘cmd [args]’ when the test script exits.  This is