libguestfs/virt-v2v

commit 970d7123c2025bc148870f4bc6fa75fa9e95905f

input/ssh: Use nbdinfo --can connect (instead of --size)
nbdinfo --size prints the size on stdout, causing it to appear in
virt-v2v output.  Using --can connect instead is silent.

Note that nbdinfo --can connect was added in libnbd 1.9.2 (Jul 2021)
and our previous minimum version of libnbd was 1.9.3 so we're OK.
However since neither of these was a stable version I also updated the
minimum libnbd requirement to 1.10 (Sep 2021), and added a proper
check in ./configure

Fixes: commit fb72e059863a60503b6011b8590c25c3a010a58f

input/ssh.ml
@@ -56,8 +56,8 @@ let download_file ~server ?port ?user ?password path output =
 let remote_file_exists ~server ?port ?user ?password path =
   let uri = start_nbdkit ~server ?port ?user ?password path in
 
-  (* Testing for remote size using nbdinfo should be sufficient to
+  (* Testing that we can connect to the nbdkit server is enough to
    * prove the remote file exists.
    *)
-  let cmd = [ "nbdinfo"; "--size"; uri ] in
+  let cmd = [ "nbdinfo"; "--can"; "connect"; uri ] in
   run_command cmd = 0

README
@@ -51,7 +51,7 @@ REQUIREMENTS
 
 * OCaml bindings for libvirt (https://gitlab.com/libvirt/libvirt-ocaml)
 
-* libnbd >= 1.9.3 (https://gitlab.com/nbdkit/libnbd)
+* libnbd >= 1.10 (https://gitlab.com/nbdkit/libnbd)
 
 * OCaml bindings for libnbd
 

m4/guestfs-libraries.m4
@@ -21,7 +21,7 @@ dnl Of course we need libguestfs.
 PKG_CHECK_MODULES([LIBGUESTFS], [libguestfs >= 1.44])
 
 dnl And libnbd.
-PKG_CHECK_MODULES([LIBNBD], [libnbd])
+PKG_CHECK_MODULES([LIBNBD], [libnbd >= 1.10])
 
 dnl Test if it's GNU or XSI strerror_r.
 AC_FUNC_STRERROR_R

commit 986edd3e609cc33e0004e41447b74b93572bd61e

input/ssh.ml: Fix Ssh.remote_file_exists
This function was inadvertently broken in commit 970d7123c2
("input/ssh: Use nbdinfo --can connect (instead of --size)"), since
'nbdinfo --can connect' just says that nbdinfo managed to connect at
all, even if the connection failed to fully negotiate the NBD
handshake.  (This is possibly a bug in nbdinfo.)

Using --size means we must have negotiated the NBD handshake.

However this leaves the problem that commit 970d7123c2 was originally
intended to fix, that 'nbdinfo --size' prints the size on stdout.  To
fix this, replace use of run_command with shell_command so we can
redirect stdout.

Reported-by: Ming Xie
Thanks: Ming Xie
Updates: commit fb72e059863a60503b6011b8590c25c3a010a58f
Reverts: commit 970d7123c2025bc148870f4bc6fa75fa9e95905f

input/ssh.ml
@@ -56,10 +56,13 @@ let download_file ~server ?port ?user ?password path output =
 let remote_file_exists ~server ?port ?user ?password path =
   let uri = start_nbdkit ~server ?port ?user ?password path in
 
-  (* Testing that we can connect to the nbdkit server is enough to
+  (* Testing that the nbdkit server can get the size is enough to
    * prove the remote file exists.
    *)
-  let cmd = [ Config.nbdinfo; "--can"; "connect"; uri ] in
-  let r = run_command cmd = 0 in
+  let cmd = sprintf "%s --size %s >/dev/null %s"
+              Config.nbdinfo (quote uri)
+              (* If verbose then allow stderr to go to the log, else hide it *)
+              (if verbose () then "" else "2>&1") in
+  let r = shell_command cmd = 0 in
   debug "ssh: remote_file_exists: testing %s -> %b" path r;
   r