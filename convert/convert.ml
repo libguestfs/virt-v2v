libguestfs/virt-v2v

commit db696f4cff486373a87c71d1de2d4dda4b047c8f

smp: use cgroup CPU limits for appliance SMP
Use Cgroup.nr_cpus_available instead of nr_processors_online
to respect container cgroup CPU limits when setting SMP.

convert/convert.ml
@@ -67,13 +67,7 @@ let rec convert input_disks options source =
   let smp =
     match options.smp with
     | None ->
-       (* Default (if [--smp] option is not used) is to set the number
-        * according to the number of physical CPUs on the host, but
-        * limit it because each vCPU consumes guest RAM.  This is
-        * necessary to allow parallel mkinitrd which greatly improves
-        * performance.
-        *)
-       min 8 (Sysconf.nr_processors_online ())
+       min 8 (Cgroup.nr_cpus_available ())
     | Some smp -> smp in
   g#set_smp smp;
 

commit 23f7289762cd866d7740e62335d5219ced0d4b6c

convert/convert.ml: Add back comment that was deleted by previous commit
This comment explains why we set SMP, so leave it in.

Fixes: commit db696f4cff486373a87c71d1de2d4dda4b047c8f

convert/convert.ml
@@ -67,6 +67,12 @@ let rec convert input_disks options source =
   let smp =
     match options.smp with
     | None ->
+       (* Default (if [--smp] option is not used) is to set the number
+        * according to the number of physical CPUs available (on the
+        * host or in the current cgroup).  But limit it because each
+        * vCPU consumes guest RAM.  This is necessary to allow parallel
+        * mkinitrd which greatly improves performance.
+        *)
        min 8 (Cgroup.nr_cpus_available ())
     | Some smp -> smp in
   g#set_smp smp;